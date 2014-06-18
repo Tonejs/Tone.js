6fc33a29c208d52d5f17eff4d608877889c91855
Yotam Mann <yotammann@gmail.com>
2014-06-18 18:43:47 -0400

    can't test web audio with phantomJS. :(

--- test/test.js
+++ test/test.js
@@ -3,19 +3,9 @@ require.config({
 	paths : {
 		"Tone" : "../Tone",
 		"chai" : "./testDeps/chai",
-		"Recorder" : "./testDeps/recorder"
-	},
-	shim : {
-		"Recorder" : {
-			exports : "Recorder"
-		}
 	}
 });
 
 require(["tests/Timing", "tests/Signal", "tests/Math", "tests/Transport", "tests/Sources"], function(){
-	if (window.mochaPhantomJS) { 
-		mochaPhantomJS.run(); 
-	} else { 
-		mocha.run(); 
-	}
+	mocha.run(); 
 });
--- test/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash          
-echo "running tests"
-#runs all the test in teh current dir
-mocha-phantomjs -R dot ./test.html
