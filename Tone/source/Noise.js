a3abf8e97984890dc50867566b54147aa4c0f10f
Yotam Mann <yotammann@gmail.com>
2014-06-20 00:25:00 -0400

    better noise! with test and example

--- Tone/source/Noise.js
+++ Tone/source/Noise.js
@@ -1,89 +1,229 @@
-///////////////////////////////////////////////////////////////////////////////
-//
-//  NOISE
-//
-///////////////////////////////////////////////////////////////////////////////
-define(["Tone/core/Tone"], function(Tone){
+define(["Tone/core/Tone", "Tone/source/Source"], function(Tone){
 
-    //@param {string} type the noise type
-    Tone.Noise = function(type){
-    	//extend Unit
-    	Tone.call(this);
+	var sampleRate = Tone.context.sampleRate;
+	//two seconds per buffer
+	var bufferLength = sampleRate * 4;
 
-    	//components
-    	this.jsNode = this.context.createScriptProcessor(this.bufferSize, 0, 1);
+	/**
+	 *  Noise generator. 
+	 *
+	 *  uses looped noise buffers to save on performance. 
+	 *
+	 *  @constructor
+	 *  @extends {Tone.Source}
+	 *  @param {string} type the noise type (white|pink|brown)
+	 */
+	Tone.Noise = function(type){
 
-    	//connections
-        this.jsNode.connect(this.output);
+		Tone.Source.call(this);
 
-    	this.setType(this.defaultArg(type, "white"));
-    }
+		/**
+		 *  @private
+		 *  @type {AudioBufferSourceNode}
+		 */
+		this._source = null;
+		
+		/**
+		 *  the buffer
+		 *  @private
+		 *  @type {AudioBuffer}
+		 */
+		this._buffer = null;
 
-    Tone.extend(Tone.Noise, Tone);
+		/**
+		 *  set a callback function to invoke when the sample is over
+		 *  
+		 *  @type {function}
+		 */
+		this.onended = function(){};
 
-    //@param {string} type ('white', 'pink', 'brown')
-    Tone.Noise.prototype.setType = function(type){
-    	switch (type){
-    		case "white" : 
-    			this.jsNode.onaudioprocess = this._whiteNoise.bind(this);
-    			break;
-    		case "pink" : 
-    			this.jsNode.onaudioprocess = this._pinkNoise.bind(this);
-    			break;
-    		case "brown" : 
-    			this.jsNode.onaudioprocess = this._brownNoise.bind(this);
-    			break;
-    		default : 
-    			this.jsNode.onaudioprocess = this._whiteNoise.bind(this);
-    	}
-    }
+		this.setType(this.defaultArg(type, "white"));
+	};
 
-    //modified from http://noisehack.com/generate-noise-web-audio-api/
-    Tone.Noise.prototype._pinkNoise = (function() {
-        var b0, b1, b2, b3, b4, b5, b6;
-        b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
-        return function(e) {
-            var bufferSize = this.jsNode.bufferSize;
-            var output = e.outputBuffer.getChannelData(0);
-            for (var i = 0; i < bufferSize; i++) {
-                var white = Math.random() * 2 - 1;
-                b0 = 0.99886 * b0 + white * 0.0555179;
-                b1 = 0.99332 * b1 + white * 0.0750759;
-                b2 = 0.96900 * b2 + white * 0.1538520;
-                b3 = 0.86650 * b3 + white * 0.3104856;
-                b4 = 0.55000 * b4 + white * 0.5329522;
-                b5 = -0.7616 * b5 - white * 0.0168980;
-                output[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
-                output[i] *= 0.11; // (roughly) compensate for gain
-                b6 = white * 0.115926;
-            }
-        }
-    })();
+	Tone.extend(Tone.Noise, Tone.Source);
 
-    //modified from http://noisehack.com/generate-noise-web-audio-api/
-    Tone.Noise.prototype._brownNoise = (function() {
-        var lastOut = 0.0;
-        return function(e) {
-            var bufferSize = this.jsNode.bufferSize;
-            var output = e.outputBuffer.getChannelData(0);
-            for (var i = 0; i < bufferSize; i++) {
-                var white = Math.random() * 2 - 1;
-                output[i] = (lastOut + (0.02 * white)) / 1.02;
-                lastOut = output[i];
-                output[i] *= 3.5; // (roughly) compensate for gain
-            }
-        }
-        return node;
-    })();
+	/**
+	 *  set the noise type
+	 *  
+	 *  @param {string} type the noise type (white|pink|brown)
+	 *  @param {Tone.Time} time (optional) time that the set will occur
+	 */
+	Tone.Noise.prototype.setType = function(type, time){
+		switch (type){
+			case "white" : 
+				this._buffer = _whiteNoise;
+				break;
+			case "pink" : 
+				this._buffer = _pinkNoise;
+				break;
+			case "brown" : 
+				this._buffer = _brownNoise;
+				break;
+			default : 
+				this._buffer = _whiteNoise;
+		}
+		//if it's playing, stop and restart it
+		if (this.state === Tone.Source.State.STARTED){
+			time = this.toSeconds(time);
+			//remove the listener
+			this._source.onended = undefined;
+			this._stop(time);
+			this._start(time);
+		}
+	};
 
-    //modified from http://noisehack.com/generate-noise-web-audio-api/
-    Tone.Noise.prototype._whiteNoise = function(e){
-        var bufferSize = this.jsNode.bufferSize;
-        var output = e.outputBuffer.getChannelData(0);
-        for (var i = 0; i < bufferSize; i++) {
-            output[i] = Math.random() * 2 - 1;
-        }
-    }
+	/**
+	 *  internal start method
+	 *  
+	 *  @type {Tone.Time} time
+	 *  @private
+	 */
+	Tone.Noise.prototype._start = function(time){		
+		this._source = this.context.createBufferSource();
+		this._source.buffer = this._buffer;
+		this._source.loop = true;
+		this._source.start(this.toSeconds(time));
+		this.chain(this._source, this.output);
+		this._source.onended = this._onended.bind(this);
+	};
 
-    return Tone.Noise;
+	/**
+	 *  start the noise at a specific time
+	 *  
+	 *  @type {Tone.Time} time
+	 */
+	Tone.Noise.prototype.start = function(time){
+		if (this.state === Tone.Source.State.STOPPED){
+			this.state = Tone.Source.State.STARTED;
+			//make the source
+			this._start(time);
+		}
+	};
+
+	/**
+	 *  internal stop method
+	 *  
+	 *  @type {Tone.Time} time
+	 *  @private
+	 */
+	Tone.Noise.prototype._stop = function(time){
+		this._source.stop(this.toSeconds(time));
+	};
+
+
+	/**
+	 *  stop the noise at a specific time
+	 *  
+	 *  @type {Tone.Time} time
+	 */
+	Tone.Noise.prototype.stop = function(time){
+		if (this.state === Tone.Source.State.STARTED) {
+			if (this._buffer && this._source){
+				if (!time){
+					this.state = Tone.Source.State.STOPPED;
+				}
+				this._stop(time);
+			}
+		}
+	};
+
+	/**
+	 *  internal call when the buffer is done playing
+	 *  
+	 *  @private
+	 */
+	Tone.Noise.prototype._onended = function(){
+		this.state = Tone.Source.State.STOPPED;
+		this.onended();
+	};
+
+	/**
+	 *  dispose all the components
+	 */
+	Tone.Noise.prototype.dispose = function(){
+		if (this._source !== null){
+			this._source.disconnect();
+			this._source = null;
+		}
+		this._buffer = null;
+		this.output.disconnect();
+		this.output = null;
+	};
+
+
+	///////////////////////////////////////////////////////////////////////////
+	// THE BUFFERS
+	// borred heavily from http://noisehack.com/generate-noise-web-audio-api/
+	///////////////////////////////////////////////////////////////////////////
+
+	/**
+	 *	static brown noise buffer
+	 *  
+	 *  @static
+	 *  @private
+	 *  @type {AudioBuffer}
+	 */
+	var _pinkNoise = (function() {
+		var buffer = Tone.context.createBuffer(2, bufferLength, sampleRate);
+		for (var channelNum = 0; channelNum < buffer.numberOfChannels; channelNum++){
+			var channel = buffer.getChannelData(channelNum);
+			var b0, b1, b2, b3, b4, b5, b6;
+			b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
+			for (var i = 0; i < bufferLength; i++) {
+				var white = Math.random() * 2 - 1;
+				b0 = 0.99886 * b0 + white * 0.0555179;
+				b1 = 0.99332 * b1 + white * 0.0750759;
+				b2 = 0.96900 * b2 + white * 0.1538520;
+				b3 = 0.86650 * b3 + white * 0.3104856;
+				b4 = 0.55000 * b4 + white * 0.5329522;
+				b5 = -0.7616 * b5 - white * 0.0168980;
+				channel[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
+				channel[i] *= 0.11; // (roughly) compensate for gain
+				b6 = white * 0.115926;
+			}
+		}
+		return buffer;
+	}());
+
+	/**
+	 *	static brown noise buffer
+	 *  
+	 *  @static
+	 *  @private
+	 *  @type {AudioBuffer}
+	 */
+	var _brownNoise = (function() {
+		var buffer = Tone.context.createBuffer(2, bufferLength, sampleRate);
+		for (var channelNum = 0; channelNum < buffer.numberOfChannels; channelNum++){
+			var channel = buffer.getChannelData(channelNum);
+			var lastOut = 0.0;
+			for (var i = 0; i < bufferLength; i++) {
+				var white = Math.random() * 2 - 1;
+				channel[i] = (lastOut + (0.02 * white)) / 1.02;
+				lastOut = channel[i];
+				channel[i] *= 3.5; // (roughly) compensate for gain
+			}
+		}
+		return buffer;
+	})();
+
+	/**
+	 *	static white noise buffer
+	 *  
+	 *  @static
+	 *  @private
+	 *  @type {AudioBuffer}
+	 */
+	var _whiteNoise = (function(){
+		var buffer = Tone.context.createBuffer(2, bufferLength, sampleRate);
+		for (var channelNum = 0; channelNum < buffer.numberOfChannels; channelNum++){
+			var channel = buffer.getChannelData(channelNum);
+			for (var i = 0; i < bufferLength; i++){
+				channel[i] =  Math.random() * 2 - 1;
+			}
+		}
+		return buffer;
+	}());
+
+	return Tone.Noise;
 });
--- test/tests/Sources.js
+++ test/tests/Sources.js
@@ -1,5 +1,5 @@
-define(["chai", "Tone/source/Player", "Tone/core/Master", "Tone/source/Oscillator", "Tone/component/Recorder", "tests/WebAudio"], 
-function(chai, Player, Master, Oscillator, Recorder){
+define(["chai", "Tone/source/Player", "Tone/core/Master", "Tone/source/Oscillator", "Tone/component/Recorder", "Tone/source/Noise", "tests/WebAudio"], 
+function(chai, Player, Master, Oscillator, Recorder, Noise){
 
 	var expect = chai.expect;
 
@@ -12,6 +12,11 @@ function(chai, Player, Master, Oscillator, Recorder){
 		
 		after(function(){
 			player.dispose();
+		});
+
+		it("can be created and disposed", function(){
+			var p = new Player();
+			p.dispose();
 		});
 
 		it("loads a file", function(done){
@@ -65,6 +70,11 @@ function(chai, Player, Master, Oscillator, Recorder){
 			oscillator.dispose();
 		});
 
+		it("can be created and disposed", function(){
+			var o = new Oscillator();
+			o.dispose();
+		});
+
 		it("starts and stops", function(done){
 			expect(oscillator.state).to.equal("stopped");
 			oscillator.start();
@@ -105,7 +115,7 @@ function(chai, Player, Master, Oscillator, Recorder){
 				var buffer = recorder.getFloat32Array()[0];
 				for (var i = 0; i < buffer.length; i++){
 					if (buffer[i] !== 0){
-						expect(oscillator.samplesToSeconds(i)).to.be.closeTo(0.05, 0.001);
+						expect(oscillator.samplesToSeconds(i)).to.be.closeTo(0.05, 0.01);
 						break;
 					}
 				}
@@ -126,6 +136,82 @@ function(chai, Player, Master, Oscillator, Recorder){
 		});
 	});
 
-	
+	describe("Tone.Noise", function(){
+		this.timeout(1000);
+
+		var noise = new Noise();
+		noise.toMaster();
+
+		after(function(){
+			noise.dispose();
+		});
+
+		it("can be created and disposed", function(){
+			var n = new Noise();
+			n.dispose();
+		});
+
+		it("starts and stops", function(done){
+			expect(noise.state).to.equal("stopped");
+			noise.start();
+			expect(noise.state).to.equal("started");
+			setTimeout(function(){
+				noise.stop();
+				done();
+			}, 100);
+		});
+
+		it("can be scheduled to stop", function(done){
+			expect(noise.state).to.equal("stopped");
+			noise.start();
+			expect(noise.state).to.equal("started");
+			noise.stop("+0.05");
+			setTimeout(function(){
+				expect(noise.state).to.equal("stopped");
+				done();
+			}, 100);
+		});
+
+		it("won't start again before stopping", function(){
+			expect(noise.state).to.equal("stopped");
+			noise.start();
+			noise.start();
+			noise.stop();
+			noise.stop();
+			expect(noise.state).to.equal("stopped");
+		});
+
+		it("be scheduled to start in the future", function(done){
+			var recorder = new Recorder();
+			noise.connect(recorder);
+			recorder.record(0.1);
+			noise.start("+.05");
+			setTimeout(function(){
+				noise.stop();
+				var buffer = recorder.getFloat32Array()[0];
+				for (var i = 0; i < buffer.length; i++){
+					if (buffer[i] !== 0){
+						expect(noise.samplesToSeconds(i)).to.be.closeTo(0.05, 0.001);
+						break;
+					}
+				}
+				done();
+			}, 200);
+		});
+
+		it("can set the noise types", function(){
+			noise.setType("brown");
+			noise.setType("white");
+			noise.setType("pink");
+			//even after started
+			noise.start();
+			noise.setType("brown");
+			noise.setType("white");
+			noise.setType("pink");
+			noise.stop();
+		});
+
+
+	});
 
 });
