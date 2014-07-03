67a1f7e76a73e961cbde0037f9b3bd1c4255840b
Yotam Mann <yotammann@gmail.com>
2014-07-02 23:36:59 -0400

    absolute value of any input signal

--- Tone/signal/Abs.js
+++ Tone/signal/Abs.js
@@ -1,48 +1,76 @@
-define(["Tone/core/Tone"], function(Tone){
+define(["Tone/core/Tone", "Tone/signal/Threshold", "Tone/signal/Negate", "Tone/signal/EqualZero"], function(Tone){
 
 	/**
-	 *  return the absolute value of an incoming signal between -1 and 1
+	 *  return the absolute value of an incoming signal
 	 *
 	 *  @constructor
 	 *  @extends {Tone}
 	 *  @param {number} value
 	 */
 	Tone.Abs = function(value){
+		Tone.call(this);
+
 		/**
-		 *  @type {WaveShaperNode}
+		 *  @type {Tone.Threshold}
 		 *  @private
 		 */
-		this._abs = this.context.createWaveShaper();
+		this._thresh = new Tone.Threshold(0);
+		
 		/**
-		 *  @type {WaveShaperNode}
+		 *  @type {Tone.Negate}
+		 *  @private
 		 */
-		this.input = this.output = this._abs;
+		this._negate = new Tone.Negate();
 
-		this._setAbsCurve();
+		/**
+		 *  @type {Tone.EqualZero}
+		 *  @private
+		 */
+		this._not = new Tone.EqualZero();
+
+		/**
+		 *  @type {GainNode}
+		 *  @private
+		 */
+		this._positive = this.context.createGain();
+
+		/**
+		 *  @type {GainNode}
+		 *  @private
+		 */
+		this._negative = this.context.createGain();
+
+		this.input.connect(this._thresh);
+		//two routes, one positive, one negative
+		this.chain(this.input, this._positive, this.output);
+		this.chain(this.input, this._negate, this._negative, this.output);
+		//the switching logic
+		this._thresh.connect(this._positive.gain);
+		this._positive.gain.value = 0;
+		this.chain(this._thresh, this._not, this._negative.gain);
+		this._negative.gain.value = 0;
 	};
 
 	Tone.extend(Tone.Abs);
 
 	/**
-	 *  @param {number} thresh 
-	 *  @private
-	 */
-	Tone.Abs.prototype._setAbsCurve = function(){
-		var curveLength = 1024;
-		var curve = new Float32Array(curveLength);
-		for (var i = 0; i < curveLength; i++){
-			var normalized = (i / (curveLength - 1)) * 2 - 1;
-			curve[i] = val = Math.abs(normalized);
-		}
-		this._abs.curve = curve;
-	};
-
-	/**
 	 *  dispose method
 	 */
 	Tone.Abs.prototype.dispose = function(){
-		this._abs.disconnect();
-		this._abs = null;
+		this._thresh.dispose();
+		this._negate.dispose();
+		this._not.dispose();
+		this._positive.disconnect();
+		this._negative.disconnect();
+		this._input.disconnect();
+		this._output.disconnect();
+		this._thresh = null;
+		this._negate = null;
+		this._not = null;
+		this._positive = null;
+		this._negative = null;
+		this._input = null;
+		this._output = null;
 	}; 
 
 	return Tone.Abs;
