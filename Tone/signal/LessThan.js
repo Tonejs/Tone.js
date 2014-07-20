2ee294b278925eb5523a4555412ed376aff3c187
Yotam Mann <yotammann@gmail.com>
2014-07-20 18:17:01 -0400

    fixed logic.
    now nearly the same as greater than

--- Tone/signal/LessThan.js
+++ Tone/signal/LessThan.js
@@ -1,42 +1,90 @@
-define(["Tone/core/Tone", "Tone/signal/GreaterThan"], function(Tone){
+define(["Tone/core/Tone", "Tone/signal/Threshold", "Tone/signal/Add"], function(Tone){
 
 	/**
 	 *  @class  Output 1 if the signal is less than the value, otherwise outputs 0
 	 *  
 	 *  @constructor
 	 *  @extends {Tone}
-	 *  @param {number} value the value to compare the incoming signal to
+	 *  @param {number=} [value=0] the value to compare to the incoming signal
 	 */
 	Tone.LessThan = function(value){
 		/**
-		 *  
-		 *  @type {Tone.GreaterThan}
+		 *  @type {WaveShaperNode}
 		 *  @private
 		 */
-		this._gt = new Tone.GreaterThan(-value);
+		this._lt = this.context.createWaveShaper();
 
 		/**
-		 *  @type {Tone.GreaterThan}
+		 *  @type {Tone.Threshold}
+		 *  @private
 		 */
-		this.input = this.output = this._gt;
+		this._thresh = new Tone.Threshold(0.001);
+
+		/**
+		 *  subtract the value from the incoming signal
+		 *  
+		 *  @type {Tone.Add}
+		 *  @private
+		 */
+		this._adder = new Tone.Add(this.defaultArg(-value, 0));
+
+		/**
+	 	 *  alias for the adder
+		 *  @type {Tone.Add}
+		 */
+		this.input = this._adder;
+
+		/**
+		 *  alias for the thresh
+		 *  @type {Tone.Threshold}
+		 */
+		this.output = this._thresh;
+
+		//connect
+		this.chain(this._adder, this._lt, this._thresh);
+		//setup waveshaper
+		this._setLessThanZero();
 	};
 
 	Tone.extend(Tone.LessThan);
 
 	/**
+	 *  @private
+	 */
+	Tone.LessThan.prototype._setLessThanZero = function(){
+		var curveLength = 1024;
+		var curve = new Float32Array(curveLength);
+		for (var i = 0; i < curveLength; i++){
+			var normalized = (i / (curveLength)) * 2 - 1;
+			if (normalized < 0){
+				curve[i] = 1;
+			} else {
+				curve[i] = 0;
+			}
+		}
+		this._lt.curve = curve;
+	};
+
+	/**
+	 *  set the value to compare to
+	 *  
 	 *  @param {number} value
 	 */
 	Tone.LessThan.prototype.setValue = function(value){
-		this._gt.setValue(-value);
+		this._adder.setValue(-value);
 	};
 
 	/**
 	 *  dispose method
 	 */
 	Tone.LessThan.prototype.dispose = function(){
-		this._gt.dispose();
-		this._gt = null;
+		this._lt.disconnect();
+		this._adder.disconnect();
+		this._thresh.dispose();
+		this._lt = null;
+		this._adder = null;
+		this._thresh = null;
 	};
 
-	return Tone.GreaterThan;
+	return Tone.LessThan;
 });
