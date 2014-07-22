8c5610e5d618c173c93e5343a77d7ba03e7f78c4
Yotam Mann <yotammann@gmail.com>
2014-07-22 11:56:02 -0400

    fixed linting errors

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -2,21 +2,21 @@ module.exports = function(grunt) {
 
   	// Project configuration.
   	grunt.initConfig({
-  	  	//pkg: grunt.file.readJSON('package.json'),
+  	  	//pkg: grunt.file.readJSON("package.json"),
   	  	jsdoc : {
 	        src : {
-	            src: ['../Tone/**/*.js', '../README.md'], 
+	            src: ["../Tone/**/*.js", "../README.md"], 
 	            options: {
-	                destination: '../doc',
+	                destination: "../doc",
 	                template : "./doc_config/template",
             		configure : "./doc_config/template/jsdoc.conf.json",
             		private : false
 	            }
 	        },
 	        dist : {
-	            src: ['../Tone.js'], 
+	            src: ["../Tone.js"], 
 	            options: {
-	                destination: '../doc',
+	                destination: "../doc",
 	                template : "./doc_config/template",
             		configure : "./doc_config/template/jsdoc.conf.json",
             		private : false
@@ -36,30 +36,30 @@ module.exports = function(grunt) {
 	    concat: {
 		    dist: {
 		    	options: {
-		        	// Replace all 'use strict' statements in the code with a single one at the top
+		        	// Replace all "use strict" statements in the code with a single one at the top
 		        	banner: "require([",
-		        	separator: ',',
+		        	separator: ",",
 		        	process: function(src, filepath) {
-		        		// remove the '.js'. h@ckz0rs.
-		        		return '"' + filepath.substring(3,(filepath.length-3)) + '"';
+		        		// remove the ".js". h@ckz0rs.
+		        		return "\"" + filepath.substring(3,(filepath.length-3)) + "\"";
 		        	},
 		        	footer: "], function(){});",
 
 		      	},
 		      	files: {
-		        	'../main.js': ['../Tone/**/*.js'],
+		        	"../main.js": ["../Tone/**/*.js"],
 		      	}
 			},
 			removeRequireString: {
 				options: {
 					process: function(src, filepath) {
-		        		// remove the '.js'. h@ckz0rs.
+		        		// remove the ".js". h@ckz0rs.
 		        		var withoutRequire = src.substr(0, src.indexOf("require([") - 1);
 		        		return withoutRequire;
 		        	},
 		        },
 		        files: {
-		        	'../Tone.js': ['./Tone.js.tmp'],
+		        	"../Tone.js": ["./Tone.js.tmp"],
 		      	}
 			}
 		},
@@ -67,19 +67,19 @@ module.exports = function(grunt) {
 			options: {
 				force: true,
 			},
-			dist: ['../main.js','./Tone.js.tmp']
+			dist: ["../main.js","./Tone.js.tmp"]
 		}
 	});
 
   	// Load the plugin that provides the "uglify" task.
-  	grunt.loadNpmTasks('grunt-jsdoc');
-  	grunt.loadNpmTasks('grunt-contrib-requirejs');
-  	grunt.loadNpmTasks('grunt-contrib-concat');
-  	grunt.loadNpmTasks('grunt-contrib-clean');
+  	grunt.loadNpmTasks("grunt-jsdoc");
+  	grunt.loadNpmTasks("grunt-contrib-requirejs");
+  	grunt.loadNpmTasks("grunt-contrib-concat");
+  	grunt.loadNpmTasks("grunt-contrib-clean");
   	
   	// Default task(s).
-  	grunt.registerTask('src', ['jsdoc:src']);
-  	grunt.registerTask('dist', ['concat:dist','requirejs:compile','concat:removeRequireString','clean:dist','jsdoc:dist']);
-  	grunt.registerTask('build', ['concat:dist','requirejs:compile','concat:removeRequireString','clean:dist']);
+  	grunt.registerTask("src", ["jsdoc:src"]);
+  	grunt.registerTask("dist", ["concat:dist","requirejs:compile","concat:removeRequireString","clean:dist","jsdoc:dist"]);
+  	grunt.registerTask("build", ["concat:dist","requirejs:compile","concat:removeRequireString","clean:dist"]);
   	
 };
