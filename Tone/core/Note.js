5f383d1f0390906df26fbcef8dad1ed6cfa6fe75
Yotam Mann <yotammann@gmail.com>
2014-08-20 16:51:56 -0400

    Tone.Note registers note events
    makes it easy to write JSON scores for multiple instruments.

---
+++ Tone/core/Note.js
@@ -0,0 +1,139 @@
+define(["Tone/core/Tone", "Tone/core/Transport"], function(Tone){
+
+	/**
+	 *  @class  A timed note. Creating a note will register a callback 
+	 *          which will be invoked on the channel at the time with
+	 *          whatever value was specified. 
+	 *
+	 *  @constructor
+	 *  @param {Tone.Time} time the time when the note will occur
+	 *  @param {*} value the value of the note
+	 *  @param {number|string} channel the channel name of the note
+	 */
+	Tone.Note = function(time, value, channel){
+
+		/**
+		 *  the value of the note. This value is returned
+		 *  when the channel callback is invoked.
+		 *  
+		 *  @type {*}
+		 */
+		this.value = value;
+
+		/**
+		 *  the channel name or number
+		 *  
+		 *  @type {string|number}
+		 *  @private
+		 */
+		this._channel = channel;
+
+		/**
+		 *  an internal reference to the id of the timeline
+		 *  callback which is set. 
+		 *  
+		 *  @type {number}
+		 *  @private
+		 */
+		this._timelineID = Tone.Transport.setTimeline(this._trigger.bind(this), time);
+	};
+
+	/**
+	 *  invoked by the timeline
+	 *  @private
+	 *  @param {number} time the time at which the note should play
+	 */
+	Tone.Note.prototype._trigger = function(){
+		//invoke the callback
+		ChannelCallbacks(this._channel, this.value);
+	};
+
+	/**
+	 *  clean up
+	 */
+	Tone.Note.prototype.dispose = function(){ 
+		Tone.Tranport.clearTimeline(this._timelineID);
+		this.value = null;
+	};
+
+	/**
+	 *  @private
+	 *  @static
+	 *  @type {Object}
+	 */
+	var NoteChannels = {};
+
+	/**
+	 *  invoke all of the callbacks on a specific channel
+	 *  @private
+	 *  @static
+	 */
+	function ChannelCallbacks(channel, note){
+		var callbacks = NoteChannels[channel];
+		if (callbacks){
+			for (var i = 0, len = callbacks.length; i < len; i++){
+				callbacks[i](note);
+			}
+		}
+	}
+
+	/**
+	 *  listen to a specific channel, get all of the note callbacks
+	 *  @static
+	 *  @param {string|number} channel the channel to route note events from
+	 *  @param {function(*)} callback callback to be invoked when a note will occur
+	 *                                        on the specified channel
+	 */
+	Tone.Note.route = function(channel, callback){
+		if (NoteChannels[channel]){
+			NoteChannels[channel].push(callback);
+		} else {
+			NoteChannels[channel] = [callback];
+		}
+	};
+
+	/**
+	 *  remove a callback from a channel
+	 *  @static
+	 */
+	Tone.Note.unroute = function(channel, callback){
+		var channelCallback = NoteChannels[channel];
+		if (channelCallback){
+			var index = channelCallback.indexOf(callback);
+			if (index !== -1){
+				NoteChannels[channel].splice(index, 1);
+			}
+		}
+	};
+
+	/**
+	 *  parses a score and registers all of the notes
+	 *
+	 *  scores are a JSON object with instruments at the top level
+	 *  and an array of time, value tuples
+	 *
+	 *  i.e. { 
+	 *  	"synth"  : [["0", "C3"], ["0:1", "D3"], ["0:2", "E3"], ... ],
+	 *  	"bass"  : [["0", "C2"], ["1:0", "A2"], ["2:0", "C2"], ["3:0", "A2"], ... ],
+	 *  	"drums"  : [["0", "kick"], ["0:2", "snare"], ["1:0", "kick"], ["1:2", "snare"], ... ],
+	 *  	...
+	 *  }
+	 *  @static
+	 *  @param {Object} score
+	 *  @return {Array<Tone.Note>} an array of all of the notes that were created
+	 */
+	Tone.Note.parseScore = function(score){
+		var notes = [];
+		for (var inst in score){
+			for (var i = 0; i < inst.length; i++){
+				var time = inst[i][0];
+				var value = inst[i][1];
+				var note = new Tone.Note(time, value, inst);
+				notes.push(note);
+			}
+		}
+		return notes;
+	};
+
+	return Tone.Note;
+});
