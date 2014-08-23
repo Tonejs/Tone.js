f6f0a35f5707ec3dca2c420155e85e144991cadf
Yotam Mann <yotammann@gmail.com>
2014-08-23 15:19:01 -0400

    started chorus effect

---
+++ Tone/effect/Chorus.js
@@ -0,0 +1,101 @@
+define(["Tone/core/Tone", "Tone/component/LFO", "Tone/effect/FeedbackEffect"], function(Tone){
+
+	/**
+	 *  @class A Chorus effect with feedback
+	 *
+	 *	@constructor
+	 *	@extends {Tone.FeedbackEffect}
+	 *	@param {number=} [rate=2] the rate of the effect
+	 *	@param {number=} [delay=3.5] the delay of the chorus effect in ms
+	 *	@param {number=} [depth=0.7] the depth of the chorus
+	 */
+	Tone.Chorus = function(rate, delay, depth){
+
+		Tone.FeedbackEffect.call(this);
+
+		/**
+		 *  the depth of the chorus
+		 *  @type {number}
+		 *  @private
+		 */
+		this._depth = this.defaultArg(depth, 0.7);
+
+		/**
+		 *  the delayTime
+		 *  @type {number}
+		 *  @private
+		 */
+		this._delayTime = this.defaultArg(delay, 0.0035);
+
+		/**
+		 *  the lfo which controls the delayTime
+		 *  @type {Tone.LFO}
+		 *  @private
+		 */
+		this._lfo = new Tone.LFO(this.defaultArg(rate, 2), 0, 1);
+
+		/**
+		 *  @type {DelayNode}
+		 *  @private
+		 */
+		this._delayNode = this.context.createDelay();
+
+		//connections
+		this.connectEffect(this._delayNode);
+		this.effectSend.connect(this.effectReturn);
+		this._lfo.connect(this._delayNode.delayTime);
+		this._lfo.start();
+		this.setDepth(this.defaultArg(depth, 0.5));
+		this.setFeedback(0.25);
+	};
+
+	Tone.extend(Tone.Chorus, Tone.FeedbackEffect);
+
+	/**
+	 *  set the depth of the chorus
+	 *  @param {number} depth
+	 */
+	Tone.Chorus.prototype.setDepth = function(depth){
+		this._depth = depth;
+		var deviation = this._delayTime * depth;
+		this._lfo.setMin(this._delayTime - deviation);
+		this._lfo.setMax(this._delayTime + deviation);
+	};
+
+	/**
+	 *  set the delay time
+	 *  @param {number} delayTime in milliseconds
+	 */
+	Tone.Chorus.prototype.setDelayTime = function(delayTime){
+		this._delayTime = delayTime / 1000;
+		this.setDepth(this._depth);
+	};
+
+	/**
+	 *  set the chorus rate
+	 *  @param {number} rate in hertz
+	 */
+	Tone.Chorus.prototype.setRate = function(rate){
+		this._lfo.setFrequency(rate);
+	};
+
+	/**
+	 *  pointer to the parents dispose method
+	 *  @private
+	 *  @function
+	 */
+	Tone.Chorus.prototype._feedbackEffectDispose = Tone.FeedbackEffect.prototype.dispose;
+
+	/**
+	 *  clean up
+	 */
+	Tone.Chorus.prototype.dispose = function(){
+		this._feedbackEffectDispose();
+		this._lfo.dispose();
+		this._delayNode.disconnect();
+		this._lfo = null;
+		this._delayNode = null;
+	};
+
+	return Tone.Chorus;
+});
