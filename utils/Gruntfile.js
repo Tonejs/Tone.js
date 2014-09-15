3fcfd72596b5707eb6954d9249a6a7bd6f1bc5f6
Yotam Mann <yotammann@gmail.com>
2014-09-15 16:22:01 -0400

    moved doc_config stuff into a vendors folder
    this is so that github doesn’t think my project is 66% css!

--- utils/Gruntfile.js
+++ utils/Gruntfile.js
@@ -8,8 +8,8 @@ module.exports = function(grunt) {
 				src: ["../Tone/**/*.js", "!../Tone/*/preset/*", "../README.md"], 
 				options: {
 					destination: "../doc",
-					template : "./doc_config/template",
-					configure : "./doc_config/template/jsdoc.conf.json",
+					template : "./doc_config/vendors",
+					configure : "./doc_config/vendors/jsdoc.conf.json",
 					private : false
 				}
 			},
