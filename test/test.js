4ffb83bd9ce45e097584baf1a92d50580c778c51
Yotam Mann <yotammann@gmail.com>
2014-10-13 17:20:32 -0400

    include new test files

--- test/test.js
+++ test/test.js
@@ -15,8 +15,9 @@ require.config({
 var maxTimeout = 1000;
 
 var allTests = ["tests/Core", "tests/Timing", "tests/Signal", "tests/SignalComparison", 
-"tests/SignalMath", "tests/Transport", "tests/Sources", "tests/Components", "tests/Effect", "tests/Instruments"];
-// var allTests = ["tests/Core", "tests/Signal"];
+"tests/SignalMath", "tests/Transport", "tests/Sources", "tests/Components", 
+"tests/Effect", "tests/Instruments", "tests/EffectPresets", "tests/InstrumentPresets"];
+// var allTests = ["tests/Core", "tests/Instruments"];
 
 require(allTests, function(){
 	mocha.run(); 
