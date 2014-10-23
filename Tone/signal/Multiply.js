fcba9bd62a0d2a53032a96ace61bee142bf103d4
Yotam Mann <yotammann@gmail.com>
2014-10-22 21:20:43 -0400

    multiply handles two signals

--- Tone/signal/Multiply.js
+++ Tone/signal/Multiply.js
@@ -3,23 +3,38 @@ define(["Tone/core/Tone", "Tone/signal/Signal"], function(Tone){
 	"use strict";
 
 	/**
-	 *  @class  Multiply the incoming signal by some factor
+	 *  @class  Multiply the incoming signal by a number or Multiply two signals
 	 *
 	 *  @constructor
 	 *  @extends {Tone}
 	 *  @param {number=} value constant value to multiple
 	 */
 	Tone.Multiply = function(value){
+
+		/**
+		 *  input 0: multiplicand
+		 *  input 1: multiplier
+		 *  @type {Array}
+		 */
+		this.input = new Array(2);
+
 		/**
 		 *  the input node is the same as the output node
 		 *  it is also the GainNode which handles the scaling of incoming signal
 		 *  
 		 *  @type {GainNode}
+		 *  @private
 		 */
-		this.input = this.output = this.context.createGain();
+		this._mult = this.input[0] = this.output = this.context.createGain();
+
+		/**
+		 *  the scaling parameter
+		 *  @type {AudioParam}
+		 *  @private
+		 */
+		this._factor = this.input[1] = this.output.gain;
 		
-		//apply the inital scale factor
-		this.input.gain.value = this.defaultArg(value, 1);
+		this._factor.value = this.defaultArg(value, 0);
 	};
 
 	Tone.extend(Tone.Multiply);
@@ -30,7 +45,7 @@ define(["Tone/core/Tone", "Tone/signal/Signal"], function(Tone){
 	 *  @param {number} value 
 	 */
 	Tone.Multiply.prototype.setValue = function(value){
-		this.input.gain.value = value;
+		this._factor.value = value;
 	};
 
 	/**
@@ -45,6 +60,8 @@ define(["Tone/core/Tone", "Tone/signal/Signal"], function(Tone){
 	 */
 	Tone.Multiply.prototype.dispose = function(){
 		Tone.prototype.dispose.call(this);
+		this._mult = null;
+		this._factor = null;
 	}; 
 
 	return Tone.Multiply;
