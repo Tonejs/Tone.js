e1d27c552543826fa3a063c2143f6101937bafb7
Yotam Mann <yotammann@gmail.com>
2014-10-23 11:42:41 -0400

    Convolver style and formatting

--- Tone/effect/Convolver.js
+++ Tone/effect/Convolver.js
@@ -1,113 +1,107 @@
-define(["Tone/core/Tone", "Tone/core/Buffer", "Tone/effect/StereoEffect"], function(Tone){
+define(["Tone/core/Tone", "Tone/core/Buffer", "Tone/effect/Effect"], function(Tone){
 
-  "use strict";
+	"use strict";
+
 	/**
-   *  @class  Convolver wrapper for reverb and emulation.
-   *  NB: currently, this class only supports 1 buffer member.
-   *  Future iterations will include a this.buffers collection for multi buffer mode.
-   *  @constructor
-   *  @extends {Tone.Effect}
-   *  @param {string|Object=} 
-   *  @param {function} callback function
-   */
-  Tone.Convolver = function(){
-
-  //get all of the defaults
-  var options = this.optionsObject(arguments, ["url", "onload"], Tone.Convolver.defaults);
-  //connections
-	Tone.Effect.call(this, options);
-
-  /**
-	 * convolver node
-	 * @type {[type]}
-	 * @private
+	 *  @class  Convolver wrapper for reverb and emulation.
+	 *          NB: currently, this class only supports 1 buffer member.
+	 *          Future iterations will include a this.buffers collection for multi buffer mode.
+	 *  
+	 *  @constructor
+	 *  @extends {Tone.Effect}
+	 *  @param {string|Object=} url
+	 *  @param {function} callback function
 	 */
-	this._convolver = this.context.createConvolver();
-	/**
-	 * convolution buffers
-	 * @type {Array} Array of ArrayBuffers
-	 * @private
-	 */
-	this._buffer = null;
+	Tone.Convolver = function(){
 
-	//if there is a url, load it. 
-	if (!this.isUndef(options.url)){
-	  this.load(options.url, options.onload);
-    //the connections
-  }
+		//get all of the defaults
+		var options = this.optionsObject(arguments, ["url", "onload"], Tone.Convolver.defaults);
+		//connections
+		Tone.Effect.call(this, options);
 
-  this.connectEffect(this._convolver);
-  };
+	  	/**
+		 *  convolver node
+		 *  @type {ConvolverNode}
+		 *  @private
+		 */
+		this._convolver = this.context.createConvolver();
 
-  Tone.extend(Tone.Convolver, Tone.Effect);
-  
-  /**
-   *  @static
-   *  @type {Object}
-  */
-  Tone.Convolver.defaults = {
-	"url": null,
-	"onload": null,
-  };
+		/**
+		 *  the convolution buffer
+		 *  
+		 *  @type {AudioBuffer}
+		 *  @private
+		 */
+		this._buffer = null;
 
-  /**
-   *  Load the impulse response url as an audio buffer.
-   *  Decodes the audio asynchronously and invokes
-   *  the callback once the audio buffer loads.
-   * @param {string} url the url of the buffer to load.
-   *        filetype support depends on the
-   *        browser.
-   * @param  {function(Tone.Convolver)=} callback
-   */
-  Tone.Convolver.prototype.load = function(url, callback){
-	var self = this;
-	if (!self._buffer){
-	  new Tone.Buffer({
-		"url"  : url,
-		"callback" :  function (buffer){
-		  self.setBuffer(buffer);
-		  if (callback){
-			callback(self);
-		  }
+		this.connectEffect(this._convolver);
+		//if there is a url, load it. 
+		if (!this.isUndef(options.url)){
+			this.load(options.url, options.onload);
 		}
-	  });
-	} else if (callback){
-	  callback(self);
-	}
-  };
+	};
 
-  /**
-   *  set the buffer
-   *
-   *  @param {AudioBuffer} buffer the buffer which the player will play.
-   *                              note: if you switch the buffer after
-   *                              the player is already started, it will not
-   *                              take effect until the next time the player
-   *                              is started.
-   */
-  Tone.Convolver.prototype.setBuffer = function(buffer){
-	this._buffer = buffer;
-	this._convolver.buffer = this._buffer;
-  };
-
-  /**
-   *  set multiple parameters at once with an object
-   *  @param {Object} params the parameters as an object
-   */
-  Tone.Convolver.prototype.set = function(params){
-	if (!this.isUndef(params.bypass)) this.setBypass(params.bypass);
-  };
+	Tone.extend(Tone.Convolver, Tone.Effect);
 
 	/**
-   *  dispose and disconnect
-   */
-  Tone.Convolver.prototype.dispose = function(){
-	Tone.StereoEffect.prototype.dispose.call(this);
-	if (this._source !== null) {
-	  this._source.disconnect();
-	  this._source = null;
-	}
-	this._buffer = null;
-  };
-  return Tone.Convolver;
+	 *  @static
+	 *  @type {Object}
+	 */
+	Tone.Convolver.defaults = {
+		"onload": function(){},
+	};
+
+	/**
+	 *  Load the impulse response url as an audio buffer.
+	 *  Decodes the audio asynchronously and invokes
+	 *  the callback once the audio buffer loads.
+	 *  @param {string} url the url of the buffer to load.
+	 *                      filetype support depends on the
+	 *                      browser.
+	 *  @param  {function(Tone.Convolver)=} callback
+	 */
+	Tone.Convolver.prototype.load = function(url, callback){
+		if (!this._buffer){
+			var self = this;
+			new Tone.Buffer(url, function (buffer){
+				self.setBuffer(buffer);
+				if (callback){
+					callback(self);
+				}
+			});
+		} else if (callback){
+			callback(this);
+		}
+	};
+
+	/**
+	 *  set the buffer
+	 *
+	 *  @param {AudioBuffer} buffer the impulse response
+	 */
+	Tone.Convolver.prototype.setBuffer = function(buffer){
+		this._buffer = buffer;
+		this._convolver.buffer = this._buffer;
+	};
+
+	/**
+	 *  set multiple parameters at once with an object
+	 *  @param {Object} params the parameters as an object
+	 */
+	Tone.Convolver.prototype.set = function(params){
+		if (!this.isUndef(params.buffer)) this.setBuffer(params.buffer);
+		Tone.Effect.prototype.set.call(this, params);
+	};
+
+	/**
+	 *  dispose and disconnect
+	 */
+	Tone.Convolver.prototype.dispose = function(){
+		Tone.Effect.prototype.dispose.call(this);
+		this._convolver.disconnect();
+		this._convolver = null;
+		this._buffer = null;
+	}; 
+
+	return Tone.Convolver;
 });
