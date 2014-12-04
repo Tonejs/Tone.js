809dc99091ba6a3a2d9b09f92c5923e3ddc80c75
Yotam Mann <yotammann@gmail.com>
2014-12-03 22:24:36 -0500

    no feedback in phaser

--- Tone/effect/preset/Phaser.preset.js
+++ Tone/effect/preset/Phaser.preset.js
@@ -12,21 +12,18 @@ define(["Tone/core/Tone", "Tone/effect/Phaser"], function(Tone){
 			"depth" : 0.2,
 			"Q" : 2,
 			"baseFrequency" : 700,
-			"feedback" : 0
 		},
 		"landing" : {
 			"rate" : 4,
 			"depth" : 1.2,
 			"Q" : 20,
 			"baseFrequency" : 800,
-			"feedback" : 0.9
 		},
 		"bubbles" : {
 			"rate" : 0.5,
 			"depth" : 5,
 			"Q" : 8,
 			"baseFrequency" : 250,
-			"feedback" : 0.5
 		}
 	};
 
