e0d7ac212b0f4e1562122b6a6ec74809ddbee2db
Yotam Mann <yotammann@gmail.com>
2015-03-24 16:29:48 -0400

    uses StereoPannerNode when available

--- Tone/component/Panner.js
+++ Tone/component/Panner.js
@@ -1,4 +1,5 @@
-define(["Tone/core/Tone", "Tone/component/CrossFade", "Tone/component/Merge", "Tone/component/Split"], 
+define(["Tone/core/Tone", "Tone/component/CrossFade", "Tone/component/Merge", 
+	"Tone/component/Split", "Tone/signal/Signal", "Tone/signal/WaveShaper"], 
 function(Tone){
 
 	"use strict";
@@ -19,41 +20,75 @@ function(Tone){
 	 */
 	Tone.Panner = function(initialPan){
 
-		Tone.call(this, 1, 0);
-		
-		/**
-		 *  the dry/wet knob
-		 *  @type {Tone.CrossFade}
-		 *  @private
-		 */
-		this._crossFade = new Tone.CrossFade();
-		
-		/**
-		 *  @type {Tone.Merge}
-		 *  @private
-		 */
-		this._merger = this.output = new Tone.Merge();
-		
-		/**
-		 *  @type {Tone.Split}
-		 *  @private
-		 */
-		this._splitter = new Tone.Split();
-		
-		/**
-		 *  the pan control
-		 *  @type {Tone.Signal}
-		 */	
-		this.pan = this._crossFade.fade;
+		Tone.call(this);
 
-		//CONNECTIONS:
-		this.input.connect(this._splitter);
-		//left channel is dry, right channel is wet
-		this._splitter.connect(this._crossFade, 0, 0);
-		this._splitter.connect(this._crossFade, 1, 1);
-		//merge it back together
-		this._crossFade.a.connect(this._merger.left);
-		this._crossFade.b.connect(this._merger.right);
+		/**
+		 *  indicates if the panner is using the new StereoPannerNode internally
+		 *  @type  {boolean}
+		 *  @private
+		 */
+		this._hasStereoPanner = this.isFunction(this.context.createStereoPanner);
+
+		if (this._hasStereoPanner){
+
+			/**
+			 *  the panner node
+			 *  @type {StereoPannerNode}
+			 *  @private
+			 */
+			this._panner = this.input = this.output = this.context.createStereoPanner();
+
+			/**
+			 *  the pan control
+			 *  @type {Tone.Signal}
+			 */	
+			this.pan = new Tone.Signal(0, Tone.Signal.Units.Normal);
+
+			/**
+			 *  scale the pan signal to between -1 and 1
+			 *  @type {Tone.WaveShaper}
+			 *  @private
+			 */
+			this._scalePan = new Tone.WaveShaper([-1, -1, 1]);
+
+			//connections
+			this.pan.chain(this._scalePan, this._panner.pan);
+			
+		} else {
+
+			/**
+			 *  the dry/wet knob
+			 *  @type {Tone.CrossFade}
+			 *  @private
+			 */
+			this._crossFade = new Tone.CrossFade();
+			
+			/**
+			 *  @type {Tone.Merge}
+			 *  @private
+			 */
+			this._merger = this.output = new Tone.Merge();
+			
+			/**
+			 *  @type {Tone.Split}
+			 *  @private
+			 */
+			this._splitter = this.input = new Tone.Split();
+			
+			/**
+			 *  the pan control
+			 *  @type {Tone.Signal}
+			 */	
+			this.pan = this._crossFade.fade;
+
+			//CONNECTIONS:
+			//left channel is a, right channel is b
+			this._splitter.connect(this._crossFade, 0, 0);
+			this._splitter.connect(this._crossFade, 1, 1);
+			//merge it back together
+			this._crossFade.a.connect(this._merger, 0, 0);
+			this._crossFade.b.connect(this._merger, 0, 1);
+		}
 
 		//initial value
 		this.pan.value = this.defaultArg(initialPan, 0.5);
@@ -67,13 +102,22 @@ function(Tone){
 	 */
 	Tone.Panner.prototype.dispose = function(){
 		Tone.prototype.dispose.call(this);
-		this._crossFade.dispose();
-		this._crossFade = null;
-		this._splitter.dispose();
-		this._splitter = null;
-		this._merger.dispose();
-		this._merger = null;
-		this.pan = null;
+		if (this._hasStereoPanner){
+			this._panner.disconnect();
+			this._panner = null;
+			this.pan.dispose();
+			this.pan = null;
+			this._scalePan.dispose();
+			this._scalePan = null;
+		} else {
+			this._crossFade.dispose();
+			this._crossFade = null;
+			this._splitter.dispose();
+			this._splitter = null;
+			this._merger.dispose();
+			this._merger = null;
+			this.pan = null;
+		}
 		return this;
 	};
 
