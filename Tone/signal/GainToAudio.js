da1d03b6167273a22b733ac49b98e4d3e42c3fa9
Yotam Mann <yotammann@gmail.com>
2015-04-05 12:13:03 -0400

    inverse of AudioToGain
    maps values in the gain range [0, 1] to the audio range [-1, 1]

---
+++ Tone/signal/GainToAudio.js
@@ -0,0 +1,38 @@
+define(["Tone/core/Tone", "Tone/signal/WaveShaper", "Tone/signal/Signal"], function(Tone){
+
+	"use strict";
+
+	/**
+	 *  @class Maps a gain value [0, 1] to an audio value [-1, 1]
+	 *
+	 *  @extends {Tone.SignalBase}
+	 *  @constructor
+	 *  @example
+	 *  var g2a = new Tone.GainToAudio();
+	 */
+	Tone.GainToAudio = function(){
+
+		/**
+		 *  @type {WaveShaperNode}
+		 *  @private
+		 */
+		this._norm = this.input = this.output = new Tone.WaveShaper(function(x){
+			return Math.abs(x) * 2 - 1;
+		});
+	};
+
+	Tone.extend(Tone.GainToAudio, Tone.SignalBase);
+
+	/**
+	 *  clean up
+	 *  @returns {Tone.GainToAudio} `this`
+	 */
+	Tone.GainToAudio.prototype.dispose = function(){
+		Tone.prototype.dispose.call(this);
+		this._norm.disconnect();
+		this._norm = null;
+		return this;
+	};
+
+	return Tone.GainToAudio;
+});
