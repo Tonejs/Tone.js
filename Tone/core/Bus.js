4b1f235781f3172aac38dd9297d69da32e8f9785
Yotam Mann <yotammann@gmail.com>
2015-06-14 01:24:12 -0400

    doc'ing bus

--- Tone/core/Bus.js
+++ Tone/core/Bus.js
@@ -19,10 +19,12 @@ define(["Tone/core/Tone"], function(Tone){
 
 	/**
 	 *  Send this signal to the channel name. 
-	 *  @param  {string} channelName 
+	 *  @param  {string} channelName A named channel to send the signal to.
 	 *  @param  {Decibels} amount The amount of the source to send to the bus. 
 	 *  @return {GainNode} The gain node which connects this node to the desired channel. 
 	 *                     Can be used to adjust the levels of the send.
+	 *  @example
+	 * source.send("reverb", -12);
 	 */
 	Tone.prototype.send = function(channelName, amount){
 		if (!Buses.hasOwnProperty(channelName)){
@@ -37,11 +39,13 @@ define(["Tone/core/Tone"], function(Tone){
 	/**
 	 *  Recieve the input from the desired channelName to the input
 	 *
-	 *  @param  {string} channelName 
+	 *  @param  {string} channelName A named channel to send the signal to.
 	 *  @param {AudioNode} [input] If no input is selected, the
 	 *                                         input of the current node is
 	 *                                         chosen. 
 	 *  @returns {Tone} this
+	 *  @example
+	 * reverbEffect.receive("reverb");
 	 */
 	Tone.prototype.receive = function(channelName, input){
 		if (!Buses.hasOwnProperty(channelName)){
