b71e577d4a2bcd4365643613ba8419d05865c86e
Yotam Mann <yotammann@gmail.com>
2015-08-01 00:59:30 -0400

    removing old note dependency

--- Tone/instrument/Instrument.js
+++ Tone/instrument/Instrument.js
@@ -1,4 +1,4 @@
-define(["Tone/core/Tone", "Tone/core/Master", "Tone/core/Note", "Tone/core/Types"], function(Tone){
+define(["Tone/core/Tone", "Tone/core/Master", "Tone/core/Types"], function(Tone){
 
 	"use strict";
 
