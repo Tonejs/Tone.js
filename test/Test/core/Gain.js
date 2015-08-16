7272269964f2b62d5a8c6f8b93c1ad6e927e0bfd
Yotam Mann <yotammann@gmail.com>
2015-08-16 14:23:40 -0400

    started test refactor

---
+++ test/Test/core/Gain.js
@@ -0,0 +1,59 @@
+define(["Test", "Tone/core/Gain", "Tone/core/Tone", "Offline", "PassAudio"], 
+	function (Test, Gain, Tone, Offline, PassAudio) {
+
+	describe("Gain", function(){
+
+		it ("can be created and disposed", function(){
+			var gain = new Gain();
+			gain.dispose();
+			Test.wasDisposed(gain);
+		});
+
+		it("handles input and output connections", function(){
+			var gain = new Gain();
+			gain.connect(Test);
+			Test.connect(gain);
+			Test.connect(gain.gain);
+			gain.dispose();
+		});
+
+		it("can set the gain value", function(){
+			var gain = new Gain();
+			gain.gain.value = 0.2;
+			expect(gain.gain.value).to.be.closeTo(0.2, 0.001);
+			gain.dispose();
+		});
+
+		it("can be constructed with options object", function(){
+			var gain = new Gain({
+				"gain" : 0.4
+			});
+			expect(gain.gain.value).to.be.closeTo(0.4, 0.001);
+			gain.dispose();
+		});
+
+		it("can be constructed with an initial value", function(){
+			var gain = new Gain(3);
+			expect(gain.gain.value).to.be.closeTo(3, 0.001);
+			gain.dispose();
+		});
+
+		it("can get the value using 'get'", function(){
+			var gain = new Gain(5);
+			var value = gain.get();
+			expect(value.gain).to.be.closeTo(5, 0.001);
+			gain.dispose();
+		});
+
+		it ("passes audio through", function(done){
+			var gain;
+			PassAudio(function(input, output){
+				gain = new Gain();
+				input.chain(gain, output);
+			}, function(){
+				gain.dispose();
+				done();
+			});
+		});		
+	});
+});
--- test/Test/core/Tone.js
+++ test/Test/core/Tone.js
@@ -1,11 +1,8 @@
-/* global it, describe, after */
-
-define(["chai", "Tone/core/Tone", "Tone/core/Master", "Tone/core/Bus", 
-	"Tone/core/Note", "tests/Common", "Tone/core/Buffer", "Tone/source/Oscillator", "Tone/instrument/SimpleSynth"], 
-function(chai, Tone, Master, Bus, Note, Test, Buffer, Oscillator, SimpleSynth){
-	var expect = chai.expect;
+define(["Test", "Tone/core/Tone", "PassAudio", "Tone/source/Oscillator", "Tone/instrument/SimpleSynth", "Offline"], 
+	function (Test, Tone, PassAudio, Oscillator, SimpleSynth, Offline) {
 
 	describe("AudioContext", function(){
+		
 		this.timeout(3000);
 
 		it ("was created", function(){
@@ -25,12 +22,30 @@ function(chai, Tone, Master, Bus, Note, Test, Buffer, Oscillator, SimpleSynth){
 			}, 20);
 		});
 
-		it ("has current API", function(){
+		it ("has shimmed API", function(){
 			expect(OscillatorNode.prototype.start).to.be.instanceof(Function);
 			expect(AudioBufferSourceNode.prototype.start).to.be.instanceof(Function);
 			expect(AudioContext.prototype.createGain).to.be.instanceof(Function);
 		});
 
+	});
+
+	describe("Tone.setContext", function(){
+
+		it ("can set a new context", function(){
+			var origCtx = Tone.context;
+			var ctx = new OfflineAudioContext(2, 44100, 44100);
+			Tone.setContext(ctx);
+			expect(Tone.context).to.equal(ctx);
+			expect(Tone.prototype.context).to.equal(ctx);
+			//then set it back
+			Tone.setContext(origCtx);
+			expect(Tone.context).to.equal(origCtx);
+			expect(Tone.prototype.context).to.equal(origCtx);
+			//and a saftey check
+			expect(ctx).to.not.equal(origCtx);
+		});
+		
 	});
 
 	describe("Tone", function(){
@@ -45,12 +60,6 @@ function(chai, Tone, Master, Bus, Note, Test, Buffer, Oscillator, SimpleSynth){
 			var t = new Tone();
 			t.dispose();
 			Test.wasDisposed(t);
-		});
-
-		it("correctly calculates samples to seconds", function(){
-			var sampleRate = tone.context.sampleRate;
-			expect(tone.samplesToSeconds(100)).to.equal(100/sampleRate);
-			expect(tone.samplesToSeconds(800)).to.equal(800/sampleRate);
 		});
 
 		it("can convert gain to db", function(){
@@ -92,7 +101,7 @@ function(chai, Tone, Master, Bus, Note, Test, Buffer, Oscillator, SimpleSynth){
 
 		it("can chain connections", function(done){
 			var node0, node1, node2;
-			Test.passesAudio(function(input, output){
+			PassAudio(function(input, output){
 				node0 = new Tone(1, 1);
 				//internal connection
 				node0.input.connect(node0.output);
@@ -111,7 +120,7 @@ function(chai, Tone, Master, Bus, Note, Test, Buffer, Oscillator, SimpleSynth){
 
 		it("can fan connections", function(done){
 			var node0, node1, node2;
-			Test.passesAudio(function(input, output){
+			PassAudio(function(input, output){
 				node0 = new Tone(1, 1);
 				//internal connection
 				node0.input.connect(node0.output);
@@ -150,39 +159,47 @@ function(chai, Tone, Master, Bus, Note, Test, Buffer, Oscillator, SimpleSynth){
 		it("ramps to a value given an object and ramp time", function(done){
 			var osc;
 			var setValue = 30;
-			Test.offlineTest(0.6, function(dest){
+			var offline = new Offline(0.6);
+			offline.before(function(dest){
 				osc = new Oscillator(0);
 				osc.frequency.connect(dest);
 				osc.set({
 					"frequency" : setValue
 				}, 0.5);
 				expect(osc.frequency.value).to.not.be.closeTo(setValue, 0.001);
-			}, function(sample, time){
+			});
+			offline.test(function(sample, time){
 				if (time > 0.5){
 					expect(sample).to.closeTo(setValue, 0.01);
 				}
-			}, function(){
+			});
+			offline.after(function(){
 				osc.dispose();
 				done();
 			});
+			offline.run();
 		});		
 
 		it("ramps to a value given a string and a value and a ramp time", function(done){
 			var osc;
 			var setValue = 30;
-			Test.offlineTest(0.6, function(dest){
+			var offline = new Offline(0.6);
+			offline.before(function(dest){
 				osc = new Oscillator(0);
 				osc.frequency.connect(dest);
 				osc.set("frequency", setValue, 0.5);
 				expect(osc.frequency.value).to.not.be.closeTo(setValue, 0.001);
-			}, function(sample, time){
+			});
+			offline.test(function(sample, time){
 				if (time > 0.5){
 					expect(sample).to.closeTo(setValue, 0.01);
 				}
-			}, function(){
+			});
+			offline.after(function(){
 				osc.dispose();
 				done();
 			});
+			offline.run();
 		});		
 
 		it("gets all defaults of the object with no arguments", function(){
@@ -236,159 +253,4 @@ function(chai, Tone, Master, Bus, Note, Test, Buffer, Oscillator, SimpleSynth){
 		});	
 
 	});
-
-	describe("Tone.Note", function(){
-
-		var tone = new Tone();
-
-		after(function(){
-			tone.dispose();
-		});
-
-		it("can convert notes into frequencies", function(){
-			expect(tone.noteToFrequency("A4")).to.be.closeTo(440, 0.0001);
-			expect(tone.noteToFrequency("Bb4")).to.be.closeTo(466.163761, 0.0001);
-		});
-
-		it("can convert frequencies into notes", function(){
-			expect(tone.frequencyToNote(440)).to.equal("A4");
-			expect(tone.frequencyToNote(4978.031739553295)).to.equal("D#8");
-		});
-
-		it("can convert note to midi values", function(){
-			expect(tone.midiToNote(60)).to.equal("C3");
-			expect(tone.midiToNote(61)).to.equal("C#3");
-		});
-
-		it("can convert midi values to note names", function(){
-			expect(tone.noteToMidi("C3")).to.equal(60);
-			expect(tone.noteToMidi("Bb2")).to.equal(58);
-			expect(tone.noteToMidi("A#2")).to.equal(58);
-		});
-
-		it("can convert semitone intervals to frequency ratios", function(){
-			expect(tone.intervalToFrequencyRatio(0)).to.equal(1);
-			expect(tone.intervalToFrequencyRatio(12)).to.equal(2);
-			expect(tone.intervalToFrequencyRatio(7)).to.be.closeTo(1.5, 0.01);
-		});
-
-		it("can convert different representations into frequencies", function(){
-			expect(tone.toFrequency("4n")).to.equal(2);
-			expect(tone.toFrequency("4hz")).to.equal(4);
-			expect(tone.toFrequency("A4")).to.be.closeTo(440, 0.001);
-			expect(tone.toFrequency(990)).to.equal(990);
-		});
-	});
-
-	describe("Tone.Master", function(){
-		it ("exists", function(){
-			expect(Tone.Master).to.exist;
-		});
-
-		it ("provides a toMaster method", function(){
-			expect(Tone.prototype.toMaster).is.a("function");
-		});
-	});
-
-	describe("Tone.Bus", function(){
-		it ("provides a send and receive method", function(){
-			expect(Tone.prototype.send).is.a("function");
-			expect(Tone.prototype.receive).is.a("function");
-		});
-
-		it ("passes audio from a send to a receive with the same name", function(done){
-			var send, recv;
-			Test.passesAudio(function(input, output){
-				//make them pass through nodes
-				send = new Tone();
-				recv = new Tone();
-				send.input.connect(send.output);
-				recv.input.connect(recv.output);
-				input.connect(send);
-				recv.connect(output);
-				send.send("test");
-				recv.receive("test");
-			}, function(){
-				send.dispose();
-				recv.dispose();
-				done();
-			});
-		});		
-	});
-
-	describe("Tone.Buffer", function(){
-		it ("can be created and disposed", function(){
-			var buff = new Tone.Buffer("./testAudio/kick.mp3");
-			buff.dispose();
-			Test.wasDisposed(buff);
-		});
-
-		it("loads a file from a url string", function(done){
-			var buffer = new Buffer("./testAudio/kick.mp3", function(buff){
-				expect(buff).to.be.instanceof(Buffer);
-				buffer.dispose();
-				done();
-			});
-		});
-
-		it("has a duration", function(done){
-			var buffer = new Buffer("./testAudio/kick.mp3", function(){
-				expect(buffer.duration).to.be.closeTo(0.23, 0.01);
-				buffer.dispose();
-				done();
-			});
-		});
-
-		it("the static onload method is invoked", function(done){
-			var buffer = new Buffer("./testAudio/hh.mp3");
-			Buffer.onload = function(){
-				buffer.dispose();
-				done();
-				//reset this method for the next one
-				Buffer.onload = function(){};
-			};
-		});
-
-		it("the static onprogress method is invoked", function(done){
-			var progressWasInvoked = false;
-			var buffer = new Buffer("./testAudio/hh.mp3", function(){
-				buffer.dispose();
-				expect(progressWasInvoked).to.be.true;
-				done();
-			});
-			Buffer.onprogress = function(){
-				progressWasInvoked = true;
-				//reset this method for the next one
-				Buffer.onprogress = function(){};
-			};
-		});
-
-		it("can reverse a buffer", function(done){
-			var buffer = new Buffer("./testAudio/kick.mp3", function(){
-				var buffArray = buffer.get();
-				var lastSample = buffArray[buffArray.length - 1];
-				buffer.reverse = true;
-				expect(buffer.get()[0]).to.equal(lastSample);
-				buffer.dispose();
-				done();
-			});
-		});
-	});
-
-	describe("Tone.setContext", function(){
-		it ("can set a new context", function(){
-			var origCtx = Tone.context;
-			var ctx = new OfflineAudioContext(2, 44100, 44100);
-			Tone.setContext(ctx);
-			expect(Tone.context).to.equal(ctx);
-			expect(Tone.prototype.context).to.equal(ctx);
-			//then set it back
-			Tone.setContext(origCtx);
-			expect(Tone.context).to.equal(origCtx);
-			expect(Tone.prototype.context).to.equal(origCtx);
-			//and a saftey check
-			expect(ctx).to.not.equal(origCtx);
-		});
-	});
-
 });
