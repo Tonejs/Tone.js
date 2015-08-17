57402b289cf3b8cd0907eb10be1f5072e87c0895
Yotam Mann <yotammann@gmail.com>
2015-08-16 22:21:42 -0400

    updated Schedulable API

--- Tone/core/Schedulable.js
+++ Tone/core/Schedulable.js
@@ -2,55 +2,96 @@ define(["Tone/core/Tone"], function (Tone) {
 
 	/**
 	 *  @class A Schedulable class has two private functions
-	 *         for scheduling and maintaining state: _setStateAtTime
-	 *         and _getStateAtTime.
+	 *         for scheduling and maintaining state: _insertEvent
+	 *         and _getEvent. A scheduled event is pushed onto
+	 *         a private _timeline array. The event must be an 
+	 *         Object with a 'time' attribute.
 	 *  @extends {Tone}
 	 */
 	Tone.Schedulable = function(){
 
 		Tone.apply(this, arguments);
 
+		/**
+		 *  The array of scheduled timeline events
+		 *  @type  {Array}
+		 *  @private
+		 */
 		this._timeline = [];
 	};
 
 	Tone.extend(Tone.Schedulable);
 
 	/**
-	 *  Set the state at the 
-	 *  @param  {[type]}  type  [description]
-	 *  @param  {[type]}  time  [description]
+	 *  Insert an event into the correct position in the timeline.
+	 *  @param  {Object}  event  The event object to insert into the 
+	 *                           timeline. Events must have a "time" attribute.
+	 *  @returns {Tone.Schedulable} this
+	 *  @private
 	 */
-	Tone.Schedulable.prototype._setStateAtTime = function(type, time){
-		var event = {
-			"type" : type, 
-			"time" : time
-		};
-		//put it in the right spot
-		for (var i = 0, len = this._timeline.length; i<len; i++){
-			var testEvnt = this._timeline[i];
-			if (testEvnt.time > event.time){
-				this._timeline.splice(i, 0, event);
-				return this;
-			}
+	Tone.Schedulable.prototype._insertEvent = function(event){
+		//the event needs to have a time attribute
+		if (this.isUndef(event.time)){
+			throw new Error("events must have a time attribute");
 		}
-		//otherwise push it on the end
-		this._timeline.push(event);
+		if (this._timeline.length){
+			var index = this._search(event.time);
+			this._timeline.splice(index + 1, 0, event);
+		} else {
+			this._timeline.push(event);			
+		}
 		return this;
 	};
 
-	Tone.Schedulable.prototype._getStateAtTime = function(time){
-		if (this._timeline.length > 0){
-			var currentState = this._timeline[0];
-			for (var i = 1; i < this._timeline.length; i++){
-				var event = this._timeline[i];
-				if (event.time <= time){
-					currentState = event;
-				} else {
-					return currentState.type;
-				}
-			}
-			return currentState.type;
+	/**
+	 *  Get the event at or after the given time
+	 *  @param  {Number}  time  The time to query.
+	 *  @returns {Object} The event object set after that time.
+	 *  @private
+	 */
+	Tone.Schedulable.prototype._getEvent = function(time){
+		var index = this._search(time);
+		if (index !== -1){
+			return this._timeline[index];
+		} else {
+			return null;
 		}
+	};
+
+	/**
+	 *  Does a binary serach on the timeline array and returns the 
+	 *  event which is after or equal to the time.
+	 *  @param  {Number}  time  
+	 *  @return  {Number} the index in the timeline array 
+	 *  @private
+	 */
+	Tone.Schedulable.prototype._search = function(time){
+		var beginning = 0;
+		var len = this._timeline.length;
+		var end = len;
+		// continue searching while [imin,imax] is not empty
+		while (beginning <= end && beginning < len){
+			// calculate the midpoint for roughly equal partition
+			var midPoint = Math.floor(beginning + (end - beginning) / 2);
+			var event = this._timeline[midPoint];
+			if (event.time === time){
+				//choose the last one that has the same time
+				for (var i = midPoint; i < this._timeline.length; i++){
+					var testEvent = this._timeline[i];
+					if (testEvent.time === time){
+						midPoint = i;
+					}
+				}
+				return midPoint;
+			} else if (event.time > time){
+				//search lower
+				end = midPoint - 1;
+			} else if (event.time < time){
+				//search upper
+				beginning = midPoint + 1;
+			} 
+		}
+		return beginning - 1;
 	};
 
 	/**
