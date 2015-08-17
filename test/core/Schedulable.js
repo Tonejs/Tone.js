4d49f01aaef2a07eb3ba7a77701275764d89f155
Yotam Mann <yotammann@gmail.com>
2015-08-17 01:03:00 -0400

    updated tests to new API

--- test/core/Schedulable.js
+++ test/core/Schedulable.js
@@ -15,15 +15,15 @@ define(["Test", "Tone/core/Schedulable"], function (Test, Schedulable) {
 
 		it ("accepts events into the timeline", function(){
 			var sched = new Schedulable();
-			sched._insertEvent({
+			sched.addEvent({
 				"state" : "A",
 				"time"  : 0
 			});
-			sched._insertEvent({
+			sched.addEvent({
 				"state" : "B",
 				"time"  : 1
 			});
-			sched._insertEvent({
+			sched.addEvent({
 				"state" : "C",
 				"time"  : 2
 			});
@@ -32,13 +32,13 @@ define(["Test", "Tone/core/Schedulable"], function (Test, Schedulable) {
 
 		it ("can insert events in the timeline in the right order", function(){
 			var sched = new Schedulable();
-			sched._insertEvent({
+			sched.addEvent({
 				"time"  : 0
 			});
-			sched._insertEvent({
+			sched.addEvent({
 				"time"  : 2
 			});
-			sched._insertEvent({
+			sched.addEvent({
 				"time"  : 1
 			});
 			expect(sched._timeline[0].time).to.equal(0);
@@ -49,13 +49,13 @@ define(["Test", "Tone/core/Schedulable"], function (Test, Schedulable) {
 
 		it ("can search for events in the timeline by time", function(){
 			var sched = new Schedulable();
-			sched._insertEvent({
+			sched.addEvent({
 				"time"  : 0
 			});
-			sched._insertEvent({
+			sched.addEvent({
 				"time"  : 2
 			});
-			sched._insertEvent({
+			sched.addEvent({
 				"time"  : 1
 			});
 			expect(sched._search(0)).to.equal(0);
@@ -69,42 +69,62 @@ define(["Test", "Tone/core/Schedulable"], function (Test, Schedulable) {
 
 		it ("can get the scheduled event at the given time", function(){
 			var sched = new Schedulable();
-			sched._insertEvent({
+			sched.addEvent({
 				"state" : "A",
 				"time"  : 2
 			});
-			sched._insertEvent({
+			sched.addEvent({
 				"state" : "C",
 				"time"  : 9.4
 			});
-			sched._insertEvent({
+			sched.addEvent({
 				"state" : "B",
 				"time"  : 6
 			});
-			expect(sched._getEvent(0)).is.null
-			expect(sched._getEvent(2).state).is.equal("A");
-			expect(sched._getEvent(5.9).state).is.equal("A");
-			expect(sched._getEvent(6.1).state).is.equal("B");
-			expect(sched._getEvent(12).state).is.equal("C");
+			expect(sched.getEvent(0)).is.null
+			expect(sched.getEvent(2).state).is.equal("A");
+			expect(sched.getEvent(5.9).state).is.equal("A");
+			expect(sched.getEvent(6.1).state).is.equal("B");
+			expect(sched.getEvent(12).state).is.equal("C");
 			sched.dispose();
 		});
 
 		it ("puts the second scheduled event after if two events are scheduled at the same time", function(){
 			var sched = new Schedulable();
-			sched._insertEvent({
+			sched.addEvent({
 				"name" : "A",
 				"time"  : 0
 			});
-			sched._insertEvent({
+			sched.addEvent({
 				"name" : "B",
 				"time"  : 0
 			});
-			expect(sched._getEvent(0).name).is.equal("B");
-			sched._insertEvent({
+			expect(sched.getEvent(0).name).is.equal("B");
+			sched.addEvent({
 				"name" : "C",
 				"time"  : 0
 			});
-			expect(sched._getEvent(0).name).is.equal("C");
+			expect(sched.getEvent(0).name).is.equal("C");
+			sched.dispose();
+		});
+
+		it ("can the next event after the given time", function(){
+			var sched = new Schedulable();
+			sched.addEvent({
+				"state" : "A",
+				"time"  : 0.1
+			});
+			sched.addEvent({
+				"state" : "B",
+				"time"  : 1.1
+			});
+			sched.addEvent({
+				"state" : "C",
+				"time"  : 2.1
+			});
+			expect(sched.getNextEvent(0).state).is.equal("A");
+			expect(sched.getNextEvent(1).state).is.equal("B");
+			expect(sched.getNextEvent(3)).is.null;
 			sched.dispose();
 		});
 	});
