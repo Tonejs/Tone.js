57578a482bedc1b31afa3530377873ffc7e00e0b
Yotam Mann <yotammann@gmail.com>
2015-08-24 17:28:23 -0400

    can pass in args to Basic Test

--- test/helper/Basic.js
+++ test/helper/Basic.js
@@ -1,17 +1,17 @@
 define(["Test", "Tone/core/Tone"], function (Test, Tone) {
 
-	return function(Constr){
+	return function(Constr, args){
 
 		context("Basic", function(){
 
 			it ("can be created and disposed", function(){
-				var instance = new Constr();
+				var instance = new Constr(args);
 				instance.dispose();
 				Test.wasDisposed(instance);
 			});
 
 			it ("extends Tone", function(){
-				var instance = new Constr();
+				var instance = new Constr(args);
 				expect(instance).to.be.an.instanceof(Tone);
 				instance.dispose();
 			});
