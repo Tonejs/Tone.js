14a780c1ca1a4ac672f2efc0889cf10dc5a9658b
Yotam Mann <yotammann@gmail.com>
2015-09-18 17:18:44 -0700

    bug fixes and compatibility tests

--- Tone/source/ExternalInput.js
+++ Tone/source/ExternalInput.js
@@ -1,6 +1,10 @@
-define(["Tone/core/Tone", "Tone/source/Source"], function(Tone){
+define(["Tone/core/Tone", "Tone/source/Source", "Tone/core/Gain"], function(Tone){
 
 	"use strict";
+
+	//polyfill for getUserMedia
+	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || 
+		navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
 	/**
 	 *  @class  Tone.ExternalInput is a WebRTC Audio Input. Check 
@@ -30,18 +34,21 @@ define(["Tone/core/Tone", "Tone/source/Source"], function(Tone){
 		Tone.Source.call(this, options);
 
 		/**
+		 *  The MediaStreamNode 
 		 *  @type {MediaStreamAudioSourceNode}
 		 *  @private
 		 */
 		this._mediaStream = null;
 		
 		/**
+		 *  The media stream created by getUserMedia.
 		 *  @type {LocalMediaStream}
 		 *  @private
 		 */
 		this._stream = null;
 		
 		/**
+		 *  The constraints argument for getUserMedia
 		 *  @type {Object}
 		 *  @private
 		 */
@@ -50,19 +57,18 @@ define(["Tone/core/Tone", "Tone/source/Source"], function(Tone){
 		/**
 		 *  The input source position in Tone.ExternalInput.sources. 
 		 *  Set before ExternalInput.open().
-		 *  @type {[type]}
+		 *  @type {Number}
+		 *  @private
 		 */
-		this.inputNum = options.inputNum;
+		this._inputNum = options.inputNum;
 
 		/**
-		 *  gates the input signal for start/stop
+		 *  Gates the input signal for start/stop. 
+		 *  Initially closed.
 		 *  @type {GainNode}
 		 *  @private
 		 */
-		this._gate = this.context.createGain();
-		this._gate.gain.value = 0;
-		this._gate.connect(this.output);
-
+		this._gate = new Tone.Gain(0).connect(this.output);
 	};
 
 	Tone.extend(Tone.ExternalInput, Tone.Source);
@@ -77,14 +83,20 @@ define(["Tone/core/Tone", "Tone/source/Source"], function(Tone){
 
 	/**
 	 * wrapper for getUserMedia function
+	 * @param {function} callback
 	 * @private
 	 */
 	Tone.ExternalInput.prototype._getUserMedia = function(callback){
-		this._constraints = {
-			audio : {
-				optional : [{sourceId: Tone.ExternalInput.sources[this.inputNum].id}]
-			}
-		};
+		if (!Tone.ExternalInput.canGetUserMedia){
+			throw new Error("browser does not support 'getUserMedia'");
+		}
+		if (Tone.ExternalInput.sources[this._inputNum]){
+			this._constraints = {
+				audio : {
+					optional : [{sourceId: Tone.ExternalInput.sources[this._inputNum].id}]
+				}
+			};
+		}
 		navigator.getUserMedia(this._constraints, function(stream){
 			this._onStream(stream);
 			callback();
@@ -94,12 +106,32 @@ define(["Tone/core/Tone", "Tone/source/Source"], function(Tone){
 	};
 
 	/**
+	 * called when the stream is successfully setup
+	 * @param  {LocalMediaStream} stream
+	 * @private
+	 */
+	Tone.ExternalInput.prototype._onStream = function(stream){
+		if (!this.isFunction(this.context.createMediaStreamSource)){
+			throw new Error("browser does not support the 'MediaStreamSourceNode'");
+		}
+		//can only start a new source if the previous one is closed
+		if (!this._stream){
+			this._stream = stream;
+			//Wrap a MediaStreamSourceNode around the live input stream.
+			this._mediaStream = this.context.createMediaStreamSource(stream);
+			//Connect the MediaStreamSourceNode to a gate gain node
+			this._mediaStream.connect(this._gate);
+		} 
+	};
+
+	/**
 	 *  Open the media stream 
-	 *  @param  {function =} callback The callback function to 
+	 *  @param  {function=} callback The callback function to 
 	 *                       execute when the stream is open
 	 *  @return {Tone.ExternalInput} this
 	 */
 	Tone.ExternalInput.prototype.open = function(callback){
+		callback = this.defaultArg(callback, Tone.noOp);
 		Tone.ExternalInput.getSources(function(){
 			this._getUserMedia(callback);
 		}.bind(this));
@@ -113,6 +145,7 @@ define(["Tone/core/Tone", "Tone/source/Source"], function(Tone){
 	Tone.ExternalInput.prototype.close = function(){
 		if(this._stream){
 			this._stream.stop();
+			this._stream = null;
 		}
 		return this;
 	};
@@ -124,36 +157,17 @@ define(["Tone/core/Tone", "Tone/source/Source"], function(Tone){
 	Tone.ExternalInput.prototype._start = function(time){
 		time = this.toSeconds(time);
 		this._gate.gain.setValueAtTime(1, time);
-	};
-
-	/**
-	 *  Stops the stream. Stream can be restarted with a different inputNum.
-	 *  @private
-	 */
-	Tone.ExternalInput.prototype._stop = function(time){
-		this._gate.gain.setValueAtTime(0, time);
 		return this;
 	};
 
 	/**
-	 * called when the stream is successfully setup
-	 * @param  {LocalMediaStream} stream
-	 * @private
+	 *  Stops the stream.
+	 *  @private
 	 */
-	Tone.ExternalInput.prototype._onStream = function(stream){
-		this._stream = stream;
-		//Wrap a MediaStreamSourceNode around the live input stream.
-		this._mediaStream = this.context.createMediaStreamSource(stream);
-		//Connect the MediaStreamSourceNode to a gate gain node
-		this._mediaStream.connect(this._gate);
-	};
-	/**
-	 * called on error
-	 * @param  {Error} e 
-	 * @private
-	 */
-	Tone.ExternalInput.prototype._onStreamError = function(e){
-		console.error(e);
+	Tone.ExternalInput.prototype._stop = function(time){
+		time = this.toSeconds(time);
+		this._gate.gain.setValueAtTime(0, time);
+		return this;
 	};
 
 	/**
@@ -162,13 +176,14 @@ define(["Tone/core/Tone", "Tone/source/Source"], function(Tone){
 	 */
 	Tone.ExternalInput.prototype.dispose = function(){
 		Tone.Source.prototype.dispose.call(this);
+		this.close();
 		if (this._mediaStream){
 			this._mediaStream.disconnect();
 			this._mediaStream = null;
 		}
-		this._stream = null;
 		this._constraints = null;
-		this.inputNum = null;
+		this._gate.dispose();
+		this._gate = null;
 		return this;
 	};
 
@@ -179,6 +194,7 @@ define(["Tone/core/Tone", "Tone/source/Source"], function(Tone){
 	/**
 	 * The array of available sources, different depending on whether connection is secure
 	 * @type {Array}
+	 * @static
 	 */
 	Tone.ExternalInput.sources = [];
 
@@ -187,31 +203,41 @@ define(["Tone/core/Tone", "Tone/source/Source"], function(Tone){
 	 * @type {Boolean}
 	 * @private
 	 */
-	Tone.ExternalInput._canGetSources = Tone.prototype.isFunction(MediaStreamTrack.getSources);
+	Tone.ExternalInput._canGetSources = !Tone.prototype.isUndef(window.MediaStreamTrack) && Tone.prototype.isFunction(MediaStreamTrack.getSources);
 
 	/**
-	 *  Populates the source list. Accepts a callback 
+	 *  Indicates if the browser supports 'getUserMedia'
+	 *  @type {Boolean}
+	 *  @static
+	 */
+	Tone.ExternalInput.canGetUserMedia = Tone.prototype.isFunction(navigator.getUserMedia);
+
+	/**
+	 *  Populates the source list. Invokes the callback with an array of 
+	 *  possible audio sources.
 	 *  @param  {function=} callback Callback to be executed after populating list 
-	 *  @return {Array} Tone.ExternalInput.sources
+	 *  @return {Tone.ExternalInput} this
+	 *  @static
 	 *  @example
-	 *  var soundflower = new Tone.ExternalInput();
-	 *  Tone.ExternalInput.getSources(selectSoundflower);
+	 * var soundflower = new Tone.ExternalInput();
+	 * Tone.ExternalInput.getSources(selectSoundflower);
 	 *
 	 * function selectSoundflower(sources){
-	 *  	for(var i = 0; i < sources.length; i++){
-	 *  		if(sources[i].label === "soundflower"){
-	 *  			soundflower.inputNum = i;
-	 *  			soundflower.open(function(){
-	 *  				soundflower.start();
-	 *  			});
-	 *  		}
-	 *  	}
-	 *  };
+	 * 	for(var i = 0; i < sources.length; i++){
+	 * 		if(sources[i].label === "soundflower"){
+	 * 			soundflower.inputNum = i;
+	 * 			soundflower.open(function(){
+	 * 				soundflower.start();
+	 * 			});
+	 * 			break;
+	 * 		}
+	 * 	}
+	 * };
 	 */
 	Tone.ExternalInput.getSources = function(callback){
 		if(Tone.ExternalInput.sources.length === 0 && Tone.ExternalInput._canGetSources){
 			MediaStreamTrack.getSources(function (media_sources){
-				for(var i = 0, max = media_sources.length; i < max; i++) {
+				for(var i = 0; i < media_sources.length; i++) {
 					if(media_sources[i].kind === "audio"){
 						Tone.ExternalInput.sources[i] = media_sources[i];
 					}
@@ -223,10 +249,6 @@ define(["Tone/core/Tone", "Tone/source/Source"], function(Tone){
 		}
 		return this;
 	};
-
-	//polyfill
-	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || 
-		navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
 	return Tone.ExternalInput;
 });
