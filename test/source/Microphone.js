dde05195a989a33b640e01912ae0a26ee7ca1aac
Yotam Mann <yotammann@gmail.com>
2015-09-18 17:19:24 -0700

    Microphone and ExternalInput tests

--- test/source/Microphone.js
+++ test/source/Microphone.js
@@ -1,6 +1,13 @@
-define(["Tone/source/Microphone", "helper/Basic"], function (Microphone, Basic) {
+define(["Tone/source/Microphone", "helper/Basic", "Tone/source/ExternalInput"], 
+	function (Microphone, Basic, ExternalInput) {
 
 	describe("Microphone", function(){
 		Basic(Microphone);
+
+		it ("extends ExternalInput", function(){
+			var mic = new Microphone();
+			expect(mic).to.be.instanceOf(ExternalInput);
+			mic.dispose();
+		});
 	});
 });
