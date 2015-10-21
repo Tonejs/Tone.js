c2311b07940ec7e89b6878a435ff018b705ac969
Yotam Mann <yotammann@gmail.com>
2015-10-21 10:04:55 -0400

    Delay extends Param

--- Tone/core/Delay.js
+++ Tone/core/Delay.js
@@ -1,35 +1,42 @@
-define(["Tone/core/Tone"], function (Tone) {
+define(["Tone/core/Tone", "Tone/core/Param"], function (Tone) {
 
 	/**
 	 *  @class Wrapper around Web Audio's native [DelayNode](http://webaudio.github.io/web-audio-api/#the-delaynode-interface). 
 	 *  @extends {Tone}
-	 *  @param {Number=} delayTime The delay applied to the incoming signal.
-	 *  @param {Number=} maxDelay The maximum delay time. 
+	 *  @param {Time=} value The delay applied to the incoming signal.
+	 *  @param {Time=} maxDelay The maximum delay time. 
 	 */
 	Tone.Delay = function(){
 
-		var options = this.optionsObject(arguments, ["delayTime", "maxDelay"], Tone.Delay.defaults);
+		var options = this.optionsObject(arguments, ["value", "maxDelay"], Tone.Delay.defaults);
 
 		/**
 		 *  The native delay node
 		 *  @type {DelayNode}
 		 *  @private
 		 */
-		this._delayNode = this.input = this.output = this.context.createDelay(options.maxDelay);
+		this._delayNode = this.context.createDelay(this.toSeconds(options.maxDelay));
+
+		Tone.Param.call(this, {
+			"param" : this._delayNode.delayTime,
+			"units" : Tone.Type.Time,
+			"value" : options.value
+		});
+
+		//set the input and output
+		this.input = this.output = this._delayNode;
 
 		/**
 		 *  The amount of time the incoming signal is
 		 *  delayed. 
-		 *  @type {Positive}
+		 *  @type {AudioParam}
 		 *  @signal
 		 */
-		this.delayTime = this._delayNode.delayTime;
-
+		this.delayTime = this._param;
 		this._readOnly("delayTime");
-		this.delayTime.value = options.delayTime;
 	};
 
-	Tone.extend(Tone.Delay);
+	Tone.extend(Tone.Delay, Tone.Param);
 
 	/**
 	 *  The defaults
@@ -38,15 +45,15 @@ define(["Tone/core/Tone"], function (Tone) {
 	 */
 	Tone.Delay.defaults = {
 		"maxDelay" : 1,
-		"delayTime" : 0
+		"value" : 0
 	};
-
+	
 	/**
 	 *  Clean up.
 	 *  @return  {Tone.Delay}  this
 	 */
 	Tone.Delay.prototype.dispose = function(){
-		Tone.prototype.dispose.call(this);
+		Tone.Param.prototype.dispose.call(this);
 		this._delayNode.disconnect();
 		this._delayNode = null;
 		this._writable("delayTime");
