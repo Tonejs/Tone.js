366e61562f2ab9b3d8d1d1c4cc10c47517ba5d79
Yotam Mann <yotammann@gmail.com>
2016-03-03 13:02:38 -0500

    canceling future states on `stop`
    also not checking if it’s already started.
    
    Fixes #121

--- Tone/source/Source.js
+++ Tone/source/Source.js
@@ -140,11 +140,10 @@ function(Tone){
 	 */
 	Tone.Source.prototype.stop = function(time){
 		time = this.toSeconds(time);
-		if (this._state.getStateAtTime(time) === Tone.State.Started){
-			this._state.setStateAtTime(Tone.State.Stopped, time);
-			if (this._stop){
-				this._stop.apply(this, arguments);
-			}
+		this._state.cancel(time);
+		this._state.setStateAtTime(Tone.State.Stopped, time);
+		if (this._stop){
+			this._stop.apply(this, arguments);
 		}
 		return this;
 	};
