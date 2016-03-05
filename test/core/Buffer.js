4a336e1675fa6015d75e1924720089e07e8e8920
Yotam Mann <yotammann@gmail.com>
2016-03-05 00:07:17 -0500

    using non-mp3s for testing

--- test/core/Buffer.js
+++ test/core/Buffer.js
@@ -4,15 +4,17 @@ define(["Test", "Tone/core/Buffer"], function (Test, Buffer) {
 		Buffer.baseUrl = "/base/test/";
 	}
 
+	var testFile = "./audio/sine.wav";
+
 	describe("Buffer", function(){
 		it ("can be created and disposed", function(){
-			var buff = new Buffer("./audio/kick.mp3");
+			var buff = new Buffer(testFile);
 			buff.dispose();
 			Test.wasDisposed(buff);
 		});
 
 		it("loads a file from a url string", function(done){
-			var buffer = new Buffer("./audio/kick.mp3", function(buff){
+			var buffer = new Buffer(testFile, function(buff){
 				expect(buff).to.be.instanceof(Buffer);
 				buffer.dispose();
 				done();
@@ -20,15 +22,15 @@ define(["Test", "Tone/core/Buffer"], function (Test, Buffer) {
 		});
 
 		it("has a duration", function(done){
-			var buffer = new Buffer("./audio/kick.mp3", function(){
-				expect(buffer.duration).to.be.closeTo(0.23, 0.01);
+			var buffer = new Buffer(testFile, function(){
+				expect(buffer.duration).to.be.closeTo(3, 0.01);
 				buffer.dispose();
 				done();
 			});
 		});
 
 		it("the static on('load') method is invoked", function(done){
-			var buffer = new Buffer("./audio/hh.mp3");
+			var buffer = new Buffer(testFile);
 			Buffer.on("load", function(){
 				buffer.dispose();
 				Buffer.off("load");
@@ -38,7 +40,7 @@ define(["Test", "Tone/core/Buffer"], function (Test, Buffer) {
 
 		it("can be constructed with an options object", function(done){
 			var buffer = new Buffer({
-				"url" : "./audio/hh.mp3",
+				"url" : testFile,
 				"reverse" : true,
 				"onload" : function(){
 					buffer.dispose();
@@ -49,7 +51,7 @@ define(["Test", "Tone/core/Buffer"], function (Test, Buffer) {
 
 		it("takes an AudioBuffer in the constructor method", function(done){
 			var buffer = new Buffer({
-				"url" : "./audio/hh.mp3",
+				"url" : testFile,
 				"onload" : function(){
 					var testOne = new Buffer(buffer.get());
 					expect(testOne.get()).to.equal(buffer.get());
@@ -62,7 +64,7 @@ define(["Test", "Tone/core/Buffer"], function (Test, Buffer) {
 
 		it("takes a Tone.Buffer in the constructor method", function(done){
 			var buffer = new Buffer({
-				"url" : "./audio/hh.mp3",
+				"url" : testFile,
 				"onload" : function(){
 					var testOne = new Buffer(buffer);
 					expect(testOne.get()).to.equal(buffer.get());
@@ -75,9 +77,9 @@ define(["Test", "Tone/core/Buffer"], function (Test, Buffer) {
 
 		it("takes Tone.Buffer in the set method", function(done){
 			var buffer = new Buffer({
-				"url" : "./audio/hh.mp3",
+				"url" : testFile,
 				"onload" : function(){
-					var testOne = new Buffer("./audio/hh.mp3");
+					var testOne = new Buffer(testFile);
 					testOne.set(buffer);
 					expect(testOne.get()).to.equal(buffer.get());
 					testOne.dispose();
@@ -89,9 +91,9 @@ define(["Test", "Tone/core/Buffer"], function (Test, Buffer) {
 
 		it("takes AudioBuffer in the set method", function(done){
 			var buffer = new Buffer({
-				"url" : "./audio/hh.mp3",
+				"url" : testFile,
 				"onload" : function(){
-					var testOne = new Buffer("./audio/hh.mp3");
+					var testOne = new Buffer(testFile);
 					testOne.set(buffer.get());
 					expect(testOne.get()).to.equal(buffer.get());
 					testOne.dispose();
@@ -102,7 +104,7 @@ define(["Test", "Tone/core/Buffer"], function (Test, Buffer) {
 		});
 
 		it("the static on('progress') method is invoked", function(done){
-			var buffer = new Buffer("./audio/hh.mp3");
+			var buffer = new Buffer(testFile);
 			Buffer.on("progress", function(percent){
 				expect(percent).to.be.a.number;
 				Buffer.off("progress");
@@ -112,7 +114,7 @@ define(["Test", "Tone/core/Buffer"], function (Test, Buffer) {
 		});
 
 		it("can reverse a buffer", function(done){
-			var buffer = new Buffer("./audio/kick.mp3", function(){
+			var buffer = new Buffer(testFile, function(){
 				var buffArray = buffer.get();
 				var lastSample = buffArray[buffArray.length - 1];
 				buffer.reverse = true;
