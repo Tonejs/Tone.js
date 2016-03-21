0e2c4fde9ad771366e9037d88d54d985f8ab7d66
Yotam Mann <yotammann@gmail.com>
2016-03-21 15:20:04 -0400

    a few more sanity checks for the polysynth

--- test/instrument/PolySynth.js
+++ test/instrument/PolySynth.js
@@ -1,12 +1,13 @@
-define(["Tone/instrument/PolySynth", "helper/Basic", "helper/InstrumentTests", "helper/OutputAudioStereo", "helper/Meter"], 
-function (PolySynth, Basic, InstrumentTests, OutputAudioStereo, Meter) {
+define(["Tone/instrument/PolySynth", "helper/Basic", "helper/InstrumentTests", "helper/OutputAudioStereo", 
+	"helper/Meter", "Tone/instrument/Instrument", "Test", "helper/OutputAudio", "Tone/instrument/MonoSynth"], 
+function (PolySynth, Basic, InstrumentTests, OutputAudioStereo, Meter, Instrument, Test, OutputAudio, MonoSynth) {
 
 	describe("PolySynth", function(){
 
 		Basic(PolySynth);
 		InstrumentTests(PolySynth, "C4");
 
-		/*context("Instrument Tests", function(){
+		context("PolySynth Tests", function(){
 
 			it ("extends Tone.Instrument", function(){
 				var polySynth = new PolySynth();
@@ -20,30 +21,10 @@ function (PolySynth, Basic, InstrumentTests, OutputAudioStereo, Meter) {
 				polySynth.dispose();
 			});
 
-			it ("can set the volume", function(){
-				var polySynth = new PolySynth({
-					"volume" : -10
-				});
-				expect(polySynth.volume.value).to.be.closeTo(-10, 0.1);
-				polySynth.dispose();
-			});
-
 			it("makes a sound", function(done){
 				var polySynth;
 				OutputAudio(function(dest){
-					polySynth = new PolySynth();
-					polySynth.connect(dest);
-					polySynth.triggerAttack("C4");
-				}, function(){
-					polySynth.dispose();
-					done();
-				});
-			});	
-
-			it("produces sound in both channels", function(done){
-				var polySynth;
-				OutputAudioStereo(function(dest){
-					polySynth = new PolySynth();
+					polySynth = new PolySynth(2);
 					polySynth.connect(dest);
 					polySynth.triggerAttack("C4");
 				}, function(){
@@ -89,45 +70,17 @@ function (PolySynth, Basic, InstrumentTests, OutputAudioStereo, Meter) {
 				meter.run();
 			});
 
-		});*/
+		});
 
 		context("API", function(){
 
-			/*it ("can get and set oscillator attributes", function(){
-				var polySynth = new PolySynth();
-				polySynth.oscillator.type = "triangle";
-				expect(polySynth.oscillator.type).to.equal("triangle");
-				polySynth.dispose();
-			});
-
-			it ("can get and set envelope attributes", function(){
-				var polySynth = new PolySynth();
-				polySynth.envelope.attack = 0.24;
-				expect(polySynth.envelope.attack).to.equal(0.24);
-				polySynth.dispose();
-			});
-
-			it ("can get and set filter attributes", function(){
-				var polySynth = new PolySynth();
-				polySynth.filter.Q.value = 0.4;
-				expect(polySynth.filter.Q.value).to.be.closeTo(0.4, 0.001);
-				polySynth.dispose();
-			});
-
-			it ("can get and set filterEnvelope attributes", function(){
-				var polySynth = new PolySynth();
-				polySynth.filterEnvelope.baseFrequency = 400;
-				expect(polySynth.filterEnvelope.baseFrequency).to.equal(400);
-				polySynth.dispose();
-			});
-
 			it ("can be constructed with an options object", function(){
-				var polySynth = new PolySynth({
+				var polySynth = new PolySynth(4, MonoSynth, {
 					"envelope" : {
 						"sustain" : 0.3
 					}
 				});
-				expect(polySynth.envelope.sustain).to.equal(0.3);
+				expect(polySynth.get().envelope.sustain).to.equal(0.3);
 				polySynth.dispose();
 			});
 
@@ -138,7 +91,7 @@ function (PolySynth, Basic, InstrumentTests, OutputAudioStereo, Meter) {
 				});
 				expect(polySynth.get().envelope.decay).to.equal(0.24);
 				polySynth.dispose();
-			});*/
+			});
 
 		});
 	});
