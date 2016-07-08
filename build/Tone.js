5b279569e6701c488fc321c471f2a6edcf45c6bb
Yotam Mann <yotammann@gmail.com>
2016-07-08 14:35:12 -0400

    updated release 7

--- build/Tone.js
+++ build/Tone.js
@@ -6903,6 +6903,8 @@
 	        } else if (state === Tone.State.Stopped) {
 	            this._nextTick = -1;
 	            this.ticks = 0;
+	        } else if (state === Tone.State.Paused) {
+	            this._nextTick = -1;
 	        }
 	    };
 	    /**
@@ -15774,6 +15776,134 @@
 	Module(function (Tone) {
 	    
 	    /**
+		 *  @class Maps a NormalRange [0, 1] to an AudioRange [-1, 1]. 
+		 *         See also Tone.AudioToGain. 
+		 *
+		 *  @extends {Tone.SignalBase}
+		 *  @constructor
+		 *  @example
+		 * var g2a = new Tone.GainToAudio();
+		 */
+	    Tone.GainToAudio = function () {
+	        /**
+			 *  @type {WaveShaperNode}
+			 *  @private
+			 */
+	        this._norm = this.input = this.output = new Tone.WaveShaper(function (x) {
+	            return Math.abs(x) * 2 - 1;
+	        });
+	    };
+	    Tone.extend(Tone.GainToAudio, Tone.SignalBase);
+	    /**
+		 *  clean up
+		 *  @returns {Tone.GainToAudio} this
+		 */
+	    Tone.GainToAudio.prototype.dispose = function () {
+	        Tone.prototype.dispose.call(this);
+	        this._norm.dispose();
+	        this._norm = null;
+	        return this;
+	    };
+	    return Tone.GainToAudio;
+	});
+	Module(function (Tone) {
+	    
+	    /**
+		 *  @class Normalize takes an input min and max and maps it linearly to NormalRange [0,1]
+		 *
+		 *  @extends {Tone.SignalBase}
+		 *  @constructor
+		 *  @param {number} inputMin the min input value
+		 *  @param {number} inputMax the max input value
+		 *  @example
+		 * var norm = new Tone.Normalize(2, 4);
+		 * var sig = new Tone.Signal(3).connect(norm);
+		 * //output of norm is 0.5. 
+		 */
+	    Tone.Normalize = function (inputMin, inputMax) {
+	        /**
+			 *  the min input value
+			 *  @type {number}
+			 *  @private
+			 */
+	        this._inputMin = this.defaultArg(inputMin, 0);
+	        /**
+			 *  the max input value
+			 *  @type {number}
+			 *  @private
+			 */
+	        this._inputMax = this.defaultArg(inputMax, 1);
+	        /**
+			 *  subtract the min from the input
+			 *  @type {Tone.Add}
+			 *  @private
+			 */
+	        this._sub = this.input = new Tone.Add(0);
+	        /**
+			 *  divide by the difference between the input and output
+			 *  @type {Tone.Multiply}
+			 *  @private
+			 */
+	        this._div = this.output = new Tone.Multiply(1);
+	        this._sub.connect(this._div);
+	        this._setRange();
+	    };
+	    Tone.extend(Tone.Normalize, Tone.SignalBase);
+	    /**
+		 * The minimum value the input signal will reach.
+		 * @memberOf Tone.Normalize#
+		 * @type {number}
+		 * @name min
+		 */
+	    Object.defineProperty(Tone.Normalize.prototype, 'min', {
+	        get: function () {
+	            return this._inputMin;
+	        },
+	        set: function (min) {
+	            this._inputMin = min;
+	            this._setRange();
+	        }
+	    });
+	    /**
+		 * The maximum value the input signal will reach.
+		 * @memberOf Tone.Normalize#
+		 * @type {number}
+		 * @name max
+		 */
+	    Object.defineProperty(Tone.Normalize.prototype, 'max', {
+	        get: function () {
+	            return this._inputMax;
+	        },
+	        set: function (max) {
+	            this._inputMax = max;
+	            this._setRange();
+	        }
+	    });
+	    /**
+		 *  set the values
+		 *  @private
+		 */
+	    Tone.Normalize.prototype._setRange = function () {
+	        this._sub.value = -this._inputMin;
+	        this._div.value = 1 / (this._inputMax - this._inputMin);
+	    };
+	    /**
+		 *  clean up
+		 *  @returns {Tone.Normalize} this
+		 */
+	    Tone.Normalize.prototype.dispose = function () {
+	        Tone.prototype.dispose.call(this);
+	        this._sub.dispose();
+	        this._sub = null;
+	        this._div.dispose();
+	        this._div = null;
+	        return this;
+	    };
+	    return Tone.Normalize;
+	});
+	Module(function (Tone) {
+	    
+	    /**
 		 *  @class Tone.PulseOscillator is a pulse oscillator with control over pulse width,
 		 *         also known as the duty cycle. At 50% duty cycle (width = 0.5) the wave is 
 		 *         a square and only odd-numbered harmonics are present. At all other widths 
@@ -19768,134 +19898,6 @@
 	    };
 	    Tone.extend(Tone.SimpleSynth, Tone.Synth);
 	    return Tone.SimpleSynth;
-	});
-	Module(function (Tone) {
-	    
-	    /**
-		 *  @class Maps a NormalRange [0, 1] to an AudioRange [-1, 1]. 
-		 *         See also Tone.AudioToGain. 
-		 *
-		 *  @extends {Tone.SignalBase}
-		 *  @constructor
-		 *  @example
-		 * var g2a = new Tone.GainToAudio();
-		 */
-	    Tone.GainToAudio = function () {
-	        /**
-			 *  @type {WaveShaperNode}
-			 *  @private
-			 */
-	        this._norm = this.input = this.output = new Tone.WaveShaper(function (x) {
-	            return Math.abs(x) * 2 - 1;
-	        });
-	    };
-	    Tone.extend(Tone.GainToAudio, Tone.SignalBase);
-	    /**
-		 *  clean up
-		 *  @returns {Tone.GainToAudio} this
-		 */
-	    Tone.GainToAudio.prototype.dispose = function () {
-	        Tone.prototype.dispose.call(this);
-	        this._norm.dispose();
-	        this._norm = null;
-	        return this;
-	    };
-	    return Tone.GainToAudio;
-	});
-	Module(function (Tone) {
-	    
-	    /**
-		 *  @class Normalize takes an input min and max and maps it linearly to NormalRange [0,1]
-		 *
-		 *  @extends {Tone.SignalBase}
-		 *  @constructor
-		 *  @param {number} inputMin the min input value
-		 *  @param {number} inputMax the max input value
-		 *  @example
-		 * var norm = new Tone.Normalize(2, 4);
-		 * var sig = new Tone.Signal(3).connect(norm);
-		 * //output of norm is 0.5. 
-		 */
-	    Tone.Normalize = function (inputMin, inputMax) {
-	        /**
-			 *  the min input value
-			 *  @type {number}
-			 *  @private
-			 */
-	        this._inputMin = this.defaultArg(inputMin, 0);
-	        /**
-			 *  the max input value
-			 *  @type {number}
-			 *  @private
-			 */
-	        this._inputMax = this.defaultArg(inputMax, 1);
-	        /**
-			 *  subtract the min from the input
-			 *  @type {Tone.Add}
-			 *  @private
-			 */
-	        this._sub = this.input = new Tone.Add(0);
-	        /**
-			 *  divide by the difference between the input and output
-			 *  @type {Tone.Multiply}
-			 *  @private
-			 */
-	        this._div = this.output = new Tone.Multiply(1);
-	        this._sub.connect(this._div);
-	        this._setRange();
-	    };
-	    Tone.extend(Tone.Normalize, Tone.SignalBase);
-	    /**
-		 * The minimum value the input signal will reach.
-		 * @memberOf Tone.Normalize#
-		 * @type {number}
-		 * @name min
-		 */
-	    Object.defineProperty(Tone.Normalize.prototype, 'min', {
-	        get: function () {
-	            return this._inputMin;
-	        },
-	        set: function (min) {
-	            this._inputMin = min;
-	            this._setRange();
-	        }
-	    });
-	    /**
-		 * The maximum value the input signal will reach.
-		 * @memberOf Tone.Normalize#
-		 * @type {number}
-		 * @name max
-		 */
-	    Object.defineProperty(Tone.Normalize.prototype, 'max', {
-	        get: function () {
-	            return this._inputMax;
-	        },
-	        set: function (max) {
-	            this._inputMax = max;
-	            this._setRange();
-	        }
-	    });
-	    /**
-		 *  set the values
-		 *  @private
-		 */
-	    Tone.Normalize.prototype._setRange = function () {
-	        this._sub.value = -this._inputMin;
-	        this._div.value = 1 / (this._inputMax - this._inputMin);
-	    };
-	    /**
-		 *  clean up
-		 *  @returns {Tone.Normalize} this
-		 */
-	    Tone.Normalize.prototype.dispose = function () {
-	        Tone.prototype.dispose.call(this);
-	        this._sub.dispose();
-	        this._sub = null;
-	        this._div.dispose();
-	        this._div = null;
-	        return this;
-	    };
-	    return Tone.Normalize;
 	});
 	Module(function (Tone) {
 	    /**
