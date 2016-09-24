10459f42817402db5c58e2acf81e0cfbfc743be9
Yotam Mann <yotammann@gmail.com>
2016-09-23 20:20:14 -0400

    set the .value of the AudioParam when the passed in time is less than one block away
    addresses #163. thanks for the suggestion @stuartpb

--- Tone/core/Param.js
+++ Tone/core/Param.js
@@ -168,7 +168,12 @@ define(["Tone/core/Tone", "Tone/type/Type"], function(Tone){
 	 */
 	Tone.Param.prototype.setValueAtTime = function(value, time){
 		value = this._fromUnits(value);
-		this._param.setValueAtTime(value, this.toSeconds(time));
+		time = this.toSeconds(time);
+		if (time < this.now() + this.blockTime){
+			this._param.value = value;
+		} else {
+			this._param.setValueAtTime(value, time);
+		}
 		return this;
 	};
 
