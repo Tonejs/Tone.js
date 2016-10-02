70ea8f715575243dd381ddebf34ff646d6512114
Yotam Mann <yotammann@gmail.com>
2016-10-01 21:45:30 -0400

    panner/listener tests

---
+++ test/component/Panner3D.js
@@ -0,0 +1,78 @@
+define(["Test", "Tone/component/Panner3D", "helper/PassAudio"], 
+	function (Test, Panner3D, PassAudio) {
+
+	describe("Panner3D", function(){
+
+		it("passes the incoming signal through", function(done){
+			var panner;
+			PassAudio(function(input, output){
+				panner = new Panner3D();
+				input.connect(panner);
+				panner.connect(output);
+			}, function(){
+				panner.dispose();
+				done();
+			});
+		});
+
+		it ("can get/set the position individually", function(){
+			var panner = new Panner3D();
+			panner.positionX = 10;
+			expect(panner.positionX).to.equal(10);
+			panner.positionY = 20;
+			expect(panner.positionY).to.equal(20);
+			panner.positionZ = -1;
+			expect(panner.positionZ).to.equal(-1);
+			panner.dispose();
+		});
+
+		it ("can get/set the orientation individually", function(){
+			var panner = new Panner3D();
+			panner.orientationX = 2;
+			expect(panner.orientationX).to.equal(2);
+			panner.orientationY = 4;
+			expect(panner.orientationY).to.equal(4);
+			panner.orientationZ = -3;
+			expect(panner.orientationZ).to.equal(-3);
+			panner.dispose();
+		});
+
+		it ("can get/set the position through setPosition", function(){
+			var panner = new Panner3D();
+			panner.setPosition(3, -11, 2);
+			expect(panner.positionX).to.equal(3);
+			expect(panner.positionY).to.equal(-11);
+			expect(panner.positionZ).to.equal(2);
+			panner.dispose();
+		});
+
+		it ("can get/set the orientation through setOrientation", function(){
+			var panner = new Panner3D();
+			panner.setOrientation(2, -1, 0.5);
+			expect(panner.orientationX).to.equal(2);
+			expect(panner.orientationY).to.equal(-1);
+			expect(panner.orientationZ).to.equal(0.5);
+			panner.dispose();
+		});
+
+		it ("can get/set all of the other attributes", function(){
+			var values = {
+				"panningModel" : "HRTF",
+				"maxDistance" : 10002,
+				"distanceModel" : "exponential",
+				"coneOuterGain" : 0.3,
+				"coneOuterAngle" : 280,
+				"coneInnerAngle" : 120,
+				"refDistance" : 0.3,
+				"rolloffFactor" : 3
+			};
+			var panner = new Panner3D();
+			for (var v in values){
+				panner[v] = values[v];
+				expect(panner[v]).to.equal(values[v]);
+			}
+			panner.dispose();
+		});
+			
+	});
+});
---
+++ test/core/Listener.js
@@ -0,0 +1,57 @@
+define(["Test", "Tone/core/Listener", "Tone/core/Tone"], 
+	function (Test, Listener, Tone) {
+
+	describe("Listener", function(){
+
+		afterEach(function(){
+			Tone.Listener.setPosition(0, 0, 0);
+			Tone.Listener.setOrientation(0, 0, 0, 0, 0, 0, 0);
+		});
+
+		it("exists", function(){
+			expect(Tone.Listener).to.exist;
+		});
+
+		it ("can get/set the position individually", function(){
+			Tone.Listener.positionX = 10;
+			expect(Tone.Listener.positionX).to.equal(10);
+			Tone.Listener.positionY = 20;
+			expect(Tone.Listener.positionY).to.equal(20);
+			Tone.Listener.positionZ = -1;
+			expect(Tone.Listener.positionZ).to.equal(-1);
+		});
+
+		it ("can get/set the forward/up individually", function(){
+			Tone.Listener.forwardX = 2;
+			expect(Tone.Listener.forwardX).to.equal(2);
+			Tone.Listener.forwardY = 4;
+			expect(Tone.Listener.forwardY).to.equal(4);
+			Tone.Listener.forwardZ = -3;
+			expect(Tone.Listener.forwardZ).to.equal(-3);
+			Tone.Listener.upX = 1;
+			expect(Tone.Listener.upX).to.equal(1);
+			Tone.Listener.upY = 0.2;
+			expect(Tone.Listener.upY).to.equal(0.2);
+			Tone.Listener.upZ = -0.3;
+			expect(Tone.Listener.upZ).to.equal(-0.3);
+		});
+
+		it ("can get/set the position through setPosition", function(){
+			Tone.Listener.setPosition(2, -10, 0);
+			expect(Tone.Listener.positionX).to.equal(2);
+			expect(Tone.Listener.positionY).to.equal(-10);
+			expect(Tone.Listener.positionZ).to.equal(0);
+		});
+
+		it ("can get/set the orientation through setOrientation", function(){
+			Tone.Listener.setOrientation(2, -1, 0.5, 0.1, 0.3, 0.2);
+			expect(Tone.Listener.forwardX).to.equal(2);
+			expect(Tone.Listener.forwardY).to.equal(-1);
+			expect(Tone.Listener.forwardZ).to.equal(0.5);
+			expect(Tone.Listener.upX).to.equal(0.1);
+			expect(Tone.Listener.upY).to.equal(0.3);
+			expect(Tone.Listener.upZ).to.equal(0.2);
+		});
+			
+	});
+});
