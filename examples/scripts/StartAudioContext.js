092055e9d180f102175fd7518c6ca0da50107522
Yotam Mann <yotammann@gmail.com>
2016-10-05 11:14:30 -0400

    updating to latest StartAudioContext
    [skip ci]

--- examples/scripts/StartAudioContext.js
+++ examples/scripts/StartAudioContext.js
@@ -6,175 +6,180 @@
  */
 (function (root, factory) {
 	if (typeof define === "function" && define.amd) {
-		define([], factory);
-	 } else if (typeof module === 'object' && module.exports) {
-        module.exports = factory();
+		define([], factory)
+	 } else if (typeof module === "object" && module.exports) {
+        module.exports = factory()
 	} else {
-		root.StartAudioContext = factory();
+		root.StartAudioContext = factory()
   }
 }(this, function () {
 
-	/**
-	 * The StartAudioContext object
-	 */
-	var StartAudioContext = {
-		/**
-		 * The audio context passed in by the user
-		 * @type {AudioContext}
-		 */
-		context : null,
-		/**
-		 * The TapListeners bound to the elements
-		 * @type {Array}
-		 * @private
-		 */
-		_tapListeners : [],
-		/**
-		 * Callbacks to invoke when the audio context is started
-		 * @type {Array}
-		 * @private
-		 */
-		_onStarted : [],
-	};
-
-
-	/**
-	 * Set the context
-	 * @param {AudioContext} ctx
-	 * @returns {StartAudioContext}
-	 */
-	StartAudioContext.setContext = function(ctx){
-		StartAudioContext.context = ctx;
-		return StartAudioContext;
-	};
-
-	/**
-	 * Add a tap listener to the audio context
-	 * @param  {Array|Element|String|jQuery} element
-	 * @returns {StartAudioContext}
-	 */
-	StartAudioContext.on = function(element){
-		if (Array.isArray(element) || (NodeList && element instanceof NodeList)){
-			for (var i = 0; i < element.length; i++){
-				StartAudioContext.on(element[i]);
-			}
-		} else if (typeof element === "string"){
-			StartAudioContext.on(document.querySelectorAll(element));
-		} else if (element.jquery && typeof element.toArray === "function"){
-			StartAudioContext.on(element.toArray());
-		} else if (Element && element instanceof Element){
-			//if it's an element, create a TapListener
-			var tap = new TapListener(element, onTap);
-			StartAudioContext._tapListeners.push(tap);
-		} 
-		return StartAudioContext;
-	};
-
-	/**
-	 * Bind a callback to when the audio context is started. 
-	 * @param {Function} cb
-	 * @return {StartAudioContext}
-	 */
-	StartAudioContext.onStarted = function(cb){
-		//if it's already started, invoke the callback
-		if (StartAudioContext.isStarted()){
-			cb();
-		} else {
-			StartAudioContext._onStarted.push(cb);
-		}
-		return StartAudioContext;
-	};
-
-	/**
-	 * returns true if the context is started
-	 * @return {Boolean}
-	 */
-	StartAudioContext.isStarted = function(){
-		return (StartAudioContext.context !== null && StartAudioContext.context.state === "running");
-	};
+	//TAP LISTENER/////////////////////////////////////////////////////////////
 
 	/**
 	 * @class  Listens for non-dragging tap ends on the given element
 	 * @param {Element} element
 	 * @internal
 	 */
-	var TapListener = function(element){
+	var TapListener = function(element, context){
 
-		this._dragged = false;
+		this._dragged = false
 
-		this._element = element;
+		this._element = element
 
-		this._bindedMove = this._moved.bind(this);
-		this._bindedEnd = this._ended.bind(this);
+		this._bindedMove = this._moved.bind(this)
+		this._bindedEnd = this._ended.bind(this, context)
 
-		element.addEventListener("touchmove", this._bindedMove);
-		element.addEventListener("touchend", this._bindedEnd);
-		element.addEventListener("mouseup", this._bindedEnd);
-	};
+		element.addEventListener("touchmove", this._bindedMove)
+		element.addEventListener("touchend", this._bindedEnd)
+		element.addEventListener("mouseup", this._bindedEnd)
+	}
 
 	/**
 	 * drag move event
 	 */
 	TapListener.prototype._moved = function(e){
-		this._dragged = true;
+		this._dragged = true
 	};
 
 	/**
 	 * tap ended listener
 	 */
-	TapListener.prototype._ended = function(e){
+	TapListener.prototype._ended = function(context){
 		if (!this._dragged){
-			onTap();
+			startContext(context)
 		}
-		this._dragged = false;
+		this._dragged = false
 	};
 
 	/**
 	 * remove all the bound events
 	 */
 	TapListener.prototype.dispose = function(){
-		this._element.removeEventListener("touchmove", this._bindedMove);
-		this._element.removeEventListener("touchend", this._bindedEnd);
-		this._element.removeEventListener("mouseup", this._bindedEnd);
-		this._bindedMove = null;
-		this._bindedEnd = null;
-		this._element = null;
+		this._element.removeEventListener("touchmove", this._bindedMove)
+		this._element.removeEventListener("touchend", this._bindedEnd)
+		this._element.removeEventListener("mouseup", this._bindedEnd)
+		this._bindedMove = null
+		this._bindedEnd = null
+		this._element = null
 	};
 
+	//END TAP LISTENER/////////////////////////////////////////////////////////
+
 	/**
-	 * Invoked the first time of the elements is tapped.
-	 * Creates a silent oscillator when a non-dragging touchend 
-	 * event has been triggered.
+	 * Plays a silent sound and also invoke the "resume" method
+	 * @param {AudioContext} context
+	 * @private
 	 */
-	function onTap(){
+	function startContext(context){
+		// this accomplishes the iOS specific requirement
+		var buffer = context.createBuffer(1, 1, context.sampleRate)
+		var source = context.createBufferSource()
+		source.buffer = buffer
+		source.connect(context.destination)
+		source.start(0)
 
-		//start the audio context with a silent oscillator
-		if (StartAudioContext.context && !StartAudioContext.isStarted()){
-			var osc = StartAudioContext.context.createOscillator();
-			var silent = StartAudioContext.context.createGain();
-			silent.gain.value = 0;
-			osc.connect(silent);
-			silent.connect(StartAudioContext.context.destination);
-			var now = StartAudioContext.context.currentTime;
-			osc.start(now);
-			osc.stop(now+0.5);
-		}
-
-		//dispose all the tap listeners
-		if (StartAudioContext._tapListeners){
-			for (var i = 0; i < StartAudioContext._tapListeners.length; i++){
-				StartAudioContext._tapListeners[i].dispose();
-			}
-			StartAudioContext._tapListeners = null;
-		}
-		//the onstarted callbacks
-		if (StartAudioContext._onStarted){
-			for (var j = 0; j < StartAudioContext._onStarted.length; j++){
-				StartAudioContext._onStarted[j]();
-			}
-			StartAudioContext._onStarted = null;
+		// resume the audio context
+		if (context.resume){
+			context.resume()
 		}
 	}
 
-	return StartAudioContext;
-}));
+	/**
+	 * Returns true if the audio context is started
+	 * @param  {AudioContext}  context
+	 * @return {Boolean}
+	 * @private
+	 */
+	function isStarted(context){
+		 return context.state === "running"
+	}
+
+	/**
+	 * Invokes the callback as soon as the AudioContext
+	 * is started
+	 * @param  {AudioContext}   context
+	 * @param  {Function} callback
+	 */
+	function onStarted(context, callback){
+
+		function checkLoop(){
+			if (isStarted(context)){
+				callback()
+			} else {
+				requestAnimationFrame(checkLoop)
+				if (context.resume){
+					context.resume()
+				}
+			}
+		}
+
+		if (isStarted(context)){
+			callback()
+		} else {
+			checkLoop()
+		}
+	}
+
+	/**
+	 * Add a tap listener to the audio context
+	 * @param  {Array|Element|String|jQuery} element
+	 * @param {Array} tapListeners
+	 */
+	function bindTapListener(element, tapListeners, context){
+		if (Array.isArray(element) || (NodeList && element instanceof NodeList)){
+			for (var i = 0; i < element.length; i++){
+				bindTapListener(element[i], tapListeners, context)
+			}
+		} else if (typeof element === "string"){
+			bindTapListener(document.querySelectorAll(element), tapListeners, context)
+		} else if (element.jquery && typeof element.toArray === "function"){
+			bindTapListener(element.toArray(), tapListeners, context)
+		} else if (Element && element instanceof Element){
+			//if it's an element, create a TapListener
+			var tap = new TapListener(element, context)
+			tapListeners.push(tap)
+		} 
+	}
+
+	/**
+	 * @param {AudioContext} context The AudioContext to start.
+	 * @param {Array|String|Element|jQuery} elements For iOS, the list of elements
+	 *                                               to bind tap event listeners
+	 *                                               which will start the AudioContext.
+	 * @param {Function=} callback The callback to invoke when the AudioContext is started.
+	 * @return {Promise} The promise is invoked when the AudioContext
+	 *                       is started.
+	 */
+	function StartAudioContext(context, elements, callback){
+
+		//the promise is invoked when the AudioContext is started
+		var promise = new Promise(function(success) {
+			onStarted(context, success)
+		})
+
+		// The TapListeners bound to the elements
+		var tapListeners = []
+
+		// add all the tap listeners
+		if (elements){
+			bindTapListener(elements, tapListeners, context)
+		}
+
+		//dispose all these tap listeners when the context is started
+		promise.then(function(){
+			for (var i = 0; i < tapListeners.length; i++){
+				tapListeners[i].dispose()
+			}
+			tapListeners = null
+
+			if (callback){
+				callback()
+			}
+		})
+
+		return promise
+	}
+
+	return StartAudioContext
+}))
