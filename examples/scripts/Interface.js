a2343bd31ae350ffda5b2c6f11a5e11204090ce8
Yotam Mann <yotammann@gmail.com>
2016-12-22 14:30:24 -0500

    replacing qwerty-hancock with custom keyboard interface
    [skip ci]

--- examples/scripts/Interface.js
+++ examples/scripts/Interface.js
@@ -482,3 +482,51 @@ Interface.Transport.prototype._loop = function(){
 	setTimeout(this._boundLoop, 50);
 	this.position.text(Tone.Transport.position);
 };
+
+
+/**
+ *
+ *	KEYBOARD
+ *  
+ */
+Interface.Keyboard = function(){
+
+	if (this instanceof Interface.Keyboard){
+
+		this.element = $("<div>", {
+			"class" : "Keyboard",
+		}).appendTo("#Content");
+
+		if (window.Keyboard){
+			this.keyboard = new Keyboard(this.element.get(0), 48, 4);
+			$(window).on("resize", this._resize.bind(this));
+			this._resize();
+
+			this.keyboard.on("keyDown", function(midi){
+				if (this.keyDown){
+					this.keyDown(Tone.Frequency(midi, "midi").toNote());
+				}
+			}.bind(this));
+
+			this.keyboard.on("keyUp", function(midi){
+				if (this.keyUp){
+					this.keyUp(Tone.Frequency(midi, "midi").toNote());
+				}
+			}.bind(this));
+		}
+
+	} else {
+		return new Interface.Keyboard();
+	}
+};
+
+Interface.Keyboard.prototype._resize = function(){
+	var width = $(window).width();
+	var octaves = Math.round(width / 220);
+	this.keyboard.octaves = Math.max(octaves, 2);
+	if (octaves > 4){
+		this.keyboard.rootNote = 36;
+	} else {
+		this.keyboard.rootNote = 48;
+	}
+}
