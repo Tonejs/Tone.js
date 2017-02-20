47325729b26a19d73ae5d5a68c1780d5e8124221
Yotam Mann <yotammann@gmail.com>
2017-02-20 00:04:45 -0500

    updating EffectsTests to new Offline API

--- test/helper/EffectTests.js
+++ test/helper/EffectTests.js
@@ -46,108 +46,80 @@ define(["helper/OutputAudio", "Tone/effect/Effect", "helper/PassAudio",
 				instance.dispose();
 			});
 
-			it("passes audio from input to output", function(done){
-				var instance;
-				PassAudio(function(input, dest){
-					instance = new Constr(args);
+			it("passes audio from input to output", function(){
+				return PassAudio(function(input){
+					var instance = new Constr(args);
 					if (before){
 						before(instance);
 					}
 					input.connect(instance);
-					instance.connect(dest);
-				}, function(){
-					instance.dispose();
-					done();
+					instance.toMaster();
 				});
 			});	
 
-			it("passes audio in both channels", function(done){
-				var instance;
-				PassAudioStereo(function(input, dest){
-					instance = new Constr(args);
+			it("passes audio in both channels", function(){
+				return PassAudioStereo(function(input){
+					var instance = new Constr(args);
 					if (before){
 						before(instance);
 					}
 					input.connect(instance);
-					instance.connect(dest);
-				}, function(){
-					instance.dispose();
-					done();
+					instance.toMaster();
 				});
 			});	
 
-			it("can pass 100% dry signal", function(done){
-				var signalL, signalR, merge, instance;
-				var offline = new Offline(1, 2);
-				offline.before(function(dest){
-					instance = new Constr(args).connect(dest);
+			it("can pass 100% dry signal", function(){
+				return Offline(function(){
+					var instance = new Constr(args).toMaster();
 					if (before){
 						before(instance);
 					}
-					merge = new Merge().connect(instance);
-					signalL = new Signal(-1).connect(merge.left);
-					signalR = new Signal(1).connect(merge.right);
+					var merge = new Merge().connect(instance);
+					var signalL = new Signal(-1).connect(merge.left);
+					var signalR = new Signal(1).connect(merge.right);
 					//make the signals ramp
 					signalL.linearRampToValue(1, 1);
 					signalR.linearRampToValue(-1, 1);
 					instance.wet.value = 0;
+				}, 0.5, 2).then(function(buffer){
+					buffer.forEach(function(L, R, time){
+						var leftValue = (time * 2) - 1;
+						var rightValue = ((1 - time) * 2) - 1;
+						expect(L).to.be.closeTo(leftValue, 0.001);
+						expect(R).to.be.closeTo(rightValue, 0.001);
+					});
 				});
-				offline.test(function(samples, time){
-					var leftValue = (time * 2) - 1;
-					var rightValue = ((1 - time) * 2) - 1;
-					expect(samples[0]).to.be.closeTo(leftValue, 0.001);
-					expect(samples[1]).to.be.closeTo(rightValue, 0.001);
-				});
-				offline.after(function(){
-					signalL.dispose();
-					signalR.dispose();
-					merge.dispose();
-					instance.dispose();
-					done();
-				});
-				offline.run();
 			});	
 
-			it("effects the incoming signal", function(done){
-				var signalL, signalR, merge, instance;
-				var offline = new Offline(1, 2);
-				var leftEffected = false;
-				var rightEffected = false;
-				offline.before(function(dest){
-					instance = new Constr(args).connect(dest);
+			it("effects the incoming signal", function(){
+				return Offline(function(){
+					var instance = new Constr(args).toMaster();
 					if (before){
 						before(instance);
 					}
-					merge = new Merge().connect(instance);
-					signalL = new Signal(-1).connect(merge.left);
-					signalR = new Signal(1).connect(merge.right);
+					var merge = new Merge().connect(instance);
+					var signalL = new Signal(-1).connect(merge.left);
+					var signalR = new Signal(1).connect(merge.right);
 					//make the signals ramp
 					signalL.linearRampToValue(1, 1);
 					signalR.linearRampToValue(-1, 1);
 					if (instance.start){
 						instance.start();
 					}
-				});
-				offline.test(function(samples, time){
-					var leftValue = (time * 2) - 1;
-					var rightValue = ((1 - time) * 2) - 1;
-					if (Math.abs(samples[0] - leftValue) > 0.01){
-						leftEffected = true;
-					}
-					if (Math.abs(samples[1] - rightValue) > 0.01){
-						rightEffected = true;	
-					}
-				});
-				offline.after(function(){
+				}, 0.5, 2).then(function(buffer){
+					buffer.forEach(function(L, R, time){
+						var leftValue = (time * 2) - 1;
+						var rightValue = ((1 - time) * 2) - 1;
+						if (Math.abs(L - leftValue) > 0.01){
+							leftEffected = true;
+						}
+						if (Math.abs(R - rightValue) > 0.01){
+							rightEffected = true;	
+						}
+					});
 					expect(leftEffected).to.be.true;
 					expect(rightEffected).to.be.true;
-					signalL.dispose();
-					signalR.dispose();
-					merge.dispose();
-					instance.dispose();
-					done();
 				});
-				offline.run();
 			});	
 		});
 
