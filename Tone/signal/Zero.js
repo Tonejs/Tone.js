662951cff97a5ef9000a07c3616501bb31ed3a47
Yotam Mann <yotammann@gmail.com>
2017-02-19 23:56:04 -0500

    uses constant from context

--- Tone/signal/Zero.js
+++ Tone/signal/Zero.js
@@ -15,7 +15,7 @@ define(["Tone/core/Tone", "Tone/core/Gain"], function (Tone) {
 		 */
 		this._gain = this.input = this.output = new Tone.Gain();
 
-		Tone.Zero._zeros.connect(this._gain);
+		Tone.context._zeros.connect(this._gain);
 	};
 
 	Tone.extend(Tone.Zero);
@@ -30,34 +30,6 @@ define(["Tone/core/Tone", "Tone/core/Gain"], function (Tone) {
 		this._gain = null;
 		return this;
 	};
-
-	/**
-	 *  Generates a constant output of 0. This is so 
-	 *  the processing graph doesn't optimize out this
-	 *  segment of the graph. 
-	 *  @static
-	 *  @private
-	 *  @const
-	 *  @type {AudioBufferSourceNode}
-	 */
-	Tone.Zero._zeros = null;
-
-	/**
-	 *  initializer function
-	 */
-	Tone._initAudioContext(function(audioContext){
-		var buffer = audioContext.createBuffer(1, 128, audioContext.sampleRate);
-		var arr = buffer.getChannelData(0);
-		for (var i = 0; i < arr.length; i++){
-			arr[i] = 0;
-		}
-		Tone.Zero._zeros = audioContext.createBufferSource();
-		Tone.Zero._zeros.channelCount = 1;
-		Tone.Zero._zeros.channelCountMode = "explicit";
-		Tone.Zero._zeros.buffer = buffer;
-		Tone.Zero._zeros.loop = true;
-		Tone.Zero._zeros.start(0);
-	});
 
 	return Tone.Zero;
 });
