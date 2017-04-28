1d13246808a811995a267f94159d579a2e706ab9
Yotam Mann <yotammann@gmail.com>
2017-04-27 22:02:54 -0400

    test logging level

--- gulp/karma.conf.js
+++ gulp/karma.conf.js
@@ -72,7 +72,7 @@ module.exports = function(config) {
 
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_INFO,
+		logLevel: config.LOG_ERROR,
 
 
 		// enable / disable watching file and executing tests whenever any file changes
