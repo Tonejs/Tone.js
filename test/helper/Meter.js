79a0e22fd7024126437c7080bb9e56d22800aea0
Yotam Mann <yotammann@gmail.com>
2017-05-01 17:44:25 -0400

    fixed timing error in Meter
    was always passing in NaN

--- test/helper/Meter.js
+++ test/helper/Meter.js
@@ -3,12 +3,11 @@ define(["helper/Offline"], function (Offline) {
 	return function(callback, duration, channels){
 
 		return Offline(callback, duration, channels).then(function(buffer){
-			var blockTime = 512/buffer.sampleRate;
 			var rms = buffer.getRMS(512);
 			rms.forEach = function(callback){
 				for (var i = 0; i < rms.length; i++){
 					var level = rms[i];
-					callback(level, blockTime * i);
+					callback(level, i * buffer.sampleTime);
 				}
 			};
 			return rms;
