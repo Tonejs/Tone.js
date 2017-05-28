5a8550690f9390808c4f624c3c92177e1f2dc36f
Yotam Mann <yotammann@gmail.com>
2017-05-28 14:02:15 -0400

    start/stop/pause events to TickSignal

--- Tone/signal/TickSignal.js
+++ Tone/signal/TickSignal.js
@@ -13,6 +13,8 @@ define(["Tone/core/Tone", "Tone/signal/TimelineSignal"], function (Tone) {
 	 */
 	Tone.TickSignal = function(value){
 
+		value = Tone.defaultArg(value, 1);
+
 		Tone.TimelineSignal.call(this, {
 			"units" : Tone.Type.Ticks,
 			"value" : value
@@ -20,6 +22,21 @@ define(["Tone/core/Tone", "Tone/signal/TimelineSignal"], function (Tone) {
 
 		//extend the memory
 		this._events.memory = Infinity;
+
+		/**
+		 * The paused time
+		 * @type {Ticks}
+		 * @private
+		 */
+		this._pausedRate = null;
+
+
+		/**
+		 * Keep track of state
+		 * @type {Tone.TimelineState}
+		 * @private
+		 */
+		// this._state = new Tone.TimelineState(Tone.State.Started);
 	};
 
 	Tone.extend(Tone.TickSignal, Tone.TimelineSignal);
@@ -33,10 +50,17 @@ define(["Tone/core/Tone", "Tone/signal/TimelineSignal"], function (Tone) {
 	 */
 	function _wrapScheduleMethods(method){
 		return function(value, time){
+			//make sure it's not paused or stopped
+			var lastEvent = this._events.get(time);
+			if (lastEvent && lastEvent.type === Tone.State.Stopped && this._pausedRate !== null){
+				throw new Error("Tone.TickSignal: cannot schedule automations while stopped.");
+			}
 			time = this.toSeconds(time);
 			method.apply(this, arguments);
 			var event = this._events.get(time);
-			event.ticks = Math.max(this.getTickAtTime(event.time - this.sampleTime), 0);
+			var previousEvent = this._events.previousEvent(event);
+			var ticksUntilTime = this._getTickUntilEvent(previousEvent, time - this.sampleTime);
+			event.ticks = Math.max(ticksUntilTime, 0);
 			return this;
 		};
 	}
@@ -64,6 +88,55 @@ define(["Tone/core/Tone", "Tone/signal/TimelineSignal"], function (Tone) {
 			var rampVal = this._exponentialApproach(prevEvent.time, prevEvent.value, value, constant, segTime);
 			this.linearRampToValueAtTime(rampVal, segTime);
 		}
+		return this;
+	};
+
+	/**
+	 * Stop the signal from ticking forward and reset the ticks to 0
+	 * until it is restarted. 
+	 * @param  {Time=} time When to stop the signal
+	 * @return {Tone.TickSignal}      this
+	 */
+	Tone.TickSignal.prototype.stop = function(time){
+		time = this.toSeconds(time);
+		this._pausedRate = this.getValueAtTime(time);
+		this.cancel(time);
+		this.setValueAtTime(0, time);
+		var event = this._events.get(time);
+		event.type = Tone.State.Stopped;
+		event.ticks = 0;
+		return this;
+	};
+
+	/**
+	 * Pause the ticks from counting forward. Will remain
+	 * at the same tick value until resumed. 
+	 * @param  {Time=} time When to pause the signal
+	 * @return {Tone.TickSignal}      this
+	 */
+	Tone.TickSignal.prototype.pause = function(time){
+		time = this.toSeconds(time);
+		this._pausedRate = this.getValueAtTime(time);
+		this.setValueAtTime(0, time);
+		var event = this._events.get(time);
+		event.type = Tone.State.Stopped;
+		return this;
+	};
+
+	/**
+	 * Start the signal after paused or stopped. Optionally with an offset
+	 * to start the tick counter at. 
+	 * @param  {Time=} time When to pause the signal
+	 * @param  {Ticks=} offset How many ticks to offset the counter
+	 * @return {Tone.TickSignal}      this
+	 */
+	Tone.TickSignal.prototype.start = function(time, offset){
+		time = this.toSeconds(time);
+		var resumeRate = this._pausedRate;
+		this._pausedRate = null;
+		this.setValueAtTime(resumeRate, time);
+		var event = this._events.get(time);
+		event.ticks = Tone.defaultArg(offset, this.getTickAtTime(time));
 		return this;
 	};
 
@@ -102,18 +175,32 @@ define(["Tone/core/Tone", "Tone/signal/TimelineSignal"], function (Tone) {
 	 * @return {Ticks}      The number of ticks which have elapsed at the time
 	 *                          given any automations. 
 	 */
-	Tone.TickSignal.prototype.getTickAtTime = function(time){
-		time = this.toSeconds(time);
-		var event = this._events.get(time);
+	Tone.TickSignal.prototype._getTickUntilEvent = function(event, time){
 		if (event === null){
 			event = {
 				"ticks" : 0,
 				"time" : 0
 			};
 		}
+		if (event.type === Tone.State.Stopped){
+			return event.ticks;
+		}
 		var val0 = this.getValueAtTime(event.time);
 		var val1 = this.getValueAtTime(time);
 		return 0.5 * (time - event.time) * (val0 + val1) + event.ticks;
+	};
+
+	/**
+	 * Returns the tick value at the time. Takes into account
+	 * any automation curves scheduled on the signal.
+	 * @param  {Time} time The time to get the tick count at
+	 * @return {Ticks}      The number of ticks which have elapsed at the time
+	 *                          given any automations. 
+	 */
+	Tone.TickSignal.prototype.getTickAtTime = function(time){
+		time = this.toSeconds(time);
+		var event = this._events.get(time);
+		return this._getTickUntilEvent(event, time);
 	};
 
 	/**
@@ -124,9 +211,7 @@ define(["Tone/core/Tone", "Tone/signal/TimelineSignal"], function (Tone) {
 	Tone.TickSignal.prototype.getTimeOfTick = function(tick){
 		var before = this._events.get(tick, "ticks");
 		var after = this._events.getAfter(tick, "ticks");
-		if (tick === 0){
-			return 0;
-		} else if (before && before.ticks === tick){
+		if (before && before.ticks === tick){
 			return before.time;
 		} else if (before && after && 
 			after.type === Tone.TimelineSignal.Type.Linear && 
