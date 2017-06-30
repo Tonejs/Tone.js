6a9dc43b6c3a0a43af684066334f0cbbb707ec2f
Yotam Mann <yotammann@gmail.com>
2017-06-30 15:38:14 -0400

    changing log level to debug travis failures

--- gulp/karma.conf.js
+++ gulp/karma.conf.js
@@ -83,7 +83,7 @@ module.exports = function(config) {
 
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_ERROR,
+		logLevel: config.LOG_INFO,
 
 
 		// enable / disable watching file and executing tests whenever any file changes
