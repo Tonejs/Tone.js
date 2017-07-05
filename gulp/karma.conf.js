ab8b5af328ef301f50f7a48c5a36709960a36407
Yotam Mann <yotammann@gmail.com>
2017-07-05 17:45:36 -0400

    using dots reporter

--- gulp/karma.conf.js
+++ gulp/karma.conf.js
@@ -44,7 +44,7 @@ module.exports = function(config) {
 		// test results reporter to use
 		// possible values: 'dots', 'progress'
 		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
-		reporters: ['progress', 'coverage'],
+		reporters: ['dots', 'coverage'],
 
 		coverageReporter: {
 			type : 'lcov',
@@ -83,7 +83,7 @@ module.exports = function(config) {
 
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-		logLevel: config.LOG_INFO,
+		logLevel: config.LOG_ERROR,
 
 
 		// enable / disable watching file and executing tests whenever any file changes
