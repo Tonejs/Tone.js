5baa735f9acd9b3e77b8129e1395335b96ed1e8c
Yotam Mann <yotammann@gmail.com>
2017-08-08 19:57:38 -0400

    running coveralls either way

--- gulp/success.sh
+++ gulp/success.sh
@@ -2,10 +2,11 @@
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 
+	# only commit the builds when not a PR
 	gulp commitDevBuild
-
-	gulp coveralls
-
 	gulp commitJSDocs
 		
 fi
+
+# do coveralls either way
+gulp coveralls
