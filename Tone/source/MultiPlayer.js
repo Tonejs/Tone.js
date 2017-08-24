7ac78d6ee3efa62d503e12555fe433550c10ae97
k23Burke <k23burke@gmail.com>
2017-08-23 23:01:01 -0400

    MultiPlayer fixes

--- Tone/source/MultiPlayer.js
+++ Tone/source/MultiPlayer.js
@@ -194,6 +194,7 @@ function (Tone) {
 			for (var i = 0; i < sources.length; i++){
 				sources[i].stop(time);
 			}
+			this._activeSources[bufferName] = [];
 		}
 		return this;
 	};
@@ -221,7 +222,10 @@ function (Tone) {
 	 */
 	Object.defineProperty(Tone.MultiPlayer.prototype, "state", {
 		get : function(){
-			return this._activeSources.length > 0 ? Tone.State.Started : Tone.State.Stopped;
+			for (var key in this._activeSources) {
+        if (this._activeSources[key].length) { return Tone.State.Started; }
+	    }
+	    return Tone.State.Stopped;
 		}
 	});
 
