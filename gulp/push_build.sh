37c6c796287b2846a41e983842701c7396491a2a
Yotam Mann <yotammann@gmail.com>
2017-08-30 12:45:31 -0400

    pushing version folder for builds on master

--- gulp/push_build.sh
+++ gulp/push_build.sh
@@ -7,9 +7,6 @@ TONE_DIR=$(pwd)/..
 
 BUILD_DIR=$TMP_DIR/build
 
-echo tone dir: $TONE_DIR
-echo build dir: $BUILD_DIR
-
 # clone the build repo
 if [ "$TRAVIS" = "true" ]; then
 	GITHUB_USER=${GH_TOKEN}@
@@ -19,10 +16,9 @@ git clone https://${GITHUB_USER}github.com/Tonejs/build $BUILD_DIR > /dev/null 2
 cd $BUILD_DIR
 git checkout gh-pages
 
-echo travis branch: $TRAVIS_BRANCH
-
 # generate a new build
 gulp build
+
 
 # push to the appropriate location
 if [ "$TRAVIS" = "true" ]; then
@@ -31,13 +27,17 @@ if [ "$TRAVIS" = "true" ]; then
 	if [ "$TRAVIS_BRANCH" = "dev" ]; then
 
 		# dev builds go into the dev folder
-		mkdir $BUILD_DIR/test/
-		cp -a $TONE_DIR/build/. $BUILD_DIR/test/
+		cp -a $TONE_DIR/build/. $BUILD_DIR/dev/
 
 	elif [ "$TRAVIS_BRANCH" = "master" ]; then
 
 		# master builds are on the root level folder
 		cp -a $TONE_DIR/build/. $BUILD_DIR/
+
+		# and also in a folder with the version name
+		VERSION=$(node $TONE_DIR/gulp/version.js $TONE_DIR)
+		mkdir $BUILD_DIR/$VERSION
+		cp -a $TONE_DIR/build/. $BUILD_DIR/$VERSION
 
 	fi
 
