664dd60554fcfbcca3eebfc098ebe327c93974af
Yotam Mann <yotammann@gmail.com>
2017-09-09 18:56:03 -0400

    using most recent version number between dev and master

--- gulp/increment_version.js
+++ gulp/increment_version.js
@@ -1,17 +1,25 @@
 const fs = require('fs')
+const semver = require('semver')
 const child_process = require('child_process')
 
-const dev = process.env.TRAVIS && process.env.TRAVIS_BRANCH === 'dev' || true
+const devVersion = child_process.execSync('npm show tone@next version').toString()
+const masterVersion = child_process.execSync('npm show tone version').toString()
 
-let version = child_process.execSync(`npm show tone${dev?'@next':''} version`).toString()
+//go with whichever is the latest version
+let version = masterVersion
+if (semver.gt(devVersion, masterVersion)){
+	version = devVersion
+}
+
 version = version.split('.')
 //increment the patch
 version[2] = parseInt(version[2]) + 1
 //put it back in semver
 version = version.join('.')
+console.log(`incrementing to version ${version}`)
 
 //write it to the package.json
-const packageFile = '../package.json' 
+const packageFile = '../package.json'
 const package = JSON.parse(fs.readFileSync(packageFile, 'utf-8'))
 package.version = version
 fs.writeFileSync(packageFile, JSON.stringify(package, undefined, '  '))
