749c1e64cb48d3ff44d150f6bdb13d485d2d965d
Yotam Mann <yotammann@gmail.com>
2017-10-26 13:58:24 -0400

    ignoring shims from coverage

--- gulp/karma.conf.js
+++ gulp/karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function(config) {
 		// preprocess matching files before serving them to the browser
 		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'Tone/*/*.js': ['coverage']
+			'Tone/!(shim)/*.js': ['coverage']
 		},
 
 
