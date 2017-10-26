255b20cebbb95fdfbb2279831445b5458c706077
Yotam Mann <yotammann@gmail.com>
2017-10-26 11:33:42 -0400

    signal uses ConstantSourceNode (shimmed when not available)

--- Tone/signal/Signal.js
+++ Tone/signal/Signal.js
@@ -1,29 +1,31 @@
-define(["Tone/core/Tone", "Tone/signal/WaveShaper", "Tone/type/Type", "Tone/core/Param", "Tone/core/Gain"], function(Tone){
+define(["Tone/core/Tone", "Tone/signal/WaveShaper", "Tone/type/Type", "Tone/core/Param",
+	"Tone/shim/ConstantSourceNode", "Tone/core/Gain"], function(Tone){
 
 	"use strict";
 
 	/**
 	 *  @class  A signal is an audio-rate value. Tone.Signal is a core component of the library.
 	 *          Unlike a number, Signals can be scheduled with sample-level accuracy. Tone.Signal
-	 *          has all of the methods available to native Web Audio 
+	 *          has all of the methods available to native Web Audio
 	 *          [AudioParam](http://webaudio.github.io/web-audio-api/#the-audioparam-interface)
-	 *          as well as additional conveniences. Read more about working with signals 
+	 *          as well as additional conveniences. Read more about working with signals
 	 *          [here](https://github.com/Tonejs/Tone.js/wiki/Signals).
 	 *
 	 *  @constructor
 	 *  @extends {Tone.Param}
 	 *  @param {Number|AudioParam} [value] Initial value of the signal. If an AudioParam
 	 *                                     is passed in, that parameter will be wrapped
-	 *                                     and controlled by the Signal. 
-	 *  @param {string} [units=Number] unit The units the signal is in. 
+	 *                                     and controlled by the Signal.
+	 *  @param {string} [units=Number] unit The units the signal is in.
 	 *  @example
 	 * var signal = new Tone.Signal(10);
 	 */
 	Tone.Signal = function(){
 
 		var options = Tone.defaults(arguments, ["value", "units"], Tone.Signal);
-		var gainNode = Tone.context.createGain();
-		options.param = gainNode.gain;
+		var constantSource = Tone.context.createConstantSource();
+		constantSource.start(0);
+		options.param = constantSource.offset;
 		Tone.Param.call(this, options);
 
 		/**
@@ -31,17 +33,14 @@ define(["Tone/core/Tone", "Tone/signal/WaveShaper", "Tone/type/Type", "Tone/core
 		 * @type {GainNode}
 		 * @private
 		 */
-		this.output = gainNode;
+		this.output = constantSource;
 
 		/**
 		 * The node where the value is set.
 		 * @type {Tone.Param}
 		 * @private
 		 */
-		this.input = this._param = this.output.gain;
-
-		//connect the const output to the node output
-		this.context.getConstant(1).connect(this.output);
+		this.input = this._param = this.output.offset;
 	};
 
 	Tone.extend(Tone.Signal, Tone.Param);
@@ -59,12 +58,12 @@ define(["Tone/core/Tone", "Tone/signal/WaveShaper", "Tone/type/Type", "Tone/core
 	};
 
 	/**
-	 *  When signals connect to other signals or AudioParams, 
-	 *  they take over the output value of that signal or AudioParam. 
-	 *  For all other nodes, the behavior is the same as a default <code>connect</code>. 
+	 *  When signals connect to other signals or AudioParams,
+	 *  they take over the output value of that signal or AudioParam.
+	 *  For all other nodes, the behavior is the same as a default <code>connect</code>.
 	 *
 	 *  @override
-	 *  @param {AudioParam|AudioNode|Tone.Signal|Tone} node 
+	 *  @param {AudioParam|AudioNode|Tone.Signal|Tone} node
 	 *  @param {number} [outputNumber=0] The output number to connect from.
 	 *  @param {number} [inputNumber=0] The input number to connect to.
 	 *  @returns {Tone.SignalBase} this
