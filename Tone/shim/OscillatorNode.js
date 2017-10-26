305fdc02da5163f38cb2daee017bbd027a4e7c89
Yotam Mann <yotammann@gmail.com>
2017-10-25 23:00:37 -0400

    moving shim to another file

---
+++ Tone/shim/OscillatorNode.js
@@ -0,0 +1,12 @@
+define(["Tone/core/Tone"], function(Tone){
+
+	if (Tone.supported){
+		if (!OscillatorNode.prototype.setPeriodicWave){
+			OscillatorNode.prototype.setPeriodicWave = OscillatorNode.prototype.setWaveTable;
+		}
+		if (!AudioContext.prototype.createPeriodicWave){
+			AudioContext.prototype.createPeriodicWave = AudioContext.prototype.createWaveTable;
+		}
+	}
+
+});
