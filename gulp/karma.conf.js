eaa4587eae73017a66846f93569c1053feeebb4b
Yotam Mann <yotammann@gmail.com>
2017-11-21 10:05:32 -0800

    returning to just Chrome tests

--- gulp/karma.conf.js
+++ gulp/karma.conf.js
@@ -92,8 +92,8 @@ module.exports = function(config) {
 
 		// start these browsers
 		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-		// browsers: ['HeadlessChrome', 'HeadlessFirefox'],
-		browsers: ['HeadlessFirefox'],
+		browsers: ['HeadlessChrome'],
+		// browsers: ['HeadlessFirefox'],
 
 
 		// Continuous Integration mode
