167af325b5a2649e7af98ec78d16d542a1d67c1d
Yotam Mann <yotammann@gmail.com>
2017-12-15 12:48:46 -0500

    decodeAudioData promise shim

--- Tone/shim/AudioContext.js
+++ Tone/shim/AudioContext.js
@@ -1,4 +1,4 @@
-define(["Tone/core/Tone"], function(Tone){
+define(["Tone/core/Tone", "Tone/shim/OfflineAudioContext"], function(Tone){
 
 	if (Tone.supported){
 		if (!window.hasOwnProperty("AudioContext") && window.hasOwnProperty("webkitAudioContext")){
@@ -31,6 +31,30 @@ define(["Tone/core/Tone"], function(Tone){
 		//createDelay
 		if (!AudioContext.prototype.createDelay && AudioContext.prototype.createDelayNode){
 			AudioContext.prototype.createDelay = AudioContext.prototype.createDelayNode;
+		}
+
+		//test decodeAudioData returns a promise
+		//https://github.com/mohayonao/web-audio-api-shim/blob/master/src/AudioContext.js
+		//@mohayonao MIT
+		var decodeAudioDataPromise = false
+		var offlineContext = new OfflineAudioContext(1, 1, 44100);
+		var audioData = new Uint32Array([ 1179011410, 48, 1163280727, 544501094, 16, 131073, 44100, 176400, 1048580, 1635017060, 8, 0, 0, 0, 0 ]).buffer;
+		try {
+			var ret = offlineContext.decodeAudioData(audioData);
+			if (ret instanceof Promise){
+				decodeAudioDataPromise = true;
+			}
+		} catch (e) {
+			decodeAudioDataPromise = false;
+		}
+
+		if (!decodeAudioDataPromise) {
+			AudioContext.prototype._native_decodeAudioData = AudioContext.prototype.decodeAudioData;
+			AudioContext.prototype.decodeAudioData = function(audioData){
+				return new Promise(function(success, error){
+					this._native_decodeAudioData(audioData, success, error);
+				}.bind(this));
+			};
 		}
 	}
 });
