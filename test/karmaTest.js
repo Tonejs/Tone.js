a84b2ae1609885d64ccc33f80274f2feda1953de
Yotam Mann <yotammann@gmail.com>
2017-12-30 00:30:52 -0500

    adding polyfill

--- test/karmaTest.js
+++ test/karmaTest.js
@@ -1,32 +1,34 @@
 var tests = [];
 for (var file in window.__karma__.files) {
-  if (window.__karma__.files.hasOwnProperty(file)) {
-    if (file.indexOf("/base/test/") === 0 && file.indexOf("js") !== -1 &&
-    	file.indexOf("karmaTest.js") === -1 && file.indexOf("helper") === -1 &&
-    	file.indexOf("deps") === -1) {
-	  tests.push(file);
-    }
-  }
+	if (window.__karma__.files.hasOwnProperty(file)) {
+		if (file.indexOf("/base/test/") === 0 && file.indexOf("js") !== -1 &&
+		file.indexOf("karmaTest.js") === -1 && file.indexOf("helper") === -1 &&
+		file.indexOf("deps") === -1) {
+			tests.push(file);
+		}
+	}
 }
 
+tests.push("deps/babel-polyfill");
+
 requirejs.config({
-    // Karma serves files from '/base'
-    baseUrl: "/base/test",
+	// Karma serves files from '/base'
+	baseUrl : "/base/test",
 
-   paths : {
-        "Tone" : "../Tone",
-        "Test" : "./helper/Test",
-        "Examples" : "../examples/scripts/ExampleList"
-    },
-    shim: {
-        "Examples" : {
-            exports : "ExampleList"
-        }
-    },
+	paths : {
+		"Tone" : "../Tone",
+		"Test" : "./helper/Test",
+		"Examples" : "../examples/scripts/ExampleList"
+	},
+	shim : {
+		"Examples" : {
+			exports : "ExampleList"
+		}
+	},
 
-    // ask Require.js to load these files (all our tests)
-    deps: tests,
+	// ask Require.js to load these files (all our tests)
+	deps : tests,
 
-    // start test run, once Require.js is done
-    callback: window.__karma__.start
+	// start test run, once Require.js is done
+	callback : window.__karma__.start
 });
