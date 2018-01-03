a57ac25187bd857d697aa3f087e5813fa0eb9529
Yotam Mann <yotammann@gmail.com>
2018-01-03 12:03:41 -0500

    returning promise to OfflineTest defers the callback of 'render'

--- test/helper/Offline.js
+++ test/helper/Offline.js
@@ -1,12 +1,14 @@
-define(["Tone/core/Tone", "Tone/core/Offline", "helper/BufferTest", "Tone/core/Master"], 
-	function (Tone, Offline, BufferTest, Master) {
+define(["Tone/core/Tone", "Tone/core/Offline", "helper/BufferTest", "Tone/core/Master"],
+	function(Tone, Offline, BufferTest, Master) {
 
 		return function(callback, duration, channels){
 			duration = duration || 0.1;
 			channels = channels || 1;
 			return Offline(function(Transport){
 				var testFn = callback(Transport);
-				if (testFn){
+				if (testFn instanceof Promise){
+					return testFn;
+				} else if (Tone.isFunction(testFn)){
 					Transport.context.on("tick", function(){
 						testFn(Transport.now());
 					});
