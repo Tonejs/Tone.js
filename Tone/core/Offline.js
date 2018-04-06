198450e5a75825e565880c78cb1abd94ff08051f
Yotam Mann <yotammann@gmail.com>
2018-04-06 16:56:11 -0700

    note about starting on user action

--- Tone/core/Offline.js
+++ Tone/core/Offline.js
@@ -28,7 +28,9 @@ define(["Tone/core/Tone", "Tone/core/Transport", "Tone/core/Buffer", "Tone/core/
 	 *  Generate a buffer by rendering all of the Tone.js code within the callback using the OfflineAudioContext.
 	 *  The OfflineAudioContext is capable of rendering much faster than real time in many cases.
 	 *  The callback function also passes in an offline instance of Tone.Transport which can be used
-	 *  to schedule events along the Transport.
+	 *  to schedule events along the Transport. **NOTE** OfflineAudioContext has the same restrictions
+	 *  as the AudioContext in that on certain platforms (like iOS) it must be invoked by an explicit
+	 *  user action like a click or tap. 
 	 *  @param  {Function}  callback  All Tone.js nodes which are created and scheduled within this callback are recorded into the output Buffer.
 	 *  @param  {Time}  duration     the amount of time to record for.
 	 *  @return  {Promise}  The promise which is invoked with the Tone.Buffer of the recorded output.
