2ce6ba5437cb209e8782278618979941e7065458
David Sherman <thedcsherman@gmail.com>
2018-05-16 08:20:19 +0200

    Moved Supports.ONLINE_TESTING to original position

--- test/component/Meter.js
+++ test/component/Meter.js
@@ -40,58 +40,59 @@ function (Meter, Basic, Offline, Test, Signal, PassAudio, Tone, Merge, Oscillato
 					input.chain(meter, Tone.Master);
 				});
 			});
-
-			it("defaults to rms level of incoming signal", function(){
-				var meter = new Meter();
-				expect(meter.type).to.equal("rms");
-				meter.dispose();
-			});
-
-			it("throws when invalid type is set", function(){
-				var meter = new Meter();
-				expect(function() {
-					meter.type = "shouldThrow";
-				}).to.throw("Tone.Meter: invalid type: shouldThrow");
-				meter.dispose();
-
-				expect(function(){
-					new Meter({ type: "shouldThrow" });
-				}).to.throw("Tone.Meter: invalid type: shouldThrow");
-			});
-
-			it("can change type", function(){
-				var meter = new Meter();
-				expect(meter.type).to.equal("rms");
-
-				meter.type = "peak";
-				expect(meter.type).to.equal("peak");
-
-				meter.type = "rms";
-				expect(meter.type).to.equal("rms");
-
-				meter.dispose();
-			});
-
-			it("gets peak value correctly", function(){
-				var values = [1.0, -2.0, 3.0, -4.0, 5.0, -6.0, 7.0, -8.0, 9.0, -10.0];
-				var meter = new Meter();
-				var peak = meter.getPeakFloatValue(values);
-				expect(peak).to.equal(10);
-
-				meter.dispose();
-			});
-
-			// Based on https://rosettacode.org/wiki/Averages/Root_mean_square#JavaScript
-			it("gets rms value correctly", function(){
-				var values = [-1.0, 2.0, -3.0, 4.0, -5.0, 6.0, -7.0, 8.0, -9.0, 10.0];
-				var meter = new Meter();
-				var peak = meter.getRmsFloatValue(values);
-				expect(peak).to.be.closeTo(6.2048, 0.0001);
-
-				meter.dispose();
-			});
-
+			
 			if (Supports.ONLINE_TESTING) {
+				it("defaults to rms level of incoming signal", function(){
+					var meter = new Meter();
+					expect(meter.type).to.equal("rms");
+					meter.dispose();
+				});
+
+				it("throws when invalid type is set", function(){
+					var meter = new Meter();
+					expect(function() {
+						meter.type = "shouldThrow";
+					}).to.throw("Tone.Meter: invalid type: shouldThrow");
+					meter.dispose();
+
+					expect(function(){
+						new Meter({ type: "shouldThrow" });
+					}).to.throw("Tone.Meter: invalid type: shouldThrow");
+				});
+
+				it("can change type", function(){
+					var meter = new Meter();
+					expect(meter.type).to.equal("rms");
+
+					meter.type = "peak";
+					expect(meter.type).to.equal("peak");
+
+					meter.type = "rms";
+					expect(meter.type).to.equal("rms");
+
+					meter.dispose();
+				});
+
+				it("gets peak value correctly", function(){
+					var values = [1.0, -2.0, 3.0, -4.0, 5.0, -6.0, 7.0, -8.0, 9.0, -10.0];
+					var meter = new Meter();
+					var peak = meter.getPeakFloatValue(values);
+					expect(peak).to.equal(10);
+
+					meter.dispose();
+				});
+
+				// Based on https://rosettacode.org/wiki/Averages/Root_mean_square#JavaScript
+				it("gets rms value correctly", function(){
+					var values = [-1.0, 2.0, -3.0, 4.0, -5.0, 6.0, -7.0, 8.0, -9.0, 10.0];
+					var meter = new Meter();
+					var peak = meter.getRmsFloatValue(values);
+					expect(peak).to.be.closeTo(6.2048, 0.0001);
+
+					meter.dispose();
+				});
+
+			
 				it("measures the peak incoming signal", function(done){
 					var meter = new Meter({ type: "peak" });
 					var signal = new Signal(1).connect(meter);
