3538ae5b488b16306e9eb58af64b74e5607b4971
David Sherman <thedcsherman@gmail.com>
2018-05-16 10:11:33 +0200

    Moded Supports.ONLINE_TESTING to top

--- test/component/Meter.js
+++ test/component/Meter.js
@@ -7,41 +7,40 @@ function (Meter, Basic, Offline, Test, Signal, PassAudio, Tone, Merge, Oscillato
 		Basic(Meter);
 
 		context("Metering", function(){
-
-			it("handles input and output connections", function(){
-				var meter = new Meter();
-				Test.connect(meter);
-				meter.connect(Test);
-				meter.dispose();
-			});
-
-			it("handles getter/setter as Object", function(){
-				var meter = new Meter();
-				var values = {
-					"smoothing" : 0.2
-				};
-				meter.set(values);
-				expect(meter.get().smoothing).to.equal(0.2);
-				meter.dispose();
-			});
-
-			it("can be constructed with an object", function(){
-				var meter = new Meter({
-					"smoothing" : 0.3
-				});
-				expect(meter.smoothing).to.equal(0.3);
-				meter.dispose();
-			});
-
-			it("passes the audio through", function(){
-				var meter;
-				return PassAudio(function(input){
-					meter = new Meter();
-					input.chain(meter, Tone.Master);
-				});
-			});
-			
 			if (Supports.ONLINE_TESTING) {
+				it("handles input and output connections", function(){
+					var meter = new Meter();
+					Test.connect(meter);
+					meter.connect(Test);
+					meter.dispose();
+				});
+
+				it("handles getter/setter as Object", function(){
+					var meter = new Meter();
+					var values = {
+						"smoothing" : 0.2
+					};
+					meter.set(values);
+					expect(meter.get().smoothing).to.equal(0.2);
+					meter.dispose();
+				});
+
+				it("can be constructed with an object", function(){
+					var meter = new Meter({
+						"smoothing" : 0.3
+					});
+					expect(meter.smoothing).to.equal(0.3);
+					meter.dispose();
+				});
+
+				it("passes the audio through", function(){
+					var meter;
+					return PassAudio(function(input){
+						meter = new Meter();
+						input.chain(meter, Tone.Master);
+					});
+				});
+			
 				it("defaults to rms level of incoming signal", function(){
 					var meter = new Meter();
 					expect(meter.type).to.equal("rms");
