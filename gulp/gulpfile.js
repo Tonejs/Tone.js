a92f90ac5d6c13d94aa0451465f45504d3bd36ee
Yotam Mann <yotammann@gmail.com>
2018-05-19 14:30:50 -0400

    cleaning up gulpfile

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,14 +1,3 @@
-// const gutil = require("gulp-util");
-// const concat = require("gulp-concat");
-// const amdOptimize = require("amd-optimize");
-// const replace = require("gulp-replace");
-// const indent = require("gulp-indent");
-// const insert = require("gulp-insert");
-// const del = require("del");
-// const uglify = require("gulp-uglify");
-// const rename = require("gulp-rename");
-// const sass = require("gulp-ruby-sass");
-// const prefix = require("gulp-autoprefixer");
 const gulp = require("gulp");
 const glob = require("glob");
 const { execSync } = require("child_process");
@@ -21,18 +10,11 @@ const argv = require("yargs")
 	.alias("d", "dir")
 	.argv;
 
-const { version, dev } = require("../Tone/version.js");
-const KarmaServer = require("karma").Server;
-
-const VERSION = version;
-const TMP_FOLDER = "../tmp";
-
 /**
- *  BUILDING
+ *  COLLECT DEPENDENCIES
  */
-
-//collect all of the files into one file prefixed with 'require'
 gulp.task("collectDependencies", function(done){
+	//collect all of the files into one file prefixed with 'require'
 	glob("../Tone/*/*.js", function(err, files){
 		var modules = [];
 		// gutil.log(gutil.colors.magenta("files found:", files.length));
@@ -47,6 +29,22 @@ gulp.task("collectDependencies", function(done){
 		var reqString = modules.map(r => `require("${r}");`).join("\n");
 		reqString += "\nmodule.exports = require(\"Tone/core/Tone\");\n";
 		fs.writeFile("../Tone/index.js", reqString, done);
+	});
+});
+
+/**
+ *  COLLECT TESTS
+ */
+gulp.task("collectTests", function(done){
+	var tests = "../test/!(helper|deps|examples)/*.js";
+	if (argv.file){
+		tests = `../test/*/${argv.file}.js`;
+	} else if (argv.dir){
+		tests = `../test/${argv.dir}/*.js`;
+	}
+	glob(tests, (err, files) => {
+		var reqString = files.map(r => `require("${r}");`).join("\n");
+		fs.writeFile("../test/test.js", reqString, done);
 	});
 });
 
@@ -68,19 +66,6 @@ gulp.task("lint-fix", function(){
 		.pipe(eslint.format())
 		.pipe(eslint.failAfterError())
 		.pipe(gulp.dest("../Tone"));
-});
-
-gulp.task("collectTests", function(done){
-	var tests = "../test/!(helper|deps|examples)/*.js";
-	if (argv.file){
-		tests = `../test/*/${argv.file}.js`;
-	} else if (argv.dir){
-		tests = `../test/${argv.dir}/*.js`;
-	}
-	glob(tests, (err, files) => {
-		var reqString = files.map(r => `require("${r}");`).join("\n");
-		fs.writeFile("../test/test.js", reqString, done);
-	});
 });
 
 /**
