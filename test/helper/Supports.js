33f663c5fcfc82eb5470170e7dff2e1e870cd449
tambien <yotammann@gmail.com>
2019-01-27 13:33:18 -0500

    test on Chrome only, but not Chrome 71

--- test/helper/Supports.js
+++ test/helper/Supports.js
@@ -23,20 +23,15 @@ function isntVersion(browser, version){
 export default {
 	//can disconnect from a specific node
 	NODE_DISCONNECT : is("Chrome", 50),
-	//doesn't seem to support the pluck synth
-	PLUCK_SYNTH : isnt("Safari"),
 	//offline rendering matches Chrome closely
 	//chrome is the platform the files were rendered on
 	//so it is the default for continuity testing
 	CHROME_AUDIO_RENDERING : is("Chrome"),
-	//has float time domain analyser
-	ANALYZE_FLOAT_TIME_DOMAIN : AnalyserNode && typeof AnalyserNode.prototype.getFloatTimeDomainData === "function",
 	//if the tests run in focus
-	ONLINE_TESTING : isntVersion("Chrome", 71),
+	ONLINE_TESTING : is("Chrome") && isntVersion("Chrome", 71),
 	//the close method resolves a promise
 	AUDIO_CONTEXT_CLOSE_RESOLVES : isnt("Firefox") && isnt("Safari", 10),
 	//if it supports gUM testing
 	GET_USER_MEDIA : isnt("Safari"),
-	RUN_EXAMPLES : isnt("Safari", 10)
 };
 
