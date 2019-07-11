8a9c8f2edf5c133061f9dadef19bd46bff16193a
Yotam Mann <yotammann@gmail.com>
2019-07-11 17:11:07 -0400

    convert to ts

---
+++ Tone/signal/Zero.test.ts
@@ -0,0 +1,24 @@
+import { BasicTests } from "test/helper/Basic";
+import { connectTo } from "test/helper/Connect";
+import { ConstantOutput } from "test/helper/ConstantOutput";
+import { Zero } from "./Zero";
+
+describe("Zero", () => {
+
+	BasicTests(Zero);
+
+	context("Zero", () => {
+
+		it("handles output connections", () => {
+			const abs = new Zero();
+			abs.connect(connectTo());
+			abs.dispose();
+		});
+
+		it("always outputs 0", () => {
+			return ConstantOutput(() => {
+				new Zero().toMaster();
+			}, 0, 0);
+		});
+	});
+});
---
+++ Tone/signal/Zero.ts
@@ -0,0 +1,43 @@
+import { Gain } from "../core/context/Gain";
+import { connect, disconnect, ToneAudioNodeOptions } from "../core/context/ToneAudioNode";
+import { optionsFromArguments } from "../core/util/Defaults";
+import { SignalOperator } from "./SignalOperator";
+
+/**
+ *  Tone.Zero outputs 0's at audio-rate. The reason this has to be
+ *  it's own class is that many browsers optimize out Tone.Signal
+ *  with a value of 0 and will not process nodes further down the graph.
+ */
+export class Zero extends SignalOperator<ToneAudioNodeOptions> {
+	readonly name = "Zero";
+
+	/**
+	 * The gain node which connects the constant source to the output
+	 */
+	private _gain = new Gain({ context : this.context });
+	protected _internalChannels = [this._gain];
+
+	/**
+	 * Only outputs 0
+	 */
+	output = this._gain;
+
+	/**
+	 * no input node
+	 */
+	input = undefined;
+
+	constructor() {
+		super(Object.assign(optionsFromArguments(Zero.getDefaults(), arguments)));
+		connect(this.context.getConstant(0), this._gain);
+	}
+
+	/**
+	 * clean up
+	 */
+	dispose(): this {
+		super.dispose();
+		disconnect(this.context.getConstant(0), this._gain);
+		return this;
+	}
+}
