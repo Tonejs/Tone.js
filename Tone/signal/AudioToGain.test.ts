d5014d36a8556c86111f806098279903722cf1a3
Yotam Mann <yotammann@gmail.com>
2019-07-11 17:11:29 -0400

    convert to ts

---
+++ Tone/signal/AudioToGain.test.ts
@@ -0,0 +1,61 @@
+import { expect } from "chai";
+import { BasicTests } from "test/helper/Basic";
+import { connectFrom, connectTo } from "test/helper/Connect";
+import { ConstantOutput } from "test/helper/ConstantOutput";
+import { Offline } from "test/helper/Offline";
+import { Oscillator } from "../source/oscillator/Oscillator";
+import { AudioToGain } from "./AudioToGain";
+import { Signal } from "./Signal";
+import { Zero } from "./Zero";
+
+describe("AudioToGain", () => {
+
+	BasicTests(AudioToGain);
+
+	it("handles input and output connections", () => {
+		const a2g = new AudioToGain();
+		a2g.connect(connectTo());
+		connectFrom().connect(a2g);
+		a2g.dispose();
+	});
+
+	it("normalizes an oscillator to 0,1", () => {
+		return Offline(() => {
+			const osc = new Oscillator(1000);
+			const a2g = new AudioToGain();
+			osc.connect(a2g);
+			a2g.toMaster();
+		}).then(buffer => {
+			expect(buffer.min()).to.be.closeTo(0, 0.01);
+			expect(buffer.max()).to.be.closeTo(1, 0.01);
+		});
+	});
+
+	it("outputs 0.5 for an input value of 0", () => {
+		return ConstantOutput(() => {
+			const sig = new Zero();
+			const a2g = new AudioToGain();
+			sig.connect(a2g);
+			a2g.toMaster();
+		}, 0.5);
+	});
+
+	it("outputs 1 for an input value of 1", () => {
+		return ConstantOutput(() => {
+			const sig = new Signal(1);
+			const a2g = new AudioToGain();
+			sig.connect(a2g);
+			a2g.toMaster();
+		}, 1);
+	});
+
+	it("outputs 0 for an input value of -1", () => {
+		return ConstantOutput(() => {
+			const sig = new Signal(-1);
+			const a2g = new AudioToGain();
+			sig.connect(a2g);
+			a2g.toMaster();
+		}, 0);
+	});
+});
+
---
+++ Tone/signal/AudioToGain.ts
@@ -0,0 +1,45 @@
+import { ToneAudioNodeOptions } from "../core/context/ToneAudioNode";
+import { SignalOperator } from "./SignalOperator";
+import { WaveShaper } from "./WaveShaper";
+
+/**
+ *  @class AudioToGain converts an input in AudioRange [-1,1] to NormalRange [0,1].
+ *         See Tone.GainToAudio.
+ *
+ *  @extends {Tone.SignalBase}
+ *  @constructor
+ *  @example
+ *  var a2g = new AudioToGain();
+ */
+export class AudioToGain extends SignalOperator<ToneAudioNodeOptions> {
+
+	readonly name = "AudioToGain";
+
+	/**
+	 * The node which converts the audio ranges
+	 */
+	private _norm = new WaveShaper({
+		context: this.context,
+		mapping: x => (x + 1) / 2,
+	});
+	protected _internalChannels = [this._norm];
+
+	/**
+	 * The AudioRange input [-1, 1]
+	 */
+	input = this._norm;
+
+	/**
+	 * The GainRange output [0, 1]
+	 */
+	output = this._norm;
+
+	/**
+	 *  clean up
+	 */
+	dispose(): this {
+		super.dispose();
+		this._norm.dispose();
+		return this;
+	}
+}
