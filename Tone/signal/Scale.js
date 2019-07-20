0e69b97a40cc43500aa4a072179535687fe566ea
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2019-07-20 18:44:16 +0200

    Refactored Scale.js to work at audio rate.

--- Tone/signal/Scale.js
+++ Tone/signal/Scale.js
@@ -1,4 +1,5 @@
 import Tone from "../core/Tone";
+import "../signal/Subtract";
 import "../signal/Add";
 import "../signal/Multiply";
 import "../signal/Signal";
@@ -10,8 +11,8 @@ import "../signal/Signal";
  *
  *  @constructor
  *  @extends {Tone.SignalBase}
- *  @param {number} [outputMin=0] The output value when the input is 0. 
- *  @param {number} [outputMax=1]	The output value when the input is 1. 
+ *  @param {number} [outputMin=0] The output value when the input is 0.
+ *  @param {number} [outputMax=1] The output value when the input is 1.
  *  @example
  * var scale = new Tone.Scale(50, 100);
  * var signal = new Tone.Signal(0.5).connect(scale);
@@ -20,81 +21,84 @@ import "../signal/Signal";
 Tone.Scale = function(outputMin, outputMax){
 
 	Tone.SignalBase.call(this);
-	
-	/** 
-	 *  @private
-	 *  @type {number}
-	 */
-	this._outputMin = Tone.defaultArg(outputMin, 0);
+	this.createInsOuts(1, 1);
 
-	/** 
+	/**
 	 *  @private
-	 *  @type {number}
+	 *  @type {Tone.Signal}
 	 */
-	this._outputMax = Tone.defaultArg(outputMax, 1);
+	this._outputMin = new Tone.Signal(Tone.defaultArg(outputMin, 0));
 
-	/** 
+	/**
+	 *  @private
+	 *  @type {Tone.Signal}
+	 */
+	this._outputMax = new Tone.Signal(Tone.defaultArg(outputMax, 1));
+
+	/**
+	 *  @private
+	 *  @type {Tone.Subtract}
+	 */
+	this._sub = new Tone.Subtract();
+
+	/**
 	 *  @private
 	 *  @type {Tone.Multiply}
-	 *  @private
 	 */
-	this._scale = this.input = new Tone.Multiply(1);
-	
-	/** 
+	this._multiply = new Tone.Multiply();
+
+	/**
 	 *  @private
 	 *  @type {Tone.Add}
-	 *  @private
 	 */
-	this._add = this.output = new Tone.Add(0);
+	this._add = new Tone.Add(Tone.defaultArg(outputMin, 0));
 
-	this._scale.connect(this._add);
-	this._setRange();
+	// build audio graph
+	this._outputMax.connect(this._sub, 0, 0);
+	this._outputMin.connect(this._sub, 0, 1);
+
+	Tone.connect(this.input, this._multiply);
+	this._sub.connect(this._multiply, 0, 1);
+
+	this._multiply.connect(this._add);
+
+	Tone.connect(this._add, this.output);
 };
 
 Tone.extend(Tone.Scale, Tone.SignalBase);
 
 /**
- * The minimum output value. This number is output when 
- * the value input value is 0. 
+ * The minimum output value. This number is output when
+ * the value input value is 0.
  * @memberOf Tone.Scale#
  * @type {number}
  * @name min
  */
 Object.defineProperty(Tone.Scale.prototype, "min", {
 	get : function(){
-		return this._outputMin;
+		return this._outputMin.value;
 	},
 	set : function(min){
-		this._outputMin = min;
-		this._setRange();
+		this._outputMin.value = min;
+		this._add.value = min;
 	}
 });
 
 /**
- * The maximum output value. This number is output when 
- * the value input value is 1. 
+ * The maximum output value. This number is output when
+ * the value input value is 1.
  * @memberOf Tone.Scale#
  * @type {number}
  * @name max
  */
 Object.defineProperty(Tone.Scale.prototype, "max", {
 	get : function(){
-		return this._outputMax;
+		return this._outputMax.value;
 	},
 	set : function(max){
-		this._outputMax = max;
-		this._setRange();
+		this._outputMax.value = max;
 	}
 });
-
-/**
- *  set the values
- *  @private
- */
-Tone.Scale.prototype._setRange = function(){
-	this._add.value = this._outputMin;
-	this._scale.value = this._outputMax - this._outputMin;
-};
 
 /**
  *  Clean up.
@@ -102,12 +106,17 @@ Tone.Scale.prototype._setRange = function(){
  */
 Tone.Scale.prototype.dispose = function(){
 	Tone.SignalBase.prototype.dispose.call(this);
+	this._outputMin.dispose();
+	this._outputMin = null;
+	this._outputMax.dispose();
+	this._outputMax = null;
+	this._sub.dispose();
+	this._sub = null;
 	this._add.dispose();
 	this._add = null;
-	this._scale.dispose();
-	this._scale = null;
+	this._multiply.dispose();
+	this._multiply = null;
 	return this;
-}; 
+};
 
 export default Tone.Scale;
-
