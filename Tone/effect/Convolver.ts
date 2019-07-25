1874662060acae04d56a6b2c616e234406218e68
Yotam Mann <yotammann@gmail.com>
2019-07-25 00:10:46 -0400

    converting Convolver to ts

---
+++ Tone/effect/Convolver.ts
@@ -0,0 +1,124 @@
+import { ToneAudioBuffer } from "../core/context/ToneAudioBuffer";
+import { optionsFromArguments } from "../core/util/Defaults";
+import { noOp } from "../core/util/Interface";
+import { Effect, EffectOptions } from "./Effect";
+
+interface ToneConvolverOptions extends EffectOptions {
+	onload: () => void;
+	normalize: boolean;
+	url?: string | AudioBuffer | ToneAudioBuffer;
+}
+
+/**
+ * Convolver is a wrapper around the Native Web Audio
+ * [ConvolverNode](http://webaudio.github.io/web-audio-api/#the-convolvernode-interface).
+ * Convolution is useful for reverb and filter emulation. Read more about convolution reverb on
+ * [Wikipedia](https://en.wikipedia.org/wiki/Convolution_reverb).
+ *
+ *  @param url The URL of the impulse response or the Tone.Buffer contianing the impulse response.
+ *  @param onload The callback to invoke when the url is loaded.
+ *  @example
+ * //initializing the convolver with an impulse response
+ * var convolver = new Convolver("./path/to/ir.wav").toDestination();
+ */
+export class Convolver extends Effect<ToneConvolverOptions> {
+
+	name = "Convolver";
+
+	/**
+	 *  The native ConvolverNode
+	 */
+	private _convolver: ConvolverNode = this.context.createConvolver();
+
+	/**
+	 *  The Buffer belonging to the convolver
+	 */
+	private _buffer: ToneAudioBuffer;
+
+	constructor(url?: string | AudioBuffer | ToneAudioBuffer, onload?: () => void);
+	constructor(options?: Partial<ToneConvolverOptions>);
+	constructor() {
+
+		super(optionsFromArguments(Convolver.getDefaults(), arguments, ["url", "onload"]));
+		const options = optionsFromArguments(Convolver.getDefaults(), arguments, ["url", "onload"]);
+
+		this._buffer = new ToneAudioBuffer(options.url, buffer => {
+			this.buffer = buffer;
+			options.onload();
+		});
+
+		// set if it's already loaded
+		if (this._buffer.loaded) {
+			this.buffer = this._buffer;
+		}
+
+		// initially set normalization
+		this.normalize = options.normalize;
+
+		// connect it up
+		this.connectEffect(this._convolver);
+	}
+
+	static getDefaults(): ToneConvolverOptions {
+		return Object.assign(Effect.getDefaults(), {
+			normalize : true,
+			onload : noOp,
+		});
+	}
+
+	/**
+	 * Load an impulse response url as an audio buffer.
+	 * Decodes the audio asynchronously and invokes
+	 * the callback once the audio buffer loads.
+	 * @param url The url of the buffer to load. filetype support depends on the browser.
+	 */
+	async load(url: string): Promise<void> {
+		this.buffer = await this._buffer.load(url);
+	}
+
+	/**
+	 *  The convolver's buffer
+	 */
+	get buffer(): ToneAudioBuffer | null {
+		if (this._buffer.length) {
+			return this._buffer;
+		} else {
+			return null;
+		}
+	}
+	set buffer(buffer) {
+		if (buffer) {
+			this._buffer.set(buffer);
+		}
+		// if it's already got a buffer, create a new one
+		if (this._convolver.buffer) {
+			// disconnect the old one
+			this.effectSend.disconnect();
+			this._convolver.disconnect();
+			// create and connect a new one
+			this._convolver = this.context.createConvolver();
+			this.connectEffect(this._convolver);
+		}
+		const buff = this._buffer.get();
+		this._convolver.buffer = buff ? buff : null;
+	}
+
+	/**
+	 * The normalize property of the ConvolverNode interface is a boolean that
+	 * controls whether the impulse response from the buffer will be scaled by
+	 * an equal-power normalization when the buffer attribute is set, or not.
+	 */
+	get normalize(): boolean {
+		return this._convolver.normalize;
+	}
+	set normalize(norm) {
+		this._convolver.normalize = norm;
+	}
+
+	dispose(): this {
+		super.dispose();
+		this._buffer.dispose();
+		this._convolver.disconnect();
+		return this;
+	}
+}
---
+++ Tone/effect/Effect.ts
@@ -0,0 +1,90 @@
+import { CrossFade } from "../component/channel/CrossFade";
+import { Gain } from "../core/context/Gain";
+import { InputNode, ToneAudioNode, ToneAudioNodeOptions } from "../core/context/ToneAudioNode";
+import { readOnly } from "../core/util/Interface";
+import { Signal } from "../signal/Signal";
+
+export interface EffectOptions extends ToneAudioNodeOptions {
+	wet: NormalRange;
+}
+/**
+ * 	@class  Effect is the base class for effects. Connect the effect between
+ * 	        the effectSend and effectReturn GainNodes, then control the amount of
+ * 	        effect which goes to the output using the wet control.
+ *
+ *  @constructor
+ *  @extends {Tone.AudioNode}
+ *  @param {NormalRange|Object} [wet] The starting wet value.
+ */
+export abstract class Effect<Options extends EffectOptions>
+extends ToneAudioNode<Options> {
+
+	name = "Effect";
+
+	/**
+	 *  the drywet knob to control the amount of effect
+	 */
+	private _dryWet: CrossFade = new CrossFade({ context : this.context });
+
+	/**
+	 *  The wet control is how much of the effected
+	 *  will pass through to the output. 1 = 100% effected
+	 *  signal, 0 = 100% dry signal.
+	 */
+	wet: Signal<NormalRange> = this._dryWet.fade;
+
+	/**
+	 *  connect the effectSend to the input of hte effect
+	 */
+	protected effectSend: Gain = new Gain({ context : this.context });
+
+	/**
+	 *  connect the output of the effect to the effectReturn
+	 */
+	protected effectReturn: Gain = new Gain({ context : this.context });
+
+	/**
+	 * The effect input node
+	 */
+	input: Gain = new Gain({ context : this.context });
+
+	/**
+	 * The effect output
+	 */
+	output = this._dryWet;
+
+	protected _internalChannels = [this.input, this.output, this.effectReturn, this.effectSend];
+
+	constructor(options: EffectOptions) {
+		super(options);
+
+		// connections
+		this.input.fan(this._dryWet.a, this.effectSend);
+		this.effectReturn.connect(this._dryWet.b);
+		this.wet.setValueAtTime(options.wet, 0);
+		readOnly(this, "wet");
+	}
+
+	static getDefaults(): EffectOptions {
+		return Object.assign(ToneAudioNode.getDefaults(), {
+			wet : 1,
+		});
+	}
+
+	/**
+	 *  chains the effect in between the effectSend and effectReturn
+	 */
+	protected connectEffect(effect: InputNode): this {
+		this.effectSend.chain(effect, this.effectReturn);
+		return this;
+	}
+
+	dispose(): this {
+		super.dispose();
+		this._dryWet.dispose();
+		this.effectSend.dispose();
+		this.effectReturn.dispose();
+		this.wet.dispose();
+		return this;
+	}
+}
