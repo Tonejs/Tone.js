3e6484eef6a2b53451fa5ce0b69962e97e1cf41d
Yotam Mann <yotammann@gmail.com>
2019-07-25 10:45:27 -0400

    converting Merge to ts

---
+++ Tone/component/channel/Merge.test.ts
@@ -0,0 +1,59 @@
+import { expect } from "chai";
+import { BasicTests } from "test/helper/Basic";
+import { connectFrom, connectTo } from "test/helper/Connect";
+import { Offline } from "test/helper/Offline";
+import { PassAudio } from "test/helper/PassAudio";
+import { Signal } from "Tone/signal/Signal";
+import { Merge } from "./Merge";
+
+describe("Merge", () => {
+
+	BasicTests(Merge);
+
+	context("Merging", () => {
+
+		it("handles input and output connections", () => {
+			const merge = new Merge();
+			connectFrom().connect(merge);
+			merge.connect(connectTo());
+			merge.dispose();
+		});
+
+		it("defaults to two channels", () => {
+			const merge = new Merge();
+			expect(merge.numberOfInputs).to.equal(2);
+			merge.dispose();
+		});
+
+		it("can pass in more channels", () => {
+			const merge = new Merge(4);
+			expect(merge.numberOfInputs).to.equal(4);
+			connectFrom().connect(merge, 0, 0);
+			connectFrom().connect(merge, 0, 1);
+			connectFrom().connect(merge, 0, 2);
+			connectFrom().connect(merge, 0, 3);
+			merge.dispose();
+		});
+
+		it("passes the incoming signal through", () => {
+			return PassAudio((input) => {
+				const merge = new Merge().toDestination();
+				input.connect(merge);
+			});
+		});
+
+		it("merge two signal into one stereo signal", () => {
+			return Offline(() => {
+				const sigL = new Signal(1);
+				const sigR = new Signal(2);
+				const merger = new Merge();
+				sigL.connect(merger, 0, 0);
+				sigR.connect(merger, 0, 1);
+				merger.toDestination();
+			}, 0.1, 2).then(buffer => {
+				expect(buffer.toArray()[0][0]).to.be.closeTo(1, 0.001);
+				expect(buffer.toArray()[1][0]).to.be.closeTo(2, 0.001);
+			});
+		});
+	});
+});
---
+++ Tone/component/channel/Merge.ts
@@ -0,0 +1,70 @@
+import { ToneAudioNode, ToneAudioNodeOptions } from "Tone/core/context/ToneAudioNode";
+import { optionsFromArguments } from "Tone/core/util/Defaults";
+
+interface MergeOptions extends ToneAudioNodeOptions {
+	channels: Positive;
+}
+
+/**
+ * Merge brings multiple mono input channels into a single multichannel output channel.
+ *
+ * @param channels The number of channels to merge.
+ * @example
+ * var merge = new Merge().toMaster();
+ * //routing a sine tone in the left channel
+ * //and noise in the right channel
+ * var osc = new Tone.Oscillator().connect(merge.left);
+ * var noise = new Tone.Noise().connect(merge.right);
+ * //starting our oscillators
+ * noise.start();
+ * osc.start();
+ */
+export class Merge extends ToneAudioNode<MergeOptions> {
+
+	name = "Merge";
+
+	/**
+	 *  The merger node for the two channels.
+	 */
+	private _merger: ChannelMergerNode;
+
+	/**
+	 * The output is the input channels combined into a sigle (multichannel) output
+	 */
+	output: AudioNode;
+
+	/**
+	 * Multiple input connections combine into a single output.
+	 */
+	input: AudioNode;
+
+	protected _internalChannels = [];
+
+	constructor(channels?: Positive);
+	// tslint:disable-next-line: unified-signatures
+	constructor(options?: Partial<MergeOptions>);
+	constructor() {
+		super(optionsFromArguments(Merge.getDefaults(), arguments, ["channels"]));
+		const options = optionsFromArguments(Merge.getDefaults(), arguments, ["channels"]);
+
+		this._merger = this.output = this.input = this.context.createChannelMerger(options.channels);
+
+		// @ts-ignore
+		this.numberOfInputs = options.channels;
+		// @ts-ignore
+		this.numberOfOutputs = 1;
+	}
+
+	static getDefaults(): MergeOptions {
+		return Object.assign(ToneAudioNode.getDefaults(), {
+			channels: 2,
+			numberOfOutputs : 1,
+		});
+	}
+
+	dispose(): this {
+		super.dispose();
+		this._merger.disconnect();
+		return this;
+	}
+}
