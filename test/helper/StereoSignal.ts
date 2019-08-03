3e625e4a09de6c78c9853caaa302ba9d1937f1b0
Yotam Mann <yotammann@gmail.com>
2019-08-02 21:09:48 -0400

    StereoSignal helper

---
+++ test/helper/StereoSignal.ts
@@ -0,0 +1,9 @@
+import { Merge } from "Tone/component/channel/Merge";
+import { Signal } from "Tone/signal/Signal";
+
+export function StereoSignal(l: number, r: number): Merge {
+	const merge = new Merge();
+	new Signal(l).connect(merge, 0, 0);
+	new Signal(r).connect(merge, 0, 1);
+	return merge;
+}
