8af43901bff448468e9ce5b2e680b14a100cb43b
Yotam Mann <yotammann@gmail.com>
2019-08-04 13:12:39 -0400

    exposing utils, units and param

--- Tone/core/index.ts
+++ Tone/core/index.ts
@@ -1,4 +1,5 @@
 export { ToneAudioNode } from "./context/ToneAudioNode";
+export { Param } from "./context/Param";
 export { Gain } from "./context/Gain";
 export { Context } from "./context/Context";
 export { OfflineContext } from "./context/OfflineContext";
@@ -10,6 +11,7 @@ export { Clock } from "./clock/Clock";
 export { Frequency } from "./type/Frequency";
 export { Time } from "./type/Time";
 export { Ticks } from "./type/Ticks";
+export { Midi } from "./type/Midi";
 export { TransportTime } from "./type/TransportTime";
 export { Draw } from "./util/Draw";
 export { StateTimeline } from "./util/StateTimeline";
@@ -20,4 +22,10 @@ export { Tone } from "./Tone";
 export { Destination } from "./context/Destination";
 export { Transport } from "./clock/Transport";
 export { dbToGain, gainToDb, intervalToFrequencyRatio } from "./type/Conversions";
+export { optionsFromArguments, defaultArg } from "./util/Defaults";
 export * from "./Connect";
+export * from "./util/TypeCheck";
+
+// get the units and export them under the "Unit" namespace
+import * as Unit from "./type/Units";
+export { Unit };
