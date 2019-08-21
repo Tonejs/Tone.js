ff5360f4087cb2d3f8c912413cf3f22cc3e564db
Yotam Mann <yotammann@gmail.com>
2019-08-20 22:50:12 -0700

    converting Panner to ts

---
+++ Tone/component/channel/Panner.ts
@@ -0,0 +1,69 @@
+import { Param } from "../../core/context/Param";
+import { ToneAudioNode, ToneAudioNodeOptions } from "../../core/context/ToneAudioNode";
+import { AudioRange, NormalRange } from "../../core/type/Units";
+import { optionsFromArguments } from "../../core/util/Defaults";
+import { readOnly } from "../../core/util/Interface";
+
+interface TonePannerOptions extends ToneAudioNodeOptions {
+	pan: AudioRange;
+}
+
+/**
+ * Panner is an equal power Left/Right Panner. It is a wrapper around the StereoPannerNode.
+ * @param pan The initail panner value (center).
+ * @example
+ * //pan the input signal hard right.
+ * var panner = new Panner(1);
+ */
+export class Panner extends ToneAudioNode<TonePannerOptions> {
+
+	readonly name = "Panner";
+
+	/**
+	 *  the panner node
+	 */
+	private _panner: StereoPannerNode = this.context.createStereoPanner();
+	readonly input: StereoPannerNode = this._panner;
+	readonly output: StereoPannerNode = this._panner;
+
+	/**
+	 *  The pan control. -1 = hard left, 1 = hard right.
+	 */
+	readonly pan: Param<AudioRange>;
+
+	constructor(options?: Partial<TonePannerOptions>);
+	// tslint:disable-next-line: unified-signatures
+	constructor(fade?: NormalRange);
+	constructor() {
+		super(Object.assign(optionsFromArguments(Panner.getDefaults(), arguments, ["pan"])));
+		const options = optionsFromArguments(Panner.getDefaults(), arguments, ["pan"]);
+
+		this.pan = new Param({
+			context: this.context,
+			param: this._panner.pan,
+			value: options.pan,
+		});
+
+		// this is necessary for standardized-audio-context
+		// doesn't make any difference for the native AudioContext
+		// https://github.com/chrisguttandin/standardized-audio-context/issues/647
+		this._panner.channelCount = 1;
+		this._panner.channelCountMode = "explicit";
+
+		// initial value
+		readOnly(this, "pan");
+	}
+
+	static getDefaults(): TonePannerOptions {
+		return Object.assign(ToneAudioNode.getDefaults(), {
+			pan: 0,
+		});
+	}
+
+	dispose(): this {
+		super.dispose();
+		this._panner.disconnect();
+		this.pan.dispose();
+		return this;
+	}
+}
