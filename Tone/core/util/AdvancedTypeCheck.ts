a54ab4ea68afb7fea7d5bdcb142740f9fb1ae691
Yotam Mann <yotammann@gmail.com>
2019-08-26 16:52:21 -0700

    using typechecks from standardized-audio-context

--- Tone/core/util/AdvancedTypeCheck.ts
+++ Tone/core/util/AdvancedTypeCheck.ts
@@ -1,13 +1,13 @@
-import { isAnyAudioNode } from "standardized-audio-context";
-import { isFunction } from "./TypeCheck";
+import {
+	isAnyAudioContext, isAnyAudioNode,
+	isAnyAudioParam, isAnyOfflineAudioContext,
+} from "standardized-audio-context";
 
 /**
  * Test if the given value is an instanceof AudioParam
  */
 export function isAudioParam(arg: any): arg is AudioParam {
-	return arg instanceof Object &&  Reflect.has(arg, "value") &&
-		!Reflect.has(arg, "input") &&
-		isFunction(arg.setValueAtTime);
+	return isAnyAudioParam(arg);
 }
 
 /**
@@ -21,22 +21,19 @@ export function isAudioNode(arg: any): arg is AudioNode {
  * Test if the arg is instanceof an OfflineAudioContext
  */
 export function isOfflineAudioContext(arg: any): arg is OfflineAudioContext {
-	return arg instanceof Object &&  Reflect.has(arg, "destination") &&
-		isFunction(arg.startRendering) && !Reflect.has(arg, "rawContext");
+	return isAnyOfflineAudioContext(arg);
 }
 
 /**
  * Test if the arg is an instanceof AudioContext
  */
 export function isAudioContext(arg: any): arg is AudioContext {
-	return arg instanceof Object &&  Reflect.has(arg, "destination") &&
-		isFunction(arg.close) && isFunction(arg.resume) && !Reflect.has(arg, "rawContext");
+	return isAnyAudioContext(arg);
 }
 
 /**
  * Test if the arg is instanceof an AudioBuffer
  */
 export function isAudioBuffer(arg: any): arg is AudioBuffer {
-	return arg instanceof Object &&  Reflect.has(arg, "sampleRate")
-		&& Reflect.has(arg, "duration") && !isFunction(arg.load);
+	return arg instanceof AudioBuffer;
 }
