2ff888d3b3c792331c9b3340daa70a873a2b5629
Yotam Mann <yotammann@gmail.com>
2019-08-30 09:28:45 -0700

    adding type classes
    and basic sanity check

--- Tone/fromContext.ts
+++ Tone/fromContext.ts
@@ -2,6 +2,11 @@ import * as Classes from "./classes";
 import { Transport } from "./core/clock/Transport";
 import { Context } from "./core/context/Context";
 import { Destination } from "./core/context/Destination";
+import { FrequencyClass } from "./core/type/Frequency";
+import { MidiClass } from "./core/type/Midi";
+import { TicksClass } from "./core/type/Ticks";
+import { TimeClass } from "./core/type/Time";
+import { TransportTimeClass } from "./core/type/TransportTime";
 import { isDefined, isFunction } from "./core/util/TypeCheck";
 
 /**
@@ -13,6 +18,14 @@ type Tone = {
 	Destination: Destination;
 	now: () => number;
 } & typeof Classes;
+
+/**
+ * Bind the TimeBaseClass to the context
+ */
+// tslint:disable-next-line: typedef
+function bindTypeClass(context: Context, type) {
+	return (...args: any[]) => new type(context, ...args);
+}
 
 /**
  * Return an object with all of the classes bound to the passed in context
@@ -36,5 +49,12 @@ export function fromContext(context: Context): Tone {
 	toneFromContext.now = () => context.now();
 	toneFromContext.Transport = context.transport;
 	toneFromContext.Destination = context.destination;
+	// add the type classes
+	toneFromContext.Midi = bindTypeClass(context, MidiClass);
+	toneFromContext.Time = bindTypeClass(context, TimeClass);
+	toneFromContext.Frequency = bindTypeClass(context, FrequencyClass);
+	toneFromContext.Ticks = bindTypeClass(context, TicksClass);
+	toneFromContext.TransportTime = bindTypeClass(context, TransportTimeClass);
+	// return the object
 	return toneFromContext as Tone;
 }
