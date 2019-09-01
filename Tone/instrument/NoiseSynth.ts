d4b0604eb0a8e348e5df22f9e82c8cf2a9343819
Jacob Clarke <jacobclarke92@gmail.com>
2019-09-01 18:58:56 +1000

    Restored some comments

--- Tone/instrument/NoiseSynth.ts
+++ Tone/instrument/NoiseSynth.ts
@@ -14,24 +14,27 @@ interface NoiseSynthOptions extends SynthOptions {
 }
 
 /**
- *  Tone.NoiseSynth is composed of a noise generator (Tone.Noise), one filter (Tone.Filter),
- *  and two envelopes (Tone.Envelop). One envelope controls the amplitude
- *  of the noise and the other is controls the cutoff frequency of the filter.
- *  <img src="https://docs.google.com/drawings/d/1rqzuX9rBlhT50MRvD2TKml9bnZhcZmzXF1rf_o7vdnE/pub?w=918&h=242">
+ * Tone.NoiseSynth is composed of a noise generator (Tone.Noise), one filter (Tone.Filter),
+ * and two envelopes (Tone.Envelop). One envelope controls the amplitude
+ * of the noise and the other is controls the cutoff frequency of the filter.
+ * <img src="https://docs.google.com/drawings/d/1rqzuX9rBlhT50MRvD2TKml9bnZhcZmzXF1rf_o7vdnE/pub?w=918&h=242">
  *
  * @example
  * var noiseSynth = new Tone.NoiseSynth().toMaster();
  * noiseSynth.triggerAttackRelease("8n");
  */
-
 export class NoiseSynth extends Instrument<NoiseSynthOptions> {
 
 	readonly name = "NoiseSynth";
 
-	// The noise source.
+	/**
+	 * The noise source.
+	 */
 	readonly noise: Noise;
 
-	// The amplitude envelope.
+	/**
+	 * The amplitude envelope.
+	 */
 	readonly envelope: AmplitudeEnvelope;
 
 	constructor(options?: RecursivePartial<NoiseSynthOptions>)
@@ -60,9 +63,17 @@ export class NoiseSynth extends Instrument<NoiseSynthOptions> {
 		};
 	}
 
+	/**
+	 * Start the attack portion of the envelopes. Unlike other
+	 * instruments, Tone.NoiseSynth doesn't have a note.
+	 * @example
+	 * noiseSynth.triggerAttack();
+	 */
 	triggerAttack(time: Time, velocity: NormalRange = 1): this {
 		time = this.toSeconds(time);
+		// the envelopes
 		this.envelope.triggerAttack(time, velocity);
+		// start the noise
 		this.noise.start(time);
 		if (this.envelope.sustain === 0) {
 			this.noise.stop(time + this.toSeconds(this.envelope.attack) + this.toSeconds(this.envelope.decay))
@@ -70,6 +81,9 @@ export class NoiseSynth extends Instrument<NoiseSynthOptions> {
 		return this;
 	}
 
+	/**
+	 * Start the release portion of the envelopes.
+	 */
 	triggerRelease(time: Time): this {
 		time = this.toSeconds(time);
 		this.envelope.triggerRelease(time);
@@ -77,6 +91,19 @@ export class NoiseSynth extends Instrument<NoiseSynthOptions> {
 		return this;
 	}
 
+	/**
+	 * Sync the instrument to the Transport. All subsequent calls of
+	 * [triggerAttack](#triggerattack) and [triggerRelease](#triggerrelease)
+	 * will be scheduled along the transport.
+	 * @example
+	 * synth.sync()
+	 * //schedule 3 notes when the transport first starts
+	 * synth.triggerAttackRelease('8n', 0)
+	 * synth.triggerAttackRelease('8n', '8n')
+	 * synth.triggerAttackRelease('8n', '4n')
+	 * //start the transport to hear the notes
+	 * Transport.start()
+	 */
 	sync(): this {
 		this._syncMethod("triggerAttack", 0);
 		this._syncMethod("triggerRelease", 0);
