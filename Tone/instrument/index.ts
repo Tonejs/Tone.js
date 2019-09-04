8e76c0517b96369f94b1de89af0e4ca44d623ef6
Yotam Mann <yotammann@gmail.com>
2019-09-03 21:52:23 -0400

    adding MetalSynth export

--- Tone/instrument/index.ts
+++ Tone/instrument/index.ts
@@ -2,3 +2,4 @@ export { Synth } from "./Synth";
 export { Sampler } from "./Sampler";
 export { PolySynth } from "./PolySynth";
 export { MembraneSynth } from "./MembraneSynth";
+export { MetalSynth } from "./MetalSynth";
