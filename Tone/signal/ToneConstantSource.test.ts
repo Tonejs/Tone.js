2f55cc2ec2560a04dbca10cb9ddc937d51736ce1
Yotam Mann <yotammann@gmail.com>
2019-09-06 17:10:32 -0400

    wrap the native ConstantSourceNode into ToneConstantSource
    gets more consistent onended callback and other syntactic sugar

---
+++ Tone/signal/ToneConstantSource.test.ts
@@ -0,0 +1,202 @@
+import { expect } from "chai";
+import { BasicTests } from "test/helper/Basic";
+import { CompareToFile } from "test/helper/CompareToFile";
+import { Offline, whenBetween } from "test/helper/Offline";
+import { ONLINE_TESTING } from "test/helper/Supports";
+import { ToneConstantSource } from "./ToneConstantSource";
+
+describe("ToneConstantSource", () => {
+
+	BasicTests(ToneConstantSource);
+
+	context("Constructor", () => {
+
+		it("can be constructed with an offset", () => {
+			const source = new ToneConstantSource(330);
+			expect(source.offset.value).to.equal(330);
+			source.dispose();
+		});
+
+		it("can be constructed with no arguments", () => {
+			const source = new ToneConstantSource();
+			expect(source.offset.value).to.equal(1);
+			source.dispose();
+		});
+
+		it("can be constructed with an options object", () => {
+			const source = new ToneConstantSource({
+				offset: 2,
+			});
+			expect(source.offset.value).to.be.closeTo(2, 0.01);
+			source.dispose();
+		});
+
+	});
+
+	context("onended", () => {
+
+		if (ONLINE_TESTING) {
+
+			it("invokes the onended callback in the online context", (done) => {
+				const source = new ToneConstantSource();
+				source.start();
+				source.stop("+0.3");
+				const now = source.now();
+				source.onended = () => {
+					expect(source.now() - now).to.be.within(0.25, 0.5);
+					source.dispose();
+					done();
+				};
+			});
+
+			it("invokes the onended callback only once in the online context", (done) => {
+				const source = new ToneConstantSource();
+				source.start();
+				source.stop("+0.1");
+				source.stop("+0.2");
+				source.stop("+0.3");
+				const now = source.now();
+				source.onended = () => {
+					expect(source.now() - now).to.be.within(0.25, 0.5);
+					source.dispose();
+					done();
+				};
+			});
+		}
+
+		it("invokes the onended callback in the offline context", () => {
+			let wasInvoked = false;
+			return Offline(() => {
+				const source = new ToneConstantSource();
+				source.start(0);
+				source.stop(0.2);
+				source.onended = () => {
+					expect(source.now() - 0.2).to.be.closeTo(0, 0.05);
+					source.dispose();
+					wasInvoked = true;
+				};
+			}, 0.3).then(() => {
+				expect(wasInvoked).to.equal(true);
+			});
+		});
+
+		it("invokes the onended callback only once in offline context", () => {
+			let wasInvoked = false;
+			return Offline(() => {
+				const source = new ToneConstantSource();
+				source.start(0);
+				source.stop(0.1);
+				source.stop(0.2);
+				source.stop(0.3);
+				source.onended = () => {
+					expect(source.now() - 0.3).to.be.closeTo(0, 0.05);
+					source.dispose();
+					expect(wasInvoked).to.equal(false);
+					wasInvoked = true;
+				};
+			}, 0.4).then(() => {
+				expect(wasInvoked).to.equal(true);
+			});
+		});
+	});
+
+	context("Scheduling", () => {
+
+		it("throw an error if start is called multiple time", () => {
+			const source = new ToneConstantSource();
+			source.start();
+			expect(() => {
+				source.start();
+			}).to.throw();
+			source.dispose();
+		});
+
+		it("can play for a specific duration", () => {
+			return Offline(() => {
+				const source = new ToneConstantSource().toDestination();
+				source.start(0).stop(0.1);
+			}, 0.4).then(buffer => {
+				expect(buffer.getValueAtTime(0)).to.be.above(0);
+				expect(buffer.getValueAtTime(0.09)).to.be.above(0);
+				expect(buffer.getValueAtTime(0.1)).to.equal(0);
+			});
+		});
+
+		it("can call stop multiple times and takes the last value", () => {
+			return Offline(() => {
+				const source = new ToneConstantSource().toDestination();
+				source.start(0).stop(0.1).stop(0.2);
+			}, 0.4).then((buffer) => {
+				expect(buffer.getValueAtTime(0)).to.be.above(0);
+				expect(buffer.getValueAtTime(0.1)).to.be.above(0);
+				expect(buffer.getValueAtTime(0.19)).to.be.above(0);
+				expect(buffer.getValueAtTime(0.2)).to.equal(0);
+			});
+		});
+
+		if (ONLINE_TESTING) {
+
+			it("clamps start time to the currentTime", () => {
+				const source = new ToneConstantSource();
+				source.start(0);
+				const currentTime = source.context.currentTime;
+				expect(source.getStateAtTime(0)).to.equal("stopped");
+				expect(source.getStateAtTime(currentTime)).to.equal("started");
+				source.dispose();
+			});
+
+			it("clamps stop time to the currentTime", (done) => {
+				const source = new ToneConstantSource();
+				source.start(0);
+				let currentTime = source.context.currentTime;
+				expect(source.getStateAtTime(0)).to.equal("stopped");
+				expect(source.getStateAtTime(currentTime)).to.equal("started");
+				setTimeout(() => {
+					currentTime = source.context.currentTime;
+					source.stop(0);
+					expect(source.getStateAtTime(currentTime + 0.01)).to.equal("stopped");
+					source.dispose();
+					done();
+				}, 100);
+			});
+		}
+	});
+
+	context("State", () => {
+
+		it("reports the right state", () => {
+
+			return Offline(() => {
+				const source = new ToneConstantSource();
+				source.start(0);
+				source.stop(0.05);
+				return (time) => {
+					whenBetween(time, 0, 0.05, () => {
+						expect(source.state).to.equal("started");
+					});
+					whenBetween(time, 0.05, 0.1, () => {
+						expect(source.state).to.equal("stopped");
+					});
+				};
+			}, 0.1);
+		});
+
+		it("can call stop multiple times, takes the last value", () => {
+
+			return Offline(() => {
+				const source = new ToneConstantSource();
+				source.start(0);
+				source.stop(0.05);
+				source.stop(0.1);
+				return (time) => {
+					whenBetween(time, 0, 0.1, () => {
+						expect(source.state).to.equal("started");
+					});
+					whenBetween(time, 0.1, 0.2, () => {
+						expect(source.state).to.equal("stopped");
+					});
+				};
+			}, 0.2);
+		});
+	});
+});
---
+++ Tone/signal/ToneConstantSource.ts
@@ -0,0 +1,89 @@
+import { connect } from "../core/Connect";
+import { Param } from "../core/context/Param";
+import { Seconds, Time, Unit, UnitName } from "../core/type/Units";
+import { optionsFromArguments } from "../core/util/Defaults";
+import { OneShotSource, OneShotSourceOptions } from "../source/OneShotSource";
+
+export interface ToneConstantSourceOptions<Type> extends OneShotSourceOptions {
+	convert: boolean;
+	offset: Type;
+	units: UnitName;
+}
+
+/**
+ * Wrapper around the native fire-and-forget ConstantSource.
+ * Adds the ability to reschedule the stop method.
+ * ***[[Oscillator]] is better for most use-cases***
+ */
+export class ToneConstantSource<Type extends Unit = number> extends OneShotSource<ToneConstantSourceOptions<Type>> {
+
+	readonly name: string = "ToneConstantSource";
+
+	/**
+	 *  The signal generator
+	 */
+	private _source = this.context.createConstantSource();
+
+	/**
+	 *  The offset of the signal generator
+	 */
+	readonly offset: Param<Type>;
+
+	/**
+	 * @param  offset   The offset value
+	 */
+	constructor(offset: Type);
+	constructor(options?: Partial<ToneConstantSourceOptions<Type>>);
+	constructor() {
+
+		super(optionsFromArguments(ToneConstantSource.getDefaults(), arguments, ["offset"]));
+		const options = optionsFromArguments(ToneConstantSource.getDefaults(), arguments, ["offset"]);
+
+		connect(this._source, this._gainNode);
+
+		this.offset = new Param({
+			context: this.context,
+			convert: options.convert,
+			param : this._source.offset,
+			units : options.units,
+			value : options.offset,
+		});
+	}
+
+	static getDefaults(): ToneConstantSourceOptions<any> {
+		return Object.assign(OneShotSource.getDefaults(), {
+			convert: true,
+			offset: 1,
+			units: "number" as UnitName,
+		});
+	}
+
+	/**
+	 * Start the source node at the given time
+	 * @param  time When to start the source
+	 */
+	start(time?: Time): this {
+		const computedTime = this.toSeconds(time);
+		this.log("start", computedTime);
+		this._startGain(computedTime);
+		this._source.start(computedTime);
+		return this;
+	}
+
+	protected _stopSource(time?: Seconds): void {
+		this._source.stop(time);
+	}
+
+	/**
+	 *  Clean up.
+	 */
+	dispose(): this {
+		super.dispose();
+		if (this.state === "started") {
+			this.stop();
+		}
+		this._source.disconnect();
+		this.offset.dispose();
+		return this;
+	}
+}
