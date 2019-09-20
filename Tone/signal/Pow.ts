090177efdd2b7abc07100ed782aea2a0573e0295
Filipe Herculano <fifo_@live.com>
2019-09-19 20:49:02 -0400

    update Pow to extend SignalOperator instead

--- Tone/signal/Pow.ts
+++ Tone/signal/Pow.ts
@@ -1,76 +1,75 @@
-import { Signal, SignalOptions } from "./Signal";
 import { WaveShaper, WaveShaperMappingFn } from "./WaveShaper";
 import { optionsFromArguments } from "Tone";
+import { SignalOperator } from "./SignalOperator";
+import { ToneAudioNodeOptions } from "Tone/core/context/ToneAudioNode";
+
+export interface PowOptions extends ToneAudioNodeOptions {
+	value: number;
+}
 
 /**
  *  @class Pow applies an exponent to the incoming signal. The incoming signal
  *         must be AudioRange.
  *
  *  @constructor
- *  @param exp The exponent to apply to the incoming signal, must be at least 2. 
+ *  @param exponent The exponent to apply to the incoming signal, must be at least 2. 
  *  @example
  * var pow = new Pow(2);
  * var sig = new Signal(0.5).connect(pow);
  * //output of pow is 0.25. 
  */
-export class Pow extends Signal<number> {
+export class Pow extends SignalOperator<PowOptions> {
 	
 	readonly name: string = "Pow";
-
-	/**
-	 * Indicates if the value should be overridden on connection
-	 */
-	readonly override = false;
 	
-	/**
-	 * The exponent
-	 */
 	private _exponent!: number;
+
+	private _exponentScaler: WaveShaper;
+
+	input: WaveShaper;
+
+	output: WaveShaper;
 	
-	private _exponentScaler = new WaveShaper({
-		context: this.context,
-		mapping: this._expFunc(this._exponent),
-		length: 8192,
-	});
-
-	input = this._exponentScaler;
-
-	output = this._exponentScaler;
-
-
 	/**
 	 * @param value Constant exponent value to use
 	 */
 	constructor(value?: number);
-	constructor(options?: Partial<SignalOptions<number>>);
+	constructor(options?: Partial<PowOptions>);
 	constructor() {
 		super(Object.assign(optionsFromArguments(Pow.getDefaults(), arguments, ["value"])));
+		const options = optionsFromArguments(Pow.getDefaults(), arguments, ["value"]);
 
-		this.exponent = this.value;
+		this._exponentScaler = this.input = this.output = new WaveShaper({
+			context: this.context,
+			mapping: this._expFunc(this._exponent),
+			length: 8192,
+		});
+
+		this.value = options.value;
 	}
 
-	static getDefaults(): SignalOptions<number> {
-		return Object.assign(Signal.getDefaults(), {
+	static getDefaults(): PowOptions {
+		return Object.assign(SignalOperator.getDefaults(), {
 			value: 1,
 		});
 	}
 
 	/**
 	 * the function which maps the waveshaper
-	 * @param exp exponent value
+	 * @param exponent exponent value
 	 */
-	private _expFunc(exp: number): WaveShaperMappingFn {
+	private _expFunc(exponent: number): WaveShaperMappingFn {
 		return (val: number) => {
-			return Math.pow(Math.abs(val), exp);
+			return Math.pow(Math.abs(val), exponent);
 		};
 	}
 
-	get exponent(): number {
+	get value(): number {
 		return this._exponent;
 	}
 
-	set exponent(exp: number) {
-		this._exponent = exp;
+	set value(exponent: number) {
+		this._exponent = exponent;
 		this._exponentScaler.setMap(this._expFunc(this._exponent));
 	}
 
