49dfe371a3fa56f8658583c2cdf7ea06750f5567
Yotam Mann <yotammann@gmail.com>
2019-10-28 11:50:13 -0400

    exporting all core classes
    also exporting ftom and mtof

--- Tone/core/index.ts
+++ Tone/core/index.ts
@@ -1,30 +1,32 @@
-export { ToneAudioNode } from "./context/ToneAudioNode";
-export { Param } from "./context/Param";
-export { Gain } from "./context/Gain";
-export { Context } from "./context/Context";
-export { OfflineContext } from "./context/OfflineContext";
-export { Offline } from "./context/Offline";
-export { ToneAudioBuffer } from "./context/ToneAudioBuffer";
-export { ToneAudioBuffers } from "./context/ToneAudioBuffers";
-export { Delay } from "./context/Delay";
-export { Clock } from "./clock/Clock";
-export { Frequency } from "./type/Frequency";
-export { Time } from "./type/Time";
-export { Ticks } from "./type/Ticks";
-export { Midi } from "./type/Midi";
-export { TransportTime } from "./type/TransportTime";
-export { Draw } from "./util/Draw";
-export { StateTimeline } from "./util/StateTimeline";
-export { IntervalTimeline } from "./util/IntervalTimeline";
-export { Timeline } from "./util/Timeline";
-export { Emitter } from "./util/Emitter";
-export { Tone } from "./Tone";
-export { Destination } from "./context/Destination";
-export { Transport } from "./clock/Transport";
-export { dbToGain, gainToDb, intervalToFrequencyRatio } from "./type/Conversions";
-export { optionsFromArguments, defaultArg } from "./util/Defaults";
-export * from "./Connect";
+export * from "./context/Context";
+export * from "./context/Destination";
+export * from "./context/Delay";
+export * from "./context/Gain";
+export * from "./context/Offline";
+export * from "./context/OfflineContext";
+export * from "./context/Param";
+export * from "./context/ToneAudioBuffer";
+export * from "./context/ToneAudioBuffers";
+export * from "./context/ToneAudioNode";
+export * from "./clock/Clock";
+export * from "./clock/Transport";
+export * from "./type/Frequency";
+export * from "./type/Midi";
+export * from "./type/Time";
+export * from "./type/Ticks";
+export * from "./type/TransportTime";
+export * from "./util/Draw";
+export * from "./util/Emitter";
+export * from "./util/IntervalTimeline";
+export * from "./util/StateTimeline";
+export * from "./util/Timeline";
 export * from "./util/TypeCheck";
+
+export * from "./Tone";
+export * from "./Connect";
+
+export { dbToGain, gainToDb, intervalToFrequencyRatio, ftom, mtof } from "./type/Conversions";
+export { optionsFromArguments, defaultArg } from "./util/Defaults";
 
 // get the units and export them under the "Unit" namespace
 import * as Unit from "./type/Units";
