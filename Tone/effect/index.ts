6ea1ca0a180407ccbdd3bd9a11ba7918c4e4acb8
Yotam Mann <yotammann@gmail.com>
2019-10-28 19:11:46 -0400

    converting BitCrusher to typescript
    uses the AudioWorklet

--- Tone/effect/index.ts
+++ Tone/effect/index.ts
@@ -1,5 +1,6 @@
 export * from "./AutoFilter";
 export * from "./AutoPanner";
+export * from "./BitCrusher";
 export * from "./Distortion";
 export * from "./FeedbackDelay";
 export * from "./FrequencyShifter";
