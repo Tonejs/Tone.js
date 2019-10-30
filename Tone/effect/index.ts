79d2c3d41b6ae59c5e93bf9aa33c8587321a594c
Yotam Mann <yotammann@gmail.com>
2019-10-29 23:35:27 -0400

    convert Chebyshev to typescript

--- Tone/effect/index.ts
+++ Tone/effect/index.ts
@@ -2,6 +2,7 @@ export * from "./AutoFilter";
 export * from "./AutoPanner";
 export * from "./AutoWah";
 export * from "./BitCrusher";
+export * from "./Chebyshev";
 export * from "./Distortion";
 export * from "./FeedbackDelay";
 export * from "./FrequencyShifter";
