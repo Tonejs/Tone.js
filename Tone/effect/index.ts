802ce474c7c2b848d7893ebf1f5a550be6f44fd3
Yotam Mann <yotammann@gmail.com>
2019-10-31 15:29:48 -0400

    converting Chorus to ts

--- Tone/effect/index.ts
+++ Tone/effect/index.ts
@@ -3,6 +3,7 @@ export * from "./AutoPanner";
 export * from "./AutoWah";
 export * from "./BitCrusher";
 export * from "./Chebyshev";
+export * from "./Chorus";
 export * from "./Distortion";
 export * from "./FeedbackDelay";
 export * from "./FrequencyShifter";
