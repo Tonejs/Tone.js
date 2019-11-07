811b7f52cb6816a67beed16754040291c8b858ae
Yotam Mann <yotammann@gmail.com>
2019-11-07 15:44:07 -0500

    testing within a node context

--- test/html/node_test.js
+++ test/html/node_test.js
@@ -1,14 +1,10 @@
-//test the tone.js build
+/* eslint-disable @typescript-eslint/no-var-requires */
+// test the tone.js build
 const Tone = require("../../");
 const assert = require("assert");
-const { resolve } = require("path");
-const fs = require("fs");
 const semver = require("semver");
+const { version } = require("../../package.json");
 
-assert.strictEqual(Tone.global, global);
-
-//test the version
-const packageFile = resolve(__dirname, "../../package.json");
-const { version } = JSON.parse(fs.readFileSync(packageFile, "utf-8"));
+// test the version
 const diff = semver.diff(Tone.version, version);
 assert(diff === "patch" || diff === null, "wrong version listed");
