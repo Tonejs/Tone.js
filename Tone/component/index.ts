6ed67a5d0a8420e1de7fbdc5087d58bc0f8a00e9
Yotam Mann <yotammann@gmail.com>
2019-11-14 12:13:30 -0500

    exporting dynamics components

--- Tone/component/index.ts
+++ Tone/component/index.ts
@@ -4,6 +4,7 @@ export * from "./analysis/FFT";
 export * from "./analysis/DCMeter";
 export * from "./analysis/Waveform";
 export * from "./analysis/Follower";
+
 export * from "./channel/Channel";
 export * from "./channel/CrossFade";
 export * from "./channel/Merge";
@@ -15,10 +16,17 @@ export * from "./channel/PanVol";
 export * from "./channel/Volume";
 export * from "./channel/Solo";
 export * from "./channel/Split";
+
 export * from "./dynamics/Compressor";
+export * from "./dynamics/Gate";
+export * from "./dynamics/Limiter";
+export * from "./dynamics/MidSideCompressor";
+export * from "./dynamics/MultibandCompressor";
+
 export * from "./envelope/AmplitudeEnvelope";
 export * from "./envelope/Envelope";
 export * from "./envelope/FrequencyEnvelope";
+
 export * from "./filter/EQ3";
 export * from "./filter/Filter";
 export * from "./filter/OnePoleFilter";
