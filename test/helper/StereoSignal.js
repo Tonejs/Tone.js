3068ab1cc842d50d3faaa05b7b35d6d367101739
Yotam Mann <yotammann@gmail.com>
2019-11-16 17:28:00 -0500

    removing old js test files

--- test/helper/StereoSignal.js
+++
@@ -1,9 +0,0 @@
-import Signal from "Tone/signal/Signal";
-import Merge from "Tone/component/Merge";
-export default function(l, r){
-	var merge = new Merge();
-	var left = new Signal(l).connect(merge, 0, 0);
-	var right = new Signal(r).connect(merge, 0, 1);
-	return merge;
-}
-
