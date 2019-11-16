472afc7d1db4df51d6e565ab7209f6dc1295f750
Yotam Mann <yotammann@gmail.com>
2019-11-16 17:45:22 -0500

    reorganising testing directory

--- scripts/increment_version_ts.js
+++ scripts/increment_version_ts.js
@@ -1,37 +1,39 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require("fs");
 const semver = require("semver");
 const { resolve } = require("path");
-const child_process = require("child_process");
+const { execSync } = require("child_process");
 
-const tsVersion = child_process.execSync("npm show tone@typescript version").toString();
-const masterVersion = child_process.execSync("npm show tone version").toString();
+const tsVersion = execSync("npm show tone@typescript version").toString();
+const masterVersion = execSync("npm show tone version").toString();
 
-//go with whichever is the latest version
+// go with whichever is the latest version
 let version = masterVersion;
-if (tsVersion && semver.gt(tsVersion, masterVersion)){
+if (tsVersion && semver.gt(tsVersion, masterVersion)) {
 	version = tsVersion;
 }
 
-//increment the patch
+// increment the patch
 version = semver.inc(version, "patch");
 
-//write it to the package.json
+// write it to the package.json
 const packageFile = resolve(__dirname, "../package.json");
 const packageObj = JSON.parse(fs.readFileSync(packageFile, "utf-8"));
 
-//if the package version if the latest, go with that one
-if (semver.gt(packageObj.version, version)){
+// if the package version if the latest, go with that one
+if (semver.gt(packageObj.version, version)) {
 	version = packageObj.version;
 }
 
 console.log(`incrementing to version ${version}`);
 packageObj.version = version;
-//only if it's travis, update the package.json
-if (process.env.TRAVIS){
+// only if it's travis, update the package.json
+if (process.env.TRAVIS) {
 	fs.writeFileSync(packageFile, JSON.stringify(packageObj, undefined, "  "));
 	
-	//write a version file
-	var versionFile = `export const version: string = ${JSON.stringify(version)};\n`;
+	// write a version file
+	let versionFile = `export const version: string = ${JSON.stringify(version)};\n`;
 	fs.writeFileSync(resolve(__dirname, "../Tone/version.ts"), versionFile);
 }
 
