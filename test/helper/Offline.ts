e03607d0c6987c75dd80c13a4f6d45e450f934ad
Yotam Mann <yotammann@gmail.com>
2019-11-18 14:47:18 -0500

    reset the context even if the test fails

--- test/helper/Offline.ts
+++ test/helper/Offline.ts
@@ -8,27 +8,30 @@ type ReturnFunction = (time: Seconds) => void;
 
 export async function Offline(
 	callback: (context: OfflineContext) => void | ReturnFunction | ReturnFunction[] | Promise<void | ReturnFunction> | void,
-	duration = 0.1, channels = 1, sampleRate: number = 44100,
+	duration = 0.1, channels = 1, sampleRate = 44100,
 ): Promise<TestAudioBuffer> {
 	const originalContext = getContext();
 	const offline = new OfflineContext(channels, duration + 1 / sampleRate, sampleRate);
 	setContext(offline);
-	let retFunction = callback(offline);
-	if (retFunction instanceof Promise) {
-		retFunction = await retFunction;
-	}
-	if (isFunction(retFunction)) {
-		const fn = retFunction;
-		offline.on("tick", () => fn(offline.now()));
-	} else if (isArray(retFunction)) {
-		// each element in the array is a timing callback
-		retFunction.forEach(fn => {
+	try {
+		let retFunction = callback(offline);
+		if (retFunction instanceof Promise) {
+			retFunction = await retFunction;
+		}
+		if (isFunction(retFunction)) {
+			const fn = retFunction;
 			offline.on("tick", () => fn(offline.now()));
-		});
+		} else if (isArray(retFunction)) {
+			// each element in the array is a timing callback
+			retFunction.forEach(fn => {
+				offline.on("tick", () => fn(offline.now()));
+			});
+		}
+		const buffer = await offline.render();
+		return new TestAudioBuffer(buffer.get() as AudioBuffer);
+	} finally {
+		setContext(originalContext);
 	}
-	setContext(originalContext);
-	const buffer = await offline.render();
-	return new TestAudioBuffer(buffer.get() as AudioBuffer);
 }
 
 export function whenBetween(value: Seconds, start: Seconds, stop: Seconds, callback: () => void): void {
