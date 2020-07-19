f20d19beed5cb7f467753b91ac70bf9ce1ba7b7d
Yotam Mann <yotammann@gmail.com>
2020-07-19 13:48:48 -0700

    updating example

--- Tone/component/analysis/Meter.ts
+++ Tone/component/analysis/Meter.ts
@@ -22,7 +22,7 @@ export interface MeterOptions extends MeterBaseOptions {
  * // connect mic to the meter
  * mic.connect(meter);
  * // the current level of the mic
- * const level = meter.getValue();
+ * setInterval(() => console.log(meter.getValue().toFixed(2) + " db"), 100);
  * @category Component
  */
 export class Meter extends MeterBase<MeterOptions> {
