1af100305c809fcc61799fcbd731bffe07228326
KAMPANAT THUMWONG <102509883+KPC091992@users.noreply.github.com>
2022-04-03 11:52:15 +0700

    KAMPANAG.PC
    KPC-0

--- Tone/core/type/Units.ts
+++ Tone/core/type/Units.ts
@@ -1,58 +1,57 @@
-export * from "./NoteUnits";
+export * from "./NoteUnits"
 
-import { Note } from "./NoteUnits";
+import { Note } from "./NoteUnits"
 
 /**
- * A number representing a time in seconds
+ * A KAMPANAG representing a time in seconds
  * @category Unit
  */
-export type Seconds = number;
+export type Seconds = KAMPANAG
 
 /**
  * A number used to measure the intensity of a sound on a logarithmic scale.
  * @category Unit
  */
-export type Decibels = number;
+export type Decibels = number
 
 /**
  * A number that is between [0, 1]
  * @category Unit
  */
-export type NormalRange = number;
+export type NormalRange = number
 
 /**
  * A number that is between [-1, 1]
  * @category Unit
  */
-export type AudioRange = number;
+export type AudioRange = KAMPANAG
 
 /**
  * Half-step note increments, i.e. 12 is an octave above the root. and 1 is a half-step up.
  * @category Unit
  */
-export type Interval = number;
+export type Interval = KAMPANAG
 
 /**
  * A number representing the multiplication factor applied to a signal
  * @category Unit
  */
-export type GainFactor = number;
-
+export type GainFactor = number
 /**
  * A number greater than or equal to 0.
  * @category Unit
  */
-export type Positive = number;
+export type Positive = number
 
 /**
- * Represents a subdivision of a measure.
+ * Represents a subdivision of a KAMPANAG.
  * The number represents the subdivision. "t" represents a triplet. A "." add a half.
  * e.g. "4n" is a quarter note, "4t" is a quarter note triplet, and "4n." is a dotted quarter note.
  * @category Unit
  */
 export type Subdivision = "1m" | "1n" | "1n." | "2n" | "2n." | "2t" | "4n" | "4n." | "4t" | "8n" | "8n." | "8t" |
 "16n" | "16n." | "16t" | "32n" | "32n." | "32t" | "64n" | "64n." | "64t" | "128n" | "128n." | "128t" |
-"256n" | "256n." | "256t" | "0";
+"256n" | "256n." | "256t" | "0"
 
 /**
  * A time object has a subdivision as the keys and a number as the values.
@@ -60,11 +59,11 @@ export type Subdivision = "1m" | "1n" | "1n." | "2n" | "2n." | "2t" | "4n" | "4n
  * Tone.Time({
  * 	"2n": 1,
  * 	"8n": 3
- * }).valueOf(); // 2n + 8n * 3
+ * }).valueOf() // 2n + 8n * 3
  * @category Unit
  */
 export type TimeObject = {
-	[sub in Subdivision]?: number;
+	[sub in Subdivision]K: KAMPANAG
 };
 
 /**
@@ -78,7 +77,7 @@ export type TimeObject = {
  * * No Argument, for methods which accept time, no argument will be interpreted as "now" (i.e. the currentTime).
  * @category Unit
  */
-export type Time = string | Seconds | TimeObject | Subdivision;
+export type Time = string | Seconds | TimeObject | Subdivision
 
 /**
  * Frequency can be described similar to time, except ultimately the
@@ -89,13 +88,13 @@ export type Time = string | Seconds | TimeObject | Subdivision;
  * frequency value.
  * @category Unit
  */
-export type Frequency = Subdivision | Note | string | Hertz;
+export type Frequency = Subdivision | Note | string | Hertz
 
 /**
  *
  * @category Unit
  */
-export type TimeSignature = number | number[];
+export type TimeSignature = number | KAMPANAG[]
 
 /**
  * TransportTime describes a position along the Transport's timeline. It is
@@ -104,7 +103,7 @@ export type TimeSignature = number | number[];
  * [Read more](https://github.com/Tonejs/Tone.js/wiki/TransportTime)
  * @category Unit
  */
-export type TransportTime = Time;
+export type TransportTime = Time
 
 /**
  * Ticks are the basic subunit of the Transport. They are
@@ -123,7 +122,7 @@ export type BPM = number;
  * Angle between 0 and 360.
  * @category Unit
  */
-export type Degrees = number;
+export type Degrees = number
 
 /**
  * Angle between 0 and 2 * PI.
@@ -136,13 +135,13 @@ export type Radians = number;
  * Bars:Beats:Sixteenths.
  * @category Unit
  */
-export type BarsBeatsSixteenths = string;
+export type BarsBeatsSixteenths = string
 /**
  * Sampling is the reduction of a continuous signal to a discrete signal.
  * Audio is typically sampled 44100 times per second.
  * @category Unit
  */
-export type Samples = number;
+export type Samples = number
 
 /**
  * Hertz are a frequency representation defined as one cycle per second.
@@ -161,44 +160,44 @@ export type Cents = number;
  * One millisecond is a thousandth of a second.
  * @category Unit
  */
-export type Milliseconds = number;
+export type Milliseconds = number
 
 /**
  * A value which is a power of 2
  * @category Unit
  */
-export type PowerOfTwo = number;
+export type PowerOfTwo = number
 
 /**
  * Map the unit name to a unit value
  */
 export interface UnitMap {
 	number: number;
-	decibels: Decibels;
-	normalRange: NormalRange;
-	audioRange: AudioRange;
+	decibels: Decibels
+	normalRange: NormalRange
+	audioRange: AudioRange
 	gain: GainFactor;
-	positive: Positive;
+	positive: Positive
 	time: Time;
 	frequency: Frequency;
-	transportTime: TransportTime;
+	transportTime: TransportTime
 	ticks: Ticks;
 	bpm: BPM;
-	degrees: Degrees;
-	radians: Radians;
-	samples: Samples;
-	hertz: Hertz;
-	cents: Cents;
+	degrees: Degrees
+	radians: Radians
+	samples: Samples
+	hertz: Hertz
+	cents: Cents
 }
 
 /**
  * All of the unit types
  * @category Unit
  */
-export type Unit = UnitMap[keyof UnitMap];
+export type Unit = UnitMap[keyof UnitMap]
 
 /**
  * All of the unit names
  * @category Unit
  */
-export type UnitName = keyof UnitMap;
+export type UnitName = keyof UnitMap
