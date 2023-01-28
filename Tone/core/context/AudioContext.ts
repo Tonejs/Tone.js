4d126a161d7d80d34b54279a83a4772451f6fb2d
Jordan Danford <jordandanford@gmail.com>
2023-01-27 19:11:20 -0700

    Clean up types in `createAudioWorkletNode()`

--- Tone/core/context/AudioContext.ts
+++ Tone/core/context/AudioContext.ts
@@ -1,7 +1,7 @@
 import {
 	AudioContext as stdAudioContext,
 	AudioWorkletNode as stdAudioWorkletNode,
-	OfflineAudioContext as stdOfflineAudioContext
+	OfflineAudioContext as stdOfflineAudioContext,
 } from "standardized-audio-context";
 import { assert } from "../util/Debug";
 import { isDefined } from "../util/TypeCheck";
@@ -9,15 +9,25 @@ import { isDefined } from "../util/TypeCheck";
 /**
  * Create a new AudioContext
  */
-export function createAudioContext(options?: AudioContextOptions): AudioContext {
+export function createAudioContext(
+	options?: AudioContextOptions
+): AudioContext {
 	return new stdAudioContext(options) as unknown as AudioContext;
 }
 
 /**
  * Create a new OfflineAudioContext
  */
-export function createOfflineAudioContext(channels: number, length: number, sampleRate: number): OfflineAudioContext {
-	return new stdOfflineAudioContext(channels, length, sampleRate) as unknown as OfflineAudioContext;
+export function createOfflineAudioContext(
+	channels: number,
+	length: number,
+	sampleRate: number
+): OfflineAudioContext {
+	return new stdOfflineAudioContext(
+		channels,
+		length,
+		sampleRate
+	) as unknown as OfflineAudioContext;
 }
 
 /**
@@ -28,33 +38,47 @@ export type AnyAudioContext = AudioContext | OfflineAudioContext;
 /**
  * Interface for things that Tone.js adds to the window
  */
-interface ToneWindow extends Window {
+type ToneWindow = Window &
+	typeof globalThis & {
 	TONE_SILENCE_LOGGING?: boolean;
 	TONE_DEBUG_CLASS?: string;
-}
+};
 
 /**
  * A reference to the window object
  * @hidden
  */
-export const theWindow: ToneWindow | null = typeof self === "object" ? self : null;
+export const currentWindow: ToneWindow | null =
+	typeof self === "object" ? self : null;
 
 /**
  * If the browser has a window object which has an AudioContext
  * @hidden
  */
-export const hasAudioContext = theWindow &&
-	(theWindow.hasOwnProperty("AudioContext") || theWindow.hasOwnProperty("webkitAudioContext"));
+export const hasAudioContext =
+	currentWindow &&
+	(currentWindow.hasOwnProperty("AudioContext") ||
+		currentWindow.hasOwnProperty("webkitAudioContext"));
 
-export function createAudioWorkletNode(context: AnyAudioContext, name: string, options?: Partial<AudioWorkletNodeOptions>): AudioWorkletNode {
-	assert(isDefined(stdAudioWorkletNode), "AudioWorkletNode only works in a secure context (https or localhost)");
-	// @ts-ignore
-	return new ((context instanceof theWindow.BaseAudioContext) ? theWindow.AudioWorkletNode : stdAudioWorkletNode)(context, name, options);
+export function createAudioWorkletNode(
+	context: AnyAudioContext,
+	name: string,
+	options?: Partial<AudioWorkletNodeOptions>
+): AudioWorkletNode {
+	assert(
+		isDefined(stdAudioWorkletNode),
+		"AudioWorkletNode only works in a secure context (https or localhost)"
+	);
+	const nodeClass =
+		currentWindow && context instanceof currentWindow.BaseAudioContext
+			? currentWindow.AudioWorkletNode
+			: (stdAudioWorkletNode as typeof AudioWorkletNode);
+	return new nodeClass(context, name, options);
 }
 
 /**
- * This promise resolves to a boolean which indicates if the 
- * functionality is supported within the currently used browse. 
+ * This promise resolves to a boolean which indicates if the
+ * functionality is supported within the currently used browse.
  * Taken from [standardized-audio-context](https://github.com/chrisguttandin/standardized-audio-context#issupported)
  */
 export { isSupported as supported } from "standardized-audio-context";
