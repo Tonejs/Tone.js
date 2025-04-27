c894637db8dd8201a03a3c809dd8e3b18197b2e8
Yotam Mann <hi@yotammann.info>
2025-04-27 12:40:09 -0400

    removing `require` test

--- test/integration/require/test.cjs
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const Tone = require("tone");
-const assert = require("assert");
-
-assert("MonoSynth" in Tone, "Tone missing expected export");
-assert("start" in Tone, "Tone missing expected export");
