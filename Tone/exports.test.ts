7688c252b6cb34238dde2be5b1a773f3e286aeba
Yotam Mann <hi@yotammann.info>
2025-04-27 12:42:02 -0400

    Additional coverage

--- Tone/exports.test.ts
+++ Tone/exports.test.ts
@@ -3,6 +3,7 @@ import { expect } from "chai";
 import { DestinationClass } from "./core/context/Destination.js";
 import { TransportClass } from "./core/clock/Transport.js";
 import { DrawClass } from "./core/util/Draw.js";
+import { ListenerClass } from "./core/context/Listener.js";
 
 describe("Exports", () => {
 	it("has 'now' and 'immediate' methods", () => {
@@ -16,5 +17,6 @@ describe("Exports", () => {
 		expect(Tone.getDestination()).to.be.an.instanceOf(DestinationClass);
 		expect(Tone.getDraw()).to.be.an.instanceOf(DrawClass);
 		expect(Tone.getTransport()).to.be.an.instanceOf(TransportClass);
+		expect(Tone.getListener()).to.be.an.instanceOf(ListenerClass);
 	});
 });
