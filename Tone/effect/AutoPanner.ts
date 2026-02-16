efbdd13152d2abca9daf4b86ce9a827860f26a48
Yotam Mann <hi@yotammann.info>
2026-02-16 12:26:32 -0500

    Revert "feat: added width argument for AutoPanner (#1399)"
    This reverts commit 4b6117d678cd68dc3216356ac8058a0da619b0f2.

--- Tone/effect/AutoPanner.ts
+++ Tone/effect/AutoPanner.ts
@@ -1,11 +1,10 @@
 import { Panner } from "../component/channel/Panner.js";
-import { Frequency, NormalRange } from "../core/type/Units.js";
+import { Frequency } from "../core/type/Units.js";
 import { optionsFromArguments } from "../core/util/Defaults.js";
 import { LFOEffect, LFOEffectOptions } from "./LFOEffect.js";
 
 export interface AutoPannerOptions extends LFOEffectOptions {
 	channelCount: number;
-	width: NormalRange;
 }
 
 /**
@@ -25,9 +24,7 @@ export class AutoPanner extends LFOEffect<AutoPannerOptions> {
 	/**
 	 * The filter node
 	 */
-	private readonly _panner: Panner;
-
-	private _width: NormalRange;
+	readonly _panner: Panner;
 
 	/**
 	 * @param frequency Rate of left-right oscillation.
@@ -46,40 +43,17 @@ export class AutoPanner extends LFOEffect<AutoPannerOptions> {
 			context: this.context,
 			channelCount: options.channelCount,
 		});
-
-		this._width = options.width;
-
 		// connections
 		this.connectEffect(this._panner);
 		this._lfo.connect(this._panner.pan);
-		this._updateLFORange();
+		this._lfo.min = -1;
+		this._lfo.max = 1;
 	}
 
 	static getDefaults(): AutoPannerOptions {
 		return Object.assign(LFOEffect.getDefaults(), {
 			channelCount: 1,
-			width: 1,
 		});
-	}
-
-	/**
-	 * Updates the LFO min/max based on width
-	 */
-	private _updateLFORange(): void {
-		this._lfo.min = -this._width;
-		this._lfo.max = this._width;
-	}
-
-	/**
-	 * The width of the panning effect. 0 = no panning, 1 = full left-right panning.
-	 */
-	get width(): NormalRange {
-		return this._width;
-	}
-
-	set width(width: NormalRange) {
-		this._width = width;
-		this._updateLFORange();
 	}
 
 	dispose(): this {
